c2f57cd949a07b31d73eda03bea34e8225ea269b
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2016-05-25 19:15:08 -0400

    update

--- tests/test_isl.cpp
+++ tests/test_isl.cpp
@@ -14,31 +14,24 @@
 int main(int argc, char **argv)
 {
 	isl_ctx *ctx = isl_ctx_alloc();
-
 	Computation computation0(ctx, "{S0[i,j]: 0<=i<=1000 and 0<=j<=1000}");
-
-	IF_DEBUG(str_dump("\nIteration Space IR:\n")); computation0.dump();
-
 	isl_union_map *schedule_map = create_schedule_map(ctx, "{S0[i,j]->S0[i+100,j+100]}");
-
-	IF_DEBUG(str_dump("Schedule:\n"));
-	IF_DEBUG(isl_union_map_dump(schedule_map)); IF_DEBUG(str_dump("\n\n"));
-
 	isl_schedule *schedule_tree = create_schedule_tree(ctx, isl_union_set_copy(computation0.iter_space), isl_union_map_copy(schedule_map));
 	isl_union_set *time_space = create_time_space(isl_union_set_copy(computation0.iter_space), isl_union_map_copy(schedule_map));
 
-	IF_DEBUG(str_dump("\n\nTime Space IR:\n"));
-	IF_DEBUG(isl_union_set_dump(time_space)); IF_DEBUG(str_dump("\n\n"));
+
+	IF_DEBUG(str_dump("\nIteration Space IR:\n")); computation0.dump();
+	IF_DEBUG(str_dump("Schedule:\n")); IF_DEBUG(isl_union_map_dump(schedule_map)); IF_DEBUG(str_dump("\n\n"));
+	IF_DEBUG(str_dump("\n\nTime Space IR:\n")); IF_DEBUG(isl_union_set_dump(time_space)); IF_DEBUG(str_dump("\n\n"));
+
 
 	Halide::Argument buffer_arg("buf", Halide::Argument::OutputBuffer, Halide::Int(32), 3);
     	std::vector<Halide::Argument> args(1);
     	args[0] = buffer_arg;
-
-	Halide::Internal::IRPrinter pr(std::cout);
 	Halide::Internal::Stmt s = Halide::Internal::AssertStmt::make (Halide::Expr(0), Halide::Expr(1));
-
 	Halide::Module::Module m("", Halide::get_host_target());
 	m.append(Halide::Internal::LoweredFunc("test1", args, s, Halide::Internal::LoweredFunc::External));
+
 
 	isl_ast_build *ast_build = isl_ast_build_alloc(ctx);
 	ast_build = isl_ast_build_set_after_each_for(ast_build, &for_halide_code_generator_after_for, &s);
@@ -46,8 +39,9 @@ int main(int argc, char **argv)
 	isl_ast_node *program = isl_ast_build_node_from_schedule(ast_build, schedule_tree);
 	isl_ast_build_free(ast_build);
 
-	IF_DEBUG(str_dump("\n\n"));
-	IF_DEBUG(str_dump("\nGenerated Halide Low Level IR:\n"));
+
+	IF_DEBUG(str_dump("\n\n")); IF_DEBUG(str_dump("\nGenerated Halide Low Level IR:\n"));
+	Halide::Internal::IRPrinter pr(std::cout);
     	pr.print(s);
 	IF_DEBUG(str_dump("\n\n"));
 
