3d5ea03ac5ec40107aefaa426b3bcd8f0ed7ad3e
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2016-06-04 16:21:28 -0400

    Remove a statement committed by error

--- tests/test_isl.cpp
+++ tests/test_isl.cpp
@@ -24,8 +24,6 @@ int main(int argc, char **argv)
 	Computation computation1(ctx, s1, "{S1[i,j]: 0<=i<=1023 and 0<=j<=1023}", &fct);
 
 	computation0.tile("i","j","i0","i1","j1","j2", 32,32);
-	computation0.split("j2","j21","j22", 4);
-
 	computation1.use_schedule_map("{S1[i,j]->[1,i1,j1,i2,j3,j4]: i1=floor(i/32) and j1=floor(j/32) and i2=i and j3=floor(j/4) and j4=j%4 and 0<=i<=1023 and 0<=j<=1023}");
 	pgm.tag_parallel_dimension("S0", 1);
 	pgm.tag_vector_dimension("S1", 5);
