6077ee1ca67144e3b7d5c479c0447f53a1eb5f4d
Patricia Suriana <patricia.suriana@gmail.com>
2016-10-17 17:12:05 -0700

    Adding wrapper test

---
+++ tests/wrapper_test_05.h
@@ -0,0 +1,16 @@
+#ifndef HALIDE__generated_pgm_05_h
+#define HALIDE__generated_pgm_05_h
+
+#include <coli/utils.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int f(buffer_t *_p0_buffer) HALIDE_FUNCTION_ATTRS;
+int f_argv(void **args) HALIDE_FUNCTION_ATTRS;
+
+extern const struct halide_filter_metadata_t halide_pipeline_aot_metadata;
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif
