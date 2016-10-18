34eabd5f567fd03e51d863966f056d65c3ffb3aa
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2016-10-18 08:35:39 -0400

    Fix blurxy benchmark

--- benchmarks/halide/blurxy_coli.cpp
+++ benchmarks/halide/blurxy_coli.cpp
@@ -48,20 +48,20 @@ int main(int argc, char **argv)
     buffer b_blury("b_blury", 2, {coli::expr(SIZE0),coli::expr(SIZE1)}, p_uint16, NULL, a_output, &blurxy);
     expr e_p0 = expr((int32_t) SIZE0);
     expr e_p1 = expr((int32_t) SIZE1);
-    constant p0("N", &e_p0, p_int32, true, NULL, 0, &blurxy);
-    constant p1("M", &e_p1, p_int32, true, NULL, 0, &blurxy);
+    constant p0("N", e_p0, p_int32, true, NULL, 0, &blurxy);
+    constant p1("M", e_p1, p_int32, true, NULL, 0, &blurxy);
 
     // Declare the computations c_blurx and c_blury.
-    computation c_input("[N]->{c_input[i,j]: 0<=i<N and 0<=j<N}", NULL, false, p_uint16, &blurxy);
+    computation c_input("[N]->{c_input[i,j]: 0<=i<N and 0<=j<N}", expr(), false, p_uint16, &blurxy);
 
     idx i = idx("i");
     idx j = idx("j");
 
     expr e1 = (c_input(i-1, j) + c_input(i, j) + c_input(i+1, j))/((uint16_t) 3);
-    computation c_blurx("[N,M]->{c_blurx[i,j]: 0<i<N-1 and 0<j<M-1}", &e1, true, p_uint16, &blurxy);
+    computation c_blurx("[N,M]->{c_blurx[i,j]: 1<i<N-2 and 1<j<M-2}", e1, true, p_uint16, &blurxy);
 
     expr e2 = (c_blurx(i, j-1) + c_blurx(i, j) + c_blurx(i, j+1))/((uint16_t) 3);
-    computation c_blury("[N,M]->{c_blury[i,j]: 1<i<N-2 and 1<j<M-2}", &e2, true, p_uint16, &blurxy);
+    computation c_blury("[N,M]->{c_blury[i,j]: 1<i<N-2 and 1<j<M-2}", e2, true, p_uint16, &blurxy);
 
     // Create a memory buffer (2 dimensional).
     buffer b_blurx("b_blurx", 2, {coli::expr(SIZE0),coli::expr(SIZE1)}, p_uint16, NULL, a_temporary, &blurxy);
@@ -72,11 +72,19 @@ int main(int argc, char **argv)
     c_blury.set_access("{c_blury[i,j]->b_blury[i,j]}");
 
     // Set the schedule of each computation.
-    //c_blury.split(0, 8);
-    //c_blury.tag_parallel_dimension(0);
-    //c_blury.split(2, 8);
-    //c_blury.tag_vector_dimension(3);
-    c_blury.after(c_blurx, computation::root_dimension);
+    /*c_blury.split(1, 8);
+    c_blury.tag_parallel_dimension(0);
+    c_blury.split(5, 8);
+    c_blury.tag_vector_dimension(3);
+    */
+    c_blury.after(c_blurx, 1);
+
+    /*
+    c_blurx.set_schedule("[N,M]->{c_blurx[i,j]->[i1,j1,0,i2,j2]: 0<i<N-1 and 0<j<M-1 and i1=floor(i/8) and i2=i%8 and j1=floor(j/8) and j2=j%8}");
+    c_blury.set_schedule("[N,M]->{c_blury[i,j]->[i1,j1,1,i2,j2]: 0<i<N-1 and 0<j<M-1 and i1=floor(i/8) and i2=i%8 and j1=floor(j/8) and j2=j%8}");
+    */
+    c_blury.tag_parallel_dimension(0);
+
 
     // Set the arguments to blurxy
     blurxy.set_arguments({&b_input, &b_blury});
