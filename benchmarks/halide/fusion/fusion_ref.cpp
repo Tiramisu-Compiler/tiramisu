8a82b6bac2bbb0083ecd87e15f417d9ca3f7187b
Patricia Suriana <patricia.suriana@gmail.com>
2016-10-24 09:45:34 -0700

    Update the halide fusion example

--- benchmarks/halide/fusion/fusion_ref.cpp
+++ benchmarks/halide/fusion/fusion_ref.cpp
@@ -1,9 +1,11 @@
 #include "Halide.h"
+
 using namespace Halide;
 
 int main(int argc, char **argv) {
-
-    ImageParam rgb(UInt(8), 2);
+    const int f_size = 30;
+    const int g_size = 40;
+    Image<int> f_im_ref(f_size, f_size), g_im_ref(g_size, g_size);
 
     Var x("x"), y("y");
     Func f("f"), g("g"), input("input");
@@ -13,20 +15,20 @@ int main(int argc, char **argv) {
     g(x, y) = x + input(x, y);
 
     input.compute_at(f, y);
-    g.compute_with(f, y);
+    //g.compute_with(f, y);
 
     Pipeline({f, g}).realize({f_im, g_im});
 
     Halide::Target target = Halide::get_host_target();
     Pipeline({f, g}).compile_to_object("build/generated_fct_fusion_ref.o",
-                                       {input},
+                                       {},
                                        "fusion_ref",
                                        target);
 
     Pipeline({f, g}).compile_to_lowered_stmt("build/generated_fct_fusion_ref.txt",
-                                   {input},
-                                   Text,
-                                   target);
+                                             {},
+                                             Halide::Text,
+                                             target);
 
     return 0;
 }
--- benchmarks/halide/rgb2yuv/rgb2yuv_ref.cpp
+++ benchmarks/halide/rgb2yuv/rgb2yuv_ref.cpp
@@ -1,20 +1,19 @@
 #include "Halide.h"
+
 using namespace Halide;
 
 int main(int argc, char **argv) {
 
-    ImageParam rgb(UInt(8), 2);
+    ImageParam rgb(UInt(8), 3);
 
-    Var x("x"), y("y"), z("z");
+    Image<int> y_im(rgb.width(), rgb.height()), u_im(rgb.width()/2, rgb.height()/2), v_im(rgb.width()/2, rgb.height()/2);
+
+    Var x("x"), y("y");
     Func y_part("y_part"), u_part("u_part"), v_part("v_part");
 
     y_part(x, y) = ((66 * rgb(x, y, 0) + 129 * rgb(x, y, 1) +  25 * rgb(x, y, 2) + 128) >> 8) +  16;
     u_part(x, y) = (( -38 * rgb(2*x, 2*y, 0) -  74 * rgb(2*x, 2*y, 1) + 112 * rgb(2*x, 2*y, 2) + 128) >> 8) + 128;
     v_part(x, y) = (( 112 * rgb(2*x, 2*y, 0) -  94 * rgb(2*x, 2*y, 1) -  18 * rgb(2*x, 2*y, 2) + 128) >> 8) + 128;
-
-    y_part.realize();
-    u_part.realize();
-    v_part.realize();
 
     // u_part.compute_with(y_part, y);
     // v_part.compute_with(u_part, y);
@@ -30,7 +29,7 @@ int main(int argc, char **argv) {
 
     Pipeline({y_part, u_part, v_part}).compile_to_lowered_stmt("build/generated_fct_rgb2yuv_ref.txt",
                                                                {rgb},
-                                                               Text,
+                                                               Halide::Text,
                                                                target);
 
     return 0;
