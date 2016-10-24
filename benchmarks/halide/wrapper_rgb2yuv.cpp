3331d1179a5e727b26fec55bd5fc80a926310d75
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2016-10-24 13:48:14 -0400

    Update data type

--- benchmarks/halide/wrapper_rgb2yuv.cpp
+++ benchmarks/halide/wrapper_rgb2yuv.cpp
@@ -12,19 +12,19 @@ int main(int, char**)
     std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
     std::vector<std::chrono::duration<double,std::milli>> duration_vector_2;
 
-    Halide::Image<uint16_t> input = Halide::Tools::load_image("./images/rgb.png");
+    Halide::Image<uint32_t> input = Halide::Tools::load_image("./images/rgb.png");
 
-    Halide::Image<uint16_t> output1(input.width()-8, input.height()-2);
-    Halide::Image<uint16_t> output2(input.width()-8, input.height()-2);
+    Halide::Image<uint32_t> output1(input.width(), input.height());
+    Halide::Image<uint32_t> output2(input.width(), input.height());
 
     // Warm up
-    blurxy_coli(input, output1);
+    rgb2yuv_coli(input, output1);
 
     // Reference
     for (int i=0; i<NB_TESTS; i++)
     {
         auto start1 = std::chrono::high_resolution_clock::now();
-        blurxy_coli(input, output1);
+        rgb2yuv_coli(input, output1);
         auto end1 = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double,std::milli> duration1 = end1 - start1;
         duration_vector_1.push_back(duration1);
@@ -34,20 +34,20 @@ int main(int, char**)
     for (int i=0; i<NB_TESTS; i++)
     {
         auto start2 = std::chrono::high_resolution_clock::now();
-        blurxy_ref(input, output2);
+        rgb2yuv_ref(input, output2);
         auto end2 = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double,std::milli> duration2 = end2 - start2;
         duration_vector_2.push_back(duration2);
     }
 
-    print_time("performance_CPU.csv", "blurxy",
+    print_time("performance_CPU.csv", "rgb2yuv",
                {"  COLi "," Halide "},
                {median(duration_vector_1), median(duration_vector_2)});
 
-//  compare_2_2D_arrays("Blurxy",  output1.data(), output2.data(), input.extent(0), input.extent(1));
+   //compare_2_2D_arrays("Blurxy",  output1.data(), output2.data(), input.extent(0), input.extent(1));
 
-    Halide::Tools::save_image(output1, "./build/blurxy_coli.png");
-    Halide::Tools::save_image(output2, "./build/blurxy_ref.png");
+    Halide::Tools::save_image(output1, "./build/rgb2yuv_coli.png");
+    Halide::Tools::save_image(output2, "./build/rgb2yuv_ref.png");
 
     return 0;
 }
--- benchmarks/halide/wrapper_rgb2yuv.h
+++ benchmarks/halide/wrapper_rgb2yuv.h
@@ -7,10 +7,10 @@
 extern "C" {
 #endif
 
-int blurxy_coli(buffer_t *_b_input_buffer, buffer_t *_b_blury_buffer) HALIDE_FUNCTION_ATTRS;
-int blurxy_coli_argv(void **args) HALIDE_FUNCTION_ATTRS;
-int blurxy_ref(buffer_t *_b_input_buffer, buffer_t *_b_blury_buffer) HALIDE_FUNCTION_ATTRS;
-int blurxy_ref_argv(void **args) HALIDE_FUNCTION_ATTRS;
+int rgb2yuv_coli(buffer_t *_b_input_buffer, buffer_t *_b_blury_buffer) HALIDE_FUNCTION_ATTRS;
+int rgb2yuv_coli_argv(void **args) HALIDE_FUNCTION_ATTRS;
+int rgb2yuv_ref(buffer_t *_b_input_buffer, buffer_t *_b_blury_buffer) HALIDE_FUNCTION_ATTRS;
+int rgb2yuv_ref_argv(void **args) HALIDE_FUNCTION_ATTRS;
 // Result is never null and points to constant static data
 const struct halide_filter_metadata_t *blurxy_coli_metadata() HALIDE_FUNCTION_ATTRS;
 const struct halide_filter_metadata_t *blurxy_ref_metadata() HALIDE_FUNCTION_ATTRS;
