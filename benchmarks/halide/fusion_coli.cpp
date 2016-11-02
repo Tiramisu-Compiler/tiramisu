b585a335185785c9ae7045a2dbefa128570837fe
Patricia Suriana <patricia.suriana@gmail.com>
2016-11-02 11:27:49 -0700

    Update fusion benchmark to use vectorize/parallelize

--- benchmarks/halide/fusion_coli.cpp
+++ benchmarks/halide/fusion_coli.cpp
@@ -22,7 +22,8 @@ int main(int argc, char **argv)
 
     coli::function fusion_coli("fusion_coli");
     coli::buffer buff_f("buff_f", 2, {coli::expr(100), coli::expr(100)}, coli::p_int32, NULL, coli::a_output, &fusion_coli);
-        coli::buffer buff_in("buff_in", 2, {coli::expr((int32_t)100), coli::expr((int32_t)100)}, coli::p_int32, NULL, coli::a_temporary, &fusion_coli);
+    coli::buffer buff_in("buff_in", 2, {coli::expr((int32_t)100), coli::expr((int32_t)100)}, coli::p_int32, NULL, coli::a_temporary, &fusion_coli);
+
     // Define loop bounds for dimension "_in_s0_y".
     coli::constant _in_s0_y_loop_min("_in_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant _in_s0_y_loop_extent("_in_s0_y_loop_extent", coli::expr((int32_t)100), coli::p_int32, true, NULL, 0, &fusion_coli);
@@ -30,11 +31,11 @@ int main(int argc, char **argv)
     // Define loop bounds for dimension "_in_s0_x".
     coli::constant _in_s0_x_loop_min("_in_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant _in_s0_x_loop_extent("_in_s0_x_loop_extent", coli::expr((int32_t)100), coli::p_int32, true, NULL, 0, &fusion_coli);
-
     coli::computation in("[_in_s0_y_loop_min, _in_s0_y_loop_extent, _in_s0_x_loop_min, _in_s0_x_loop_extent]->{in[_in_s0_x, _in_s0_y]: "
                         "(_in_s0_y_loop_min <= _in_s0_y <= ((_in_s0_y_loop_min + _in_s0_y_loop_extent) + -1)) and (_in_s0_x_loop_min <= _in_s0_x <= ((_in_s0_x_loop_min + _in_s0_x_loop_extent) + -1))}",
                         coli::expr((int32_t)13), true, coli::p_int32, &fusion_coli);
     in.set_access("{in[_in_s0_x, _in_s0_y]->buff_in[_in_s0_x, _in_s0_y]}");
+
     // Define loop bounds for dimension "_f_s0_y".
     coli::constant _f_s0_y_loop_min("_f_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant _f_s0_y_loop_extent("_f_s0_y_loop_extent", coli::expr((int32_t)100), coli::p_int32, true, NULL, 0, &fusion_coli);
@@ -42,11 +43,16 @@ int main(int argc, char **argv)
     // Define loop bounds for dimension "_f_s0_x".
     coli::constant _f_s0_x_loop_min("_f_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant _f_s0_x_loop_extent("_f_s0_x_loop_extent", coli::expr((int32_t)100), coli::p_int32, true, NULL, 0, &fusion_coli);
-
     coli::computation f("[_f_s0_y_loop_min, _f_s0_y_loop_extent, _f_s0_x_loop_min, _f_s0_x_loop_extent]->{f[_f_s0_x, _f_s0_y]: "
                         "(_f_s0_y_loop_min <= _f_s0_y <= ((_f_s0_y_loop_min + _f_s0_y_loop_extent) + -1)) and (_f_s0_x_loop_min <= _f_s0_x <= ((_f_s0_x_loop_min + _f_s0_x_loop_extent) + -1))}",
                         coli::expr(coli::o_cast, coli::p_float32, (in(coli::idx("_f_s0_x"), coli::idx("_f_s0_y")) >> coli::expr((int32_t)2))), true, coli::p_float32, &fusion_coli);
     f.set_access("{f[_f_s0_x, _f_s0_y]->buff_f[_f_s0_x, _f_s0_y]}");
+
+    // Add schedules.
+    in.first(computation::root_dimension);
+    in.tag_parallel_dimension(1);
+    f.after(in, computation::root_dimension);
+    f.tag_vector_dimension(2);
 
     fusion_coli.set_arguments({&buff_f});
     fusion_coli.gen_time_processor_domain();
