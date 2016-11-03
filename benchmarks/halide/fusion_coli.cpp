670cdf7fd59aedba2b1b7e2a11398bd07787f201
Patricia Suriana <patricia.suriana@gmail.com>
2016-11-03 15:08:02 -0700

    Add wrap affine

--- benchmarks/halide/fusion_coli.cpp
+++ benchmarks/halide/fusion_coli.cpp
@@ -21,40 +21,31 @@ int main(int argc, char **argv)
     global::set_default_coli_options();
 
     coli::function fusion_coli("fusion_coli");
-    coli::buffer buff_f("buff_f", 2, {coli::expr(100), coli::expr(50)}, coli::p_int32, NULL, coli::a_output, &fusion_coli);
-    coli::buffer buff_in("buff_in", 2, {coli::expr((int32_t)100), coli::expr((int32_t)50)}, coli::p_int32, NULL, coli::a_temporary, &fusion_coli);
+    coli::buffer buff_affine_s0("buff_affine_s0", 2, {coli::expr(256), coli::expr(128)}, coli::p_uint8, NULL, coli::a_output, &fusion_coli);
+    coli::buffer buff_b0("buff_b0", 2, {coli::expr(256), coli::expr(128)}, coli::p_uint8, NULL, coli::a_input, &fusion_coli);
+    coli::computation b0("{b0[i0, i1]: (0 <= i0 <= 255) and (0 <= i1 <= 127)}", expr(), false, coli::p_uint8, &fusion_coli);
+    b0.set_access("{b0[i0, i1]->buff_b0[i0, i1]}");
 
-    // Define loop bounds for dimension "_in_s0_x".
-    coli::constant _in_s0_x_loop_min("_in_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::constant _in_s0_x_loop_extent("_in_s0_x_loop_extent", coli::expr((int32_t)100), coli::p_int32, true, NULL, 0, &fusion_coli);
 
-    // Define loop bounds for dimension "_in_s0_y".
-    coli::constant _in_s0_y_loop_min("_in_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::constant _in_s0_y_loop_extent("_in_s0_y_loop_extent", coli::expr((int32_t)50), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::computation in("[_in_s0_x_loop_min, _in_s0_x_loop_extent, _in_s0_y_loop_min, _in_s0_y_loop_extent]->{in[_in_s0_x, _in_s0_y]: "
-                        "(_in_s0_x_loop_min <= _in_s0_x <= ((_in_s0_x_loop_min + _in_s0_x_loop_extent) + -1)) and (_in_s0_y_loop_min <= _in_s0_y <= ((_in_s0_y_loop_min + _in_s0_y_loop_extent) + -1))}",
-                        coli::expr((int32_t)13), true, coli::p_int32, &fusion_coli);
-    in.set_access("{in[_in_s0_x, _in_s0_y]->buff_in[_in_s0_x, _in_s0_y]}");
+    // Define loop bounds for dimension "affine_s0_y".
+    coli::constant affine_s0_y_loop_min("affine_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant affine_s0_y_loop_extent("affine_s0_y_loop_extent", coli::expr((int32_t)128), coli::p_int32, true, NULL, 0, &fusion_coli);
 
-    // Define loop bounds for dimension "_f_s0_y".
-    coli::constant _f_s0_y_loop_min("_f_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::constant _f_s0_y_loop_extent("_f_s0_y_loop_extent", coli::expr((int32_t)50), coli::p_int32, true, NULL, 0, &fusion_coli);
+    // Define loop bounds for dimension "affine_s0_x".
+    coli::constant affine_s0_x_loop_min("affine_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant affine_s0_x_loop_extent("affine_s0_x_loop_extent", coli::expr((int32_t)256), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::computation affine_s0("[affine_s0_y_loop_min, affine_s0_y_loop_extent, affine_s0_x_loop_min, affine_s0_x_loop_extent]->{affine_s0[affine_s0_x, affine_s0_y]: "
+                        "(affine_s0_y_loop_min <= affine_s0_y <= ((affine_s0_y_loop_min + affine_s0_y_loop_extent) + -1)) and (affine_s0_x_loop_min <= affine_s0_x <= ((affine_s0_x_loop_min + affine_s0_x_loop_extent) + -1))}",
+                        ((((coli::expr(coli::o_cast, coli::p_float32, b0(coli::expr(coli::o_max, coli::expr(coli::o_min, coli::expr(coli::o_cast, coli::p_int32, coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))), coli::expr((int32_t)128)), coli::expr((int32_t)0)), coli::expr(coli::o_max, coli::expr(coli::o_min, coli::expr(coli::o_cast, coli::p_int32, coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))), coli::expr((int32_t)256)), coli::expr((int32_t)0))))*(coli::expr((float)1.000000) - ((((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)) - coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))))) + (coli::expr(coli::o_cast, coli::p_float32, b0(coli::expr(coli::o_max, coli::expr(coli::o_min, (coli::expr(coli::o_cast, coli::p_int32, coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))) + coli::expr((int32_t)1)), coli::expr((int32_t)128)), coli::expr((int32_t)0)), coli::expr(coli::o_max, coli::expr(coli::o_min, coli::expr(coli::o_cast, coli::p_int32, coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))), coli::expr((int32_t)256)), coli::expr((int32_t)0))))*((((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)) - coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000))))))*(coli::expr((float)1.000000) - ((((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)) - coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))))) + (((coli::expr(coli::o_cast, coli::p_float32, b0(coli::expr(coli::o_max, coli::expr(coli::o_min, coli::expr(coli::o_cast, coli::p_int32, coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))), coli::expr((int32_t)128)), coli::expr((int32_t)0)), coli::expr(coli::o_max, coli::expr(coli::o_min, (coli::expr(coli::o_cast, coli::p_int32, coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))) + coli::expr((int32_t)1)), coli::expr((int32_t)256)), coli::expr((int32_t)0))))*(coli::expr((float)1.000000) - ((((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)) - coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))))) + (coli::expr(coli::o_cast, coli::p_float32, b0(coli::expr(coli::o_max, coli::expr(coli::o_min, (coli::expr(coli::o_cast, coli::p_int32, coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))) + coli::expr((int32_t)1)), coli::expr((int32_t)128)), coli::expr((int32_t)0)), coli::expr(coli::o_max, coli::expr(coli::o_min, (coli::expr(coli::o_cast, coli::p_int32, coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))) + coli::expr((int32_t)1)), coli::expr((int32_t)256)), coli::expr((int32_t)0))))*((((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)) - coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000))))))*((((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)) - coli::expr(o_floor, (((coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_y"))) + (coli::expr((float)0.100000)*coli::expr(coli::o_cast, coli::p_float32, coli::idx("affine_s0_x")))) + coli::expr((float)0.100000)))))), true, coli::p_float32, &fusion_coli);
+    affine_s0.set_access("{affine_s0[affine_s0_x, affine_s0_y]->buff_affine_s0[affine_s0_x, affine_s0_y]}");
 
-    // Define loop bounds for dimension "_f_s0_x".
-    coli::constant _f_s0_x_loop_min("_f_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::constant _f_s0_x_loop_extent("_f_s0_x_loop_extent", coli::expr((int32_t)100), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::computation f("[_f_s0_y_loop_min, _f_s0_y_loop_extent, _f_s0_x_loop_min, _f_s0_x_loop_extent]->{f[_f_s0_x, _f_s0_y]: "
-                        "(_f_s0_y_loop_min <= _f_s0_y <= ((_f_s0_y_loop_min + _f_s0_y_loop_extent) + -1)) and (_f_s0_x_loop_min <= _f_s0_x <= ((_f_s0_x_loop_min + _f_s0_x_loop_extent) + -1))}",
-                        coli::expr(coli::o_cast, coli::p_float32, (in(coli::idx("_f_s0_x"), coli::idx("_f_s0_y")) >> coli::expr((int32_t)2))), true, coli::p_float32, &fusion_coli);
-    f.set_access("{f[_f_s0_x, _f_s0_y]->buff_f[_f_s0_x, _f_s0_y]}");
+    // Define compute level for "affine".
+    affine_s0.first(computation::root_dimension);
 
     // Add schedules.
-    in.first(computation::root_dimension);
-    in.tag_parallel_dimension(1);
-    f.after(in, computation::root_dimension);
-    f.tag_vector_dimension(1);
+    affine_s0.tag_parallel_dimension(1);
 
-    fusion_coli.set_arguments({&buff_f});
+    fusion_coli.set_arguments({&buff_b0, &buff_affine_s0});
     fusion_coli.gen_time_processor_domain();
     fusion_coli.gen_isl_ast();
     fusion_coli.gen_halide_stmt();
@@ -63,3 +54,4 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
