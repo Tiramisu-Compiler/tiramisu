0ab9b966b0e405ab96b140b12bda8c51a17c3061
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2017-01-29 10:33:04 +0100

    Update fusion benchmark

--- benchmarks/halide/fusion_coli.cpp
+++ benchmarks/halide/fusion_coli.cpp
@@ -32,10 +32,9 @@ int main(int argc, char **argv)
     int f_extent_1 = SIZE1;
     int f_extent_0 = SIZE0;
     coli::buffer buff_f("buff_f", 3, {coli::expr(f_extent_2), coli::expr(f_extent_1), coli::expr(f_extent_0)}, coli::p_uint8, NULL, coli::a_output, &fusion_coli);
-    int g_extent_2 = SIZE2;
-    int g_extent_1 = SIZE1;
-    int g_extent_0 = SIZE0;
-    coli::buffer buff_g("buff_g", 3, {coli::expr(g_extent_2), coli::expr(g_extent_1), coli::expr(g_extent_0)}, coli::p_uint8, NULL, coli::a_output, &fusion_coli);
+    coli::buffer buff_g("buff_g", 3, {coli::expr(f_extent_2), coli::expr(f_extent_1), coli::expr(f_extent_0)}, coli::p_uint8, NULL, coli::a_output, &fusion_coli);
+    coli::buffer buff_h("buff_h", 3, {coli::expr(f_extent_2), coli::expr(f_extent_1), coli::expr(f_extent_0)}, coli::p_uint8, NULL, coli::a_output, &fusion_coli);
+    coli::buffer buff_k("buff_k", 3, {coli::expr(f_extent_2), coli::expr(f_extent_1), coli::expr(f_extent_0)}, coli::p_uint8, NULL, coli::a_output, &fusion_coli);
 
     // Input buffers.
     int input_extent_2 = SIZE2;
@@ -46,15 +45,11 @@ int main(int argc, char **argv)
     input.set_access("{input[i2, i1, i0]->buff_input[i2, i1, i0]}");
 
 
-    // Define loop bounds for dimension "f_s0_c".
+
     coli::constant f_s0_c_loop_min("f_s0_c_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant f_s0_c_loop_extent("f_s0_c_loop_extent", coli::expr(f_extent_2), coli::p_int32, true, NULL, 0, &fusion_coli);
-
-    // Define loop bounds for dimension "f_s0_y".
     coli::constant f_s0_y_loop_min("f_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant f_s0_y_loop_extent("f_s0_y_loop_extent", coli::expr(f_extent_1), coli::p_int32, true, NULL, 0, &fusion_coli);
-
-    // Define loop bounds for dimension "f_s0_x".
     coli::constant f_s0_x_loop_min("f_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant f_s0_x_loop_extent("f_s0_x_loop_extent", coli::expr(f_extent_0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::computation f_s0("[f_s0_c_loop_min, f_s0_c_loop_extent, f_s0_y_loop_min, f_s0_y_loop_extent, f_s0_x_loop_min, f_s0_x_loop_extent]->{f_s0[f_s0_c, f_s0_y, f_s0_x]: "
@@ -62,31 +57,70 @@ int main(int argc, char **argv)
                         (coli::expr((uint8_t)255) - input(coli::idx("f_s0_c"), coli::idx("f_s0_y"), coli::idx("f_s0_x"))), true, coli::p_uint8, &fusion_coli);
     f_s0.set_access("{f_s0[f_s0_c, f_s0_y, f_s0_x]->buff_f[f_s0_c, f_s0_y, f_s0_x]}");
 
-    // Define compute level for "f".
-    f_s0.first(computation::root_dimension);
+
 
     // Define loop bounds for dimension "g_s0_c".
     coli::constant g_s0_c_loop_min("g_s0_c_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::constant g_s0_c_loop_extent("g_s0_c_loop_extent", coli::expr(g_extent_2), coli::p_int32, true, NULL, 0, &fusion_coli);
-
-    // Define loop bounds for dimension "g_s0_y".
+    coli::constant g_s0_c_loop_extent("g_s0_c_loop_extent", coli::expr(f_extent_2), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant g_s0_y_loop_min("g_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::constant g_s0_y_loop_extent("g_s0_y_loop_extent", coli::expr(g_extent_1), coli::p_int32, true, NULL, 0, &fusion_coli);
-
-    // Define loop bounds for dimension "g_s0_x".
+    coli::constant g_s0_y_loop_extent("g_s0_y_loop_extent", coli::expr(f_extent_1), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::constant g_s0_x_loop_min("g_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
-    coli::constant g_s0_x_loop_extent("g_s0_x_loop_extent", coli::expr(g_extent_0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant g_s0_x_loop_extent("g_s0_x_loop_extent", coli::expr(f_extent_0), coli::p_int32, true, NULL, 0, &fusion_coli);
     coli::computation g_s0("[g_s0_c_loop_min, g_s0_c_loop_extent, g_s0_y_loop_min, g_s0_y_loop_extent, g_s0_x_loop_min, g_s0_x_loop_extent]->{g_s0[g_s0_c, g_s0_y, g_s0_x]: "
                         "(g_s0_c_loop_min <= g_s0_c <= ((g_s0_c_loop_min + g_s0_c_loop_extent) + -1)) and (g_s0_y_loop_min <= g_s0_y <= ((g_s0_y_loop_min + g_s0_y_loop_extent) + -1)) and (g_s0_x_loop_min <= g_s0_x <= ((g_s0_x_loop_min + g_s0_x_loop_extent) + -1))}",
                         (coli::expr((uint8_t)2) * input(coli::idx("g_s0_c"), coli::idx("g_s0_y"), coli::idx("g_s0_x"))), true, coli::p_uint8, &fusion_coli);
     g_s0.set_access("{g_s0[g_s0_c, g_s0_y, g_s0_x]->buff_g[g_s0_c, g_s0_y, g_s0_x]}");
 
-    // Define compute level for "g".
-    g_s0.after(f_s0, computation::root_dimension);
 
-    // Add schedules.
 
-    fusion_coli.set_arguments({&buff_input, &buff_f, &buff_g});
+    // Loop over h
+    coli::constant h_s0_c_loop_min("h_s0_c_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant h_s0_c_loop_extent("h_s0_c_loop_extent", coli::expr(f_extent_2), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant h_s0_y_loop_min("h_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant h_s0_y_loop_extent("h_s0_y_loop_extent", coli::expr(f_extent_1), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant h_s0_x_loop_min("h_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant h_s0_x_loop_extent("h_s0_x_loop_extent", coli::expr(f_extent_0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::computation h_s0("[h_s0_c_loop_min, h_s0_c_loop_extent, h_s0_y_loop_min, h_s0_y_loop_extent, h_s0_x_loop_min, h_s0_x_loop_extent]->{h_s0[h_s0_c, h_s0_y, h_s0_x]: "
+                        "(h_s0_c_loop_min <= h_s0_c <= ((h_s0_c_loop_min + h_s0_c_loop_extent) + -1)) and (h_s0_y_loop_min <= h_s0_y <= ((h_s0_y_loop_min + h_s0_y_loop_extent) + -1)) and (h_s0_x_loop_min <= h_s0_x <= ((h_s0_x_loop_min + h_s0_x_loop_extent) + -1))}",
+                        (coli::expr((uint8_t)2) * input(coli::idx("h_s0_c"), coli::idx("h_s0_y"), coli::idx("h_s0_x"))), true, coli::p_uint8, &fusion_coli);
+    h_s0.set_access("{h_s0[h_s0_c, h_s0_y, h_s0_x]->buff_h[h_s0_c, h_s0_y, h_s0_x]}");
+
+
+
+    coli::constant k_s0_c_loop_min("k_s0_c_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant k_s0_c_loop_extent("k_s0_c_loop_extent", coli::expr(f_extent_2), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant k_s0_y_loop_min("k_s0_y_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant k_s0_y_loop_extent("k_s0_y_loop_extent", coli::expr(f_extent_1), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant k_s0_x_loop_min("k_s0_x_loop_min", coli::expr((int32_t)0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::constant k_s0_x_loop_extent("k_s0_x_loop_extent", coli::expr(f_extent_0), coli::p_int32, true, NULL, 0, &fusion_coli);
+    coli::computation k_s0("[k_s0_c_loop_min, k_s0_c_loop_extent, k_s0_y_loop_min, k_s0_y_loop_extent, k_s0_x_loop_min, k_s0_x_loop_extent]->{k_s0[k_s0_c, k_s0_y, k_s0_x]: "
+                         "(k_s0_c_loop_min <= k_s0_c <= ((k_s0_c_loop_min + k_s0_c_loop_extent) + -1)) and (k_s0_y_loop_min <= k_s0_y <= ((k_s0_y_loop_min + k_s0_y_loop_extent) + -1)) and (k_s0_x_loop_min <= k_s0_x <= ((k_s0_x_loop_min + k_s0_x_loop_extent) + -1))}",
+                         (coli::expr((uint8_t)2) * input(coli::idx("k_s0_c"), coli::idx("k_s0_y"), coli::idx("k_s0_x"))), true, coli::p_uint8, &fusion_coli);
+    k_s0.set_access("{k_s0[k_s0_c, k_s0_y, k_s0_x]->buff_k[k_s0_c, k_s0_y, k_s0_x]}");
+
+
+
+    fusion_coli.set_context_set("[f_s0_c_loop_min, f_s0_c_loop_extent, f_s0_y_loop_min, f_s0_y_loop_extent, f_s0_x_loop_min, f_s0_x_loop_extent, g_s0_c_loop_min, g_s0_c_loop_extent, g_s0_y_loop_min, g_s0_y_loop_extent, g_s0_x_loop_min, g_s0_x_loop_extent, h_s0_c_loop_min, h_s0_c_loop_extent, h_s0_y_loop_min, h_s0_y_loop_extent, h_s0_x_loop_min, h_s0_x_loop_extent, k_s0_c_loop_min, k_s0_c_loop_extent, k_s0_y_loop_min, k_s0_y_loop_extent, k_s0_x_loop_min, k_s0_x_loop_extent]->{: k_s0_x_loop_extent=f_s0_x_loop_extent and k_s0_x_loop_min=f_s0_x_loop_min and k_s0_y_loop_extent=f_s0_y_loop_extent and k_s0_y_loop_min=f_s0_y_loop_min and k_s0_c_loop_extent=f_s0_c_loop_extent and k_s0_c_loop_min=f_s0_c_loop_min and         h_s0_x_loop_extent=f_s0_x_loop_extent and h_s0_x_loop_min=f_s0_x_loop_min and h_s0_y_loop_extent=f_s0_y_loop_extent and h_s0_y_loop_min=f_s0_y_loop_min and h_s0_c_loop_extent=f_s0_c_loop_extent and f_s0_c_loop_min=h_s0_c_loop_min and f_s0_c_loop_min=g_s0_c_loop_min and f_s0_c_loop_extent=g_s0_c_loop_extent and f_s0_y_loop_min=g_s0_y_loop_min and f_s0_y_loop_extent=g_s0_y_loop_extent and f_s0_c_loop_min>=0 and f_s0_c_loop_extent>0}");
+
+    f_s0.set_schedule("[f_s0_c_loop_min, f_s0_c_loop_extent, f_s0_y_loop_min, f_s0_y_loop_extent, f_s0_x_loop_min, f_s0_x_loop_extent,    g_s0_c_loop_min, g_s0_c_loop_extent, g_s0_y_loop_min, g_s0_y_loop_extent, g_s0_x_loop_min, g_s0_x_loop_extent,    h_s0_c_loop_min, h_s0_c_loop_extent, h_s0_y_loop_min, h_s0_y_loop_extent, h_s0_x_loop_min, h_s0_x_loop_extent,   k_s0_c_loop_min, k_s0_c_loop_extent, k_s0_y_loop_min, k_s0_y_loop_extent, k_s0_x_loop_min, k_s0_x_loop_extent]->{f_s0[f_s0_c, f_s0_y, f_s0_x] -> f_s0[0, f_s0_c, 0, f_s0_y, 0, f_s0_x, 0]}");
+    g_s0.set_schedule("[f_s0_c_loop_min, f_s0_c_loop_extent, f_s0_y_loop_min, f_s0_y_loop_extent, f_s0_x_loop_min, f_s0_x_loop_extent,    g_s0_c_loop_min, g_s0_c_loop_extent, g_s0_y_loop_min, g_s0_y_loop_extent, g_s0_x_loop_min, g_s0_x_loop_extent,    h_s0_c_loop_min, h_s0_c_loop_extent, h_s0_y_loop_min, h_s0_y_loop_extent, h_s0_x_loop_min, h_s0_x_loop_extent,   k_s0_c_loop_min, k_s0_c_loop_extent, k_s0_y_loop_min, k_s0_y_loop_extent, k_s0_x_loop_min, k_s0_x_loop_extent]->{g_s0[g_s0_c, g_s0_y, g_s0_x] -> g_s0[0, g_s0_c, 0, g_s0_y, 0, g_s0_x, 1]}");
+    h_s0.set_schedule("[f_s0_c_loop_min, f_s0_c_loop_extent, f_s0_y_loop_min, f_s0_y_loop_extent, f_s0_x_loop_min, f_s0_x_loop_extent,    g_s0_c_loop_min, g_s0_c_loop_extent, g_s0_y_loop_min, g_s0_y_loop_extent, g_s0_x_loop_min, g_s0_x_loop_extent,    h_s0_c_loop_min, h_s0_c_loop_extent, h_s0_y_loop_min, h_s0_y_loop_extent, h_s0_x_loop_min, h_s0_x_loop_extent,   k_s0_c_loop_min, k_s0_c_loop_extent, k_s0_y_loop_min, k_s0_y_loop_extent, k_s0_x_loop_min, k_s0_x_loop_extent]->{h_s0[h_s0_c, h_s0_y, h_s0_x] -> h_s0[0, h_s0_c, 0, h_s0_y, 0, h_s0_x, 2]}");
+    k_s0.set_schedule("[f_s0_c_loop_min, f_s0_c_loop_extent, f_s0_y_loop_min, f_s0_y_loop_extent, f_s0_x_loop_min, f_s0_x_loop_extent,    g_s0_c_loop_min, g_s0_c_loop_extent, g_s0_y_loop_min, g_s0_y_loop_extent, g_s0_x_loop_min, g_s0_x_loop_extent,    h_s0_c_loop_min, h_s0_c_loop_extent, h_s0_y_loop_min, h_s0_y_loop_extent, h_s0_x_loop_min, h_s0_x_loop_extent,   k_s0_c_loop_min, k_s0_c_loop_extent, k_s0_y_loop_min, k_s0_y_loop_extent, k_s0_x_loop_min, k_s0_x_loop_extent]->{k_s0[k_s0_c, k_s0_y, k_s0_x] -> k_s0[0, k_s0_c, 0, k_s0_y, 0, k_s0_x, 3]}");
+    f_s0.tag_parallel_dimension(0);
+    f_s0.tag_parallel_dimension(1);
+    g_s0.tag_parallel_dimension(0);
+    g_s0.tag_parallel_dimension(1);
+    h_s0.tag_parallel_dimension(0);
+    h_s0.tag_parallel_dimension(1);
+    k_s0.tag_parallel_dimension(0);
+    k_s0.tag_parallel_dimension(1);
+    f_s0.tag_vector_dimension(2);
+    g_s0.tag_vector_dimension(2);
+    f_s0.tag_vector_dimension(3);
+    g_s0.tag_vector_dimension(3);
+
+
+    fusion_coli.set_arguments({&buff_input, &buff_f, &buff_g, &buff_h, &buff_k});
     fusion_coli.gen_time_processor_domain();
     fusion_coli.gen_isl_ast();
     fusion_coli.gen_halide_stmt();
--- benchmarks/halide/fusion_ref.cpp
+++ benchmarks/halide/fusion_ref.cpp
@@ -6,20 +6,24 @@ int main(int argc, char **argv) {
     ImageParam in(UInt(8), 3, "input");
 
     Var x("x"), y("y"), c("c");
-    Func f("f"), g("g");
+    Func f("f"), g("g"), h("h"), k("k");
 
     f(x, y, c) = cast<uint8_t>(255 - in(x, y, c));
     g(x, y, c) = cast<uint8_t>(2*in(x, y, c));
+    h(x, y, c) = f(x, y, c) + g(x, y, c);
+    k(x, y, c) = f(x, y, c) - g(x, y, c);
 
     //g.compute_with(f, y);
-    f.parallel(y).vectorize(x, 8).parallel(c);
-    g.parallel(y).vectorize(x, 8).parallel(c);
+    f.parallel(y).parallel(c).vectorize(x, 8).
+    g.parallel(y).parallel(c).vectorize(x, 8).
+    h.parallel(y).parallel(c);
+    k.parallel(y).parallel(c);
 
     Halide::Target target = Halide::get_host_target();
 
-    Pipeline({f, g}).compile_to_object("build/generated_fct_fusion_ref.o", {in}, "fusion_ref", target);
+    Pipeline({f, g, h, k}).compile_to_object("build/generated_fct_fusion_ref.o", {in}, "fusion_ref", target);
 
-    Pipeline({f, g}).compile_to_lowered_stmt("build/generated_fct_fusion_ref.txt", {in}, Halide::Text, target);
+    Pipeline({f, g, h, k}).compile_to_lowered_stmt("build/generated_fct_fusion_ref.txt", {in}, Halide::Text, target);
 
     return 0;
 }
