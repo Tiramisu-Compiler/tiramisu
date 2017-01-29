18c2b05da512949b9ccfc8b7c08fe0723682cec8
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2017-01-29 11:27:46 +0100

    Fix recfilter

--- benchmarks/halide/recfilter_ref.cpp
+++ benchmarks/halide/recfilter_ref.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
     rec_filter(x, y, c) = in(x, y, c);
     rec_filter(r.x, r.y, c) = cast<uint8_t>(a0*rec_filter(r.x, r.y, c) + a1*rec_filter(r.x-1, r.y, c) + a2*rec_filter(r.x-2, r.y, c));
 
-    rec_filter.parallel(y).parallel(c).vectorize(x, 8).
+    rec_filter.parallel(y).parallel(c).vectorize(x, 8);
     rec_filter.update(0).parallel(r.y).parallel(c);
 
     rec_filter.compile_to_object("build/generated_fct_recfilter_ref.o", {in}, "recfilter_ref");
