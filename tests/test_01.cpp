455d94665836e36ca24f2b0e9cc2298d7784ca8c
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2017-02-04 15:55:33 +0100

    Adjust test01

--- tests/test_01.cpp
+++ tests/test_01.cpp
@@ -18,15 +18,11 @@ void generate_function_1(std::string name, int size, int val0, int val1)
     tiramisu::global::set_default_tiramisu_options();
 
     tiramisu::function function0(name);
-    tiramisu::expr e_N = tiramisu::expr((int32_t) size);
-    tiramisu::constant N("N", e_N, p_int32, true, NULL, 0, &function0);
-    tiramisu::expr e1 = tiramisu::expr(tiramisu::o_add,
-                               tiramisu::expr((uint8_t) val0),
-                               tiramisu::expr((uint8_t) val1));
+    tiramisu::constant N("N", tiramisu::expr((int32_t) size), p_int32, true, NULL, 0, &function0);
+    tiramisu::expr e1 = tiramisu::expr(tiramisu::o_add, tiramisu::expr((uint8_t) val0), tiramisu::expr((uint8_t) val1));
     tiramisu::computation S0("[N]->{S0[i,j]: 0<=i<N and 0<=j<N}", e1, true, p_uint8, &function0);
 
-    tiramisu::buffer buf0("buf0", 2, {size,size}, tiramisu::p_uint8, NULL,
-                        a_output, &function0);
+    tiramisu::buffer buf0("buf0", 2, {size,size}, tiramisu::p_uint8, NULL, a_output, &function0);
 
     S0.set_access("{S0[i,j]->buf0[i,j]}");
     S0.tile(1,3,2,2);
