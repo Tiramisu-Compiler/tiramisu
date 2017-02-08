757718895b7700cb89e290d70631e34e67e9be19
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2017-02-08 16:52:48 +0100

    Add experimental version for the vectorization of test_10

--- tests/test_10.cpp
+++ tests/test_10.cpp
@@ -22,20 +22,28 @@ void generate_function_1(std::string name, int size, int val0)
     tiramisu::constant N("N", tiramisu::expr((int32_t) size), p_int32, true, NULL, 0, &function0);
     tiramisu::constant M("M", tiramisu::expr((int32_t) Msize), p_int32, true, NULL, 0, &function0);
 
+    tiramisu::buffer buf0("buf0", 2, {size,size}, tiramisu::p_uint8, NULL, a_output, &function0);
+
     tiramisu::expr e1 = tiramisu::expr((uint8_t) val0);
+
+#if 0
+    tiramisu::computation S0("[N,M]->{S0[i,j]: 0<=i<N and 0<=j<N}", e1, true, p_uint8, &function0);
+    S0.set_access("[N,M]->{S0[i,j]->buf0[i,j]: 0<=i<N and 0<=j<N}");
+    S0.vectorize(1, 2, tiramisu::var(p_int32, "N"));
+#else
     tiramisu::expr e2 = tiramisu::expr((uint8_t) val0);
     tiramisu::computation S0("[N,M]->{S0[i,j]: 0<=i<N and 0<=j<M}", e1, true, p_uint8, &function0);
     tiramisu::computation S0P("[N,M]->{S0P[i,j]: 0<=i<N and M<=j<N}", e2, true, p_uint8, &function0);
 
-    tiramisu::buffer buf0("buf0", 2, {size,size}, tiramisu::p_uint8, NULL, a_output, &function0);
-
     function0.set_context_set("[N,M]->{:N>0 and M>0 and M%2=0 and M<=N}");
-    S0.set_access("[N,M]->{S0[i,j]->buf0[i,j]: 0<=i<N and 0<=j<N}");
-    S0.set_schedule("[N,M]->{S0[i,j]->S0[0,i,0,j1,j2,0]: 0<=i<N and 0<=j<M and j1=floor(j/2) and j2=j%2}"); // and M-j1*2>1
-    S0P.set_access("[N,M]->{S0P[i,j]->buf0[i,j]: 0<=i<N and 0<=j<N}");
-    S0P.set_schedule("[N,M]->{S0P[i,j]->S0P[1,i,0,j,0,0]: 0<=i<N and M<=j<N}");
-
+    S0.split(3,2);
     S0.tag_vector_dimension(2);
+
+    S0P.set_schedule("[N,M]->{S0P[i,j]->S0P[1,i,0,j,0,0,0]: 0<=i<N and M<=j<N}");
+
+    S0.set_access("[N,M]->{S0[i,j]->buf0[i,j]: 0<=i<N and 0<=j<N}");
+    S0P.set_access("[N,M]->{S0P[i,j]->buf0[i,j]: 0<=i<N and 0<=j<N}");
+#endif
 
     function0.set_arguments({&buf0});
     function0.gen_time_processor_domain();
@@ -43,24 +51,6 @@ void generate_function_1(std::string name, int size, int val0)
     function0.gen_halide_stmt();
     function0.gen_halide_obj("build/generated_fct_test_10.o");
 }
-
-/*
- * Vectorized:
- * for (c1, 0, 32) {
-    for (c2, 0, 1) {
-     for (c3, 0, 16) {
-      buf0[(c3 + (c1*32))] = (uint8)7
-     }
-    }
-   }
-
-   Unvectorized
-   for (c1, 0, 32) {
-    for (c3, 0, 32) {
-     buf0[(c3 + (c1*32))] = (uint8)7
-    }
-   }
- */
 
 int main(int argc, char **argv)
 {
