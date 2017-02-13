d6c1eaf01a94b36f6974624fbce8d321e35d991e
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2017-02-13 11:28:33 +0100

    Remove deprecated code from test10

--- tests/test_10.cpp
+++ tests/test_10.cpp
@@ -11,8 +11,6 @@
 #include <string.h>
 #include <Halide.h>
 
-#define AUTOMATIC_VECTORIATION 1
-
 using namespace tiramisu;
 
 void generate_function_1(std::string name, int size, int val0)
@@ -26,28 +24,10 @@ void generate_function_1(std::string name, int size, int val0)
 
     tiramisu::buffer buf0("buf0", 2, {size,size}, tiramisu::p_uint8, NULL, a_output, &function0);
 
-#if AUTOMATIC_VECTORIATION
     tiramisu::expr e1 = tiramisu::expr((uint8_t) val0);
     tiramisu::computation S0("[N,M]->{S0[i,j]: 0<=i<N and 0<=j<N}", e1, true, p_uint8, &function0);
     S0.set_access("[N,M]->{S0[i,j]->buf0[i,j]: 0<=i<N and 0<=j<N}");
     S0.vectorize(1, 2, tiramisu::var(p_int32, "N"));
-#else
-    function0.set_context_set("[N,M]->{:N>0 and M>0 and M%2=0 and M<=N}");
-
-    tiramisu::expr e1 = tiramisu::expr((uint8_t) val0);
-    tiramisu::expr e2 = tiramisu::expr((uint8_t) val0);
-
-    tiramisu::computation S0("[N,M]->{S0[i,j]: 0<=i<N and 0<=j<M}", e1, true, p_uint8, &function0);
-    tiramisu::computation S0P("[N,M]->{S0P[i,j]: 0<=i<N and M<=j<N}", e2, true, p_uint8, &function0);
-
-    S0.set_access("[N,M]->{S0[i,j]->buf0[i,j]: 0<=i<N and 0<=j<N}");
-    S0P.set_access("[N,M]->{S0P[i,j]->buf0[i,j]: 0<=i<N and 0<=j<N}");
-
-    S0.split(3,2);
-    S0.tag_vector_dimension(2);
-
-    S0P.set_schedule("[N,M]->{S0P[i,j]->S0P[1,i,0,j,0,0,0]: 0<=i<N and M<=j<N}");
-#endif
 
     function0.set_arguments({&buf0});
     function0.gen_time_processor_domain();
