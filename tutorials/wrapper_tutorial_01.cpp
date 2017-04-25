29384c0bb98391545972e7740c275705aaeee66a
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2017-04-25 10:29:37 +0100

    Fix tutorial 01

--- tutorials/wrapper_tutorial_01.cpp
+++ tutorials/wrapper_tutorial_01.cpp
@@ -9,19 +9,11 @@
 
 int main(int, char**)
 {
-    buffer_t input_buf = {0};
-    input_buf.host = (unsigned char *) malloc(NN*MM*sizeof(unsigned char));
-    input_buf.stride[0] = 1;
-    input_buf.stride[1] = 1;
-    input_buf.extent[0] = NN;
-    input_buf.extent[1] = MM;
-    input_buf.min[0] = 0;
-    input_buf.min[1] = 0;
-    input_buf.elem_size = 1;
+    buffer_t input_buf = allocate_2D_buffer(NN, MM);
+    init_2D_buffer_val(&input_buf, NN, MM, 9);
 
-    init_1D_buffer(&input_buf, NN*MM, 9);
     std::cout << "Array (after initialization)" << std::endl;
-    print_1D_buffer(&input_buf, NN*MM);
+    print_2D_buffer(&input_buf, NN, MM);
 
     Halide::Buffer<uint8_t> halide_input_buf(input_buf);
 
