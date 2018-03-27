1e6816d1f3dc140ac98ba3c8519e4e0736455e24
Malek Ben Romdhane <malekbr@gmail.com>
2018-03-27 12:34:54 -0400

    more helpful debugging for cuda wrappers

--- src/tiramisu_cuda_wrappers.cpp
+++ src/tiramisu_cuda_wrappers.cpp
@@ -9,11 +9,11 @@
 using size_type = uint64_t;
 
 namespace {
-    inline void handle_cuda_error(cudaError_t e)
+    inline void handle_cuda_error(cudaError_t e, const std::string & function_name)
     {
         if (e != cudaError_t::cudaSuccess)
         {
-            std::cerr << cudaGetErrorString(e) << std::endl;
+            std::cerr << "Error at " << function_name << ": " << cudaGetErrorString(e) << std::endl;
             exit(1);
         }
 
@@ -23,14 +23,14 @@ namespace {
 extern "C"
 int tiramisu_cuda_memcpy_to_device(void * to, void * from, uint64_t size)
 {
-   handle_cuda_error(cudaMemcpy(to, from, size, cudaMemcpyKind::cudaMemcpyHostToDevice));
+   handle_cuda_error(cudaMemcpy(to, from, size, cudaMemcpyKind::cudaMemcpyHostToDevice), __FUNCTION__);
    return 0;
 }
 
 extern "C"
 int tiramisu_cuda_memcpy_to_host(void * to, void * from, uint64_t size)
 {
-    handle_cuda_error(cudaMemcpy(to, from, size, cudaMemcpyKind::cudaMemcpyDeviceToHost));
+    handle_cuda_error(cudaMemcpy(to, from, size, cudaMemcpyKind::cudaMemcpyDeviceToHost), __FUNCTION__);
     return 0;
 }
 
@@ -38,13 +38,13 @@ extern "C"
 void * tiramisu_cuda_malloc(uint64_t size)
 {
     void * result;
-    handle_cuda_error(cudaMalloc(&result, size));
+    handle_cuda_error(cudaMalloc(&result, size), __FUNCTION__);
     return result;
 }
 
 extern "C"
 int tiramisu_cuda_free(void * ptr)
 {
-    handle_cuda_error(cudaFree(ptr));
+    handle_cuda_error(cudaFree(ptr), __FUNCTION__);
     return 0;
 }
