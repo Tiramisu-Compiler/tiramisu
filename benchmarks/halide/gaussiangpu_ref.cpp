2d28c391ce9ece32589ba3b797c50a66762771a2
Malek Ben Romdhane <malekbr@gmail.com>
2018-03-28 14:15:57 -0400

    fixed gaussiangpu halide again

--- benchmarks/halide/gaussiangpu_ref.cpp
+++ benchmarks/halide/gaussiangpu_ref.cpp
@@ -10,37 +10,37 @@ int main(int argc, char* argv[]) {
     ImageParam kernelX(Float(32), 1, "kernelx");
     ImageParam kernelY(Float(32), 1, "kernely");
 
-    Func gaussian_gpu("gaussian_gpu"), gaussian_gpu_x("gaussian_gpu_x");
+    Func gaussiangpu("gaussiangpu"), gaussiangpu_x("gaussiangpu_x");
     Var x("x"), y("y"), c("c");
 
     Expr e = 0.0f;
     for (int i = 0; i < 5; ++i) {
         e += in(x + i, y, c) * kernelX(i);
     }
-    gaussian_gpu_x(x, y, c) = e;
+    gaussiangpu_x(x, y, c) = e;
 
     Expr f = 0.0f;
     for (int j = 0; j < 5; ++j) {
-        f += gaussian_gpu_x(x, y + j, c) * kernelY(j);
+        f += gaussiangpu_x(x, y + j, c) * kernelY(j);
     }
-    gaussian_gpu(x, y, c) = cast<uint8_t>(f);
+    gaussiangpu(x, y, c) = cast<uint8_t>(f);
 
     Var x_inner, y_inner, x_outer, y_outer, tile_index;
-//    gaussian_gpu.tile(x, y, x_outer, y_outer, x_inner, y_inner, 8, 8)
+//    gaussiangpu.tile(x, y, x_outer, y_outer, x_inner, y_inner, 8, 8)
 //            .fuse(x_outer, y_outer, tile_index)
 //            .compute_root();
 //            .parallel(x_outer);
-    gaussian_gpu_x.compute_root();
+    gaussiangpu_x.compute_root();
 
-    gaussian_gpu_x.gpu_tile(x, y, x_inner, y_inner, x_outer, y_outer, 16, 16);
-    gaussian_gpu.gpu_tile(x, y, x_inner, y_inner, x_outer, y_outer, 16, 16);
+    gaussiangpu_x.gpu_tile(x, y, x_inner, y_inner, x_outer, y_outer, 16, 16);
+    gaussiangpu.gpu_tile(x, y, x_inner, y_inner, x_outer, y_outer, 16, 16);
 
     Halide::Target target = Halide::get_host_target();
     target.set_feature(Target::Feature::CUDA, true);
 
-    gaussian_gpu.compile_to_object("build/generated_fct_gaussian_gpu_ref.o", {in, kernelX, kernelY}, "gaussian_gpu_ref", target);
+    gaussiangpu.compile_to_object("build/generated_fct_gaussiangpu_ref.o", {in, kernelX, kernelY}, "gaussiangpu_ref", target);
 
-    gaussian_gpu.compile_to_lowered_stmt("build/generated_fct_gaussian_gpu_ref.txt", {in, kernelX, kernelY}, Text, target);
+    gaussiangpu.compile_to_lowered_stmt("build/generated_fct_gaussiangpu_ref.txt", {in, kernelX, kernelY}, Text, target);
 
   return 0;
 }
