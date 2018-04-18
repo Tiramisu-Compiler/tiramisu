a81d14ad0af5ff94d4fedb56695bf65dc96afdd5
Jessica Ray <jray@csail.mit.edu>
2018-04-18 09:59:09 -0400

    Add a simple distributed test that does no communication.

---
+++ tests/wrapper_test_98.cpp
@@ -0,0 +1,35 @@
+#include "wrapper_test_98.h"
+#include "Halide.h"
+
+#include <tiramisu/utils.h>
+#include <tiramisu/mpi_comm.h>
+#include <cstdlib>
+#include <iostream>
+
+
+int main(int, char **)
+{
+    int rank = tiramisu_MPI_init();
+
+    Halide::Buffer<int> input({100,100});
+    Halide::Buffer<int> output({100,100});
+    init_buffer(input, 2);
+
+    Halide::Buffer<int> ref_output({100,100});
+    for (int x = 0; x < 100; x++) {
+        for (int y = 0; y < 100; y++) {
+            ref_output(x,y) = input(x,y);
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        for (int x = 0; x < 100; x++) {
+            for (int y = 0; y < 100; y++) {
+                ref_output(x,y) *= 2;
+            }
+        }
+    }
+    compare_buffers(TEST_NAME_STR, output, ref_output);
+
+    tiramisu_MPI_cleanup();
+    return 0;
+}
