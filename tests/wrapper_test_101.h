c119585b2458c2c1a80d505dadad2b46e2bf618d
Jessica Ray <jray@csail.mit.edu>
2018-04-19 16:09:53 -0400

    Add distributed reduction test w/ computation AND communication

--- tests/wrapper_test_101.h
+++ tests/wrapper_test_101.h
@@ -1,9 +1,9 @@
-#ifndef TIRAMISU_WRAPPER_TEST_100_H
-#define TIRAMISU_WRAPPER_TEST_100_H
+#ifndef TIRAMISU_WRAPPER_TEST_101_H
+#define TIRAMISU_WRAPPER_TEST_101_H
 
 // Define these values for each new test
-#define TEST_NAME_STR       "Distributed communication only (non-blocking)"
-#define TEST_NUMBER_STR     "100"
+#define TEST_NAME_STR       "Distributed reduction"
+#define TEST_NUMBER_STR     "101"
 
 // --------------------------------------------------------
 // No need to modify anything in the following ------------
@@ -14,12 +14,12 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-int dist_comm_only_nonblock(halide_buffer_t *);
-int dist_comm_only_nonblock_argv(void **args);
+int dist_reduction(halide_buffer_t *, halide_buffer_t *);
+int dist_reduction_argv(void **args);
 
 extern const struct halide_filter_metadata_t halide_pipeline_aot_metadata;
 #ifdef __cplusplus
 }  // extern "C"
+#endif
 
-
-#endif //TIRAMISU_WRAPPER_TEST_100_H
+#endif //TIRAMISU_WRAPPER_TEST_101_H
