bbcc438a6a1a68f0cd5af5d1e43abd089a0c72e0
Jessica Ray <jray@csail.mit.edu>
2018-05-25 12:43:09 -0400

    Add additional address_of functions for other types.

--- include/tiramisu/tiramisu_externs.h
+++ include/tiramisu/tiramisu_externs.h
@@ -5,6 +5,14 @@
 
 extern "C" {
 
+int8_t *tiramisu_address_of_int8(halide_buffer_t *buffer, unsigned long index);
+
+int16_t *tiramisu_address_of_int16(halide_buffer_t *buffer, unsigned long index);
+
+int32_t *tiramisu_address_of_int32(halide_buffer_t *buffer, unsigned long index);
+
+int64_t *tiramisu_address_of_int64(halide_buffer_t *buffer, unsigned long index);
+
 float *tiramisu_address_of_float32(halide_buffer_t *buffer, unsigned long index);
 
 double *tiramisu_address_of_float64(halide_buffer_t *buffer, unsigned long index);
--- src/tiramisu_externs.cpp
+++ src/tiramisu_externs.cpp
@@ -2,6 +2,22 @@
 
 extern "C" {
 
+int8_t *tiramisu_address_of_int8(halide_buffer_t *buffer, unsigned long index) {
+    return &(((int8_t*)(buffer->host))[index]);
+}
+
+int16_t *tiramisu_address_of_int16(halide_buffer_t *buffer, unsigned long index) {
+    return &(((int16_t*)(buffer->host))[index]);
+}
+
+int32_t *tiramisu_address_of_int32(halide_buffer_t *buffer, unsigned long index) {
+    return &(((int32_t*)(buffer->host))[index]);
+}
+
+int64_t *tiramisu_address_of_int64(halide_buffer_t *buffer, unsigned long index) {
+    return &(((int64_t*)(buffer->host))[index]);
+}
+
 float *tiramisu_address_of_float32(halide_buffer_t *buffer, unsigned long index) {
     return &(((float*)(buffer->host))[index]);
 }
