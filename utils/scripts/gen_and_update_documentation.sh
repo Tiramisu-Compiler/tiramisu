d7350637a24c5cd56ce44c69f9c27bde466378f0
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-06-13 21:33:04 -0400

    update doc building

---
+++ utils/scripts/gen_and_update_documentation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: gen_and_update_documentation.sh <TIRAMISU_ROOT_PATH> <TIRAMISU_WEBSITE_ROOT_PATH>"
+	exit 1
+fi
+
+PROJECT_SRC_DIR=$1
+WEBSITE_DIR=$2
+
+set -e
+. ${PROJECT_SRC_DIR}/utils/scripts/functions.sh
+
+echo "### Generate new documentation, copy the generated documentation to the website. ####"
+
+echo_and_run_cmd "cd ${PROJECT_SRC_DIR}/build/"
+echo_and_run_cmd "make doc"
+echo_and_run_cmd "cp -r doc/* ${WEBSITE_DIR}/doc/"
