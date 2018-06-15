fc2b733069d5d6fec5c16fc4b6690723ddb5602e
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-06-15 13:12:25 -0400

    update tutorials

--- tutorials/developers/tutorial_09/tutorial_09.cpp
+++ tutorials/developers/tutorial_09/tutorial_09.cpp
@@ -96,7 +96,7 @@ void generate_function(std::string name, int size, int val0)
     function0.gen_isl_ast();
     function0.gen_halide_stmt();
     function0.gen_c_code();
-    function0.gen_halide_obj("build/generated_fct_tutorial_09.o");
+    function0.gen_halide_obj("build/generated_fct_developers_tutorial_09.o");
 }
 
 int main(int argc, char **argv)
