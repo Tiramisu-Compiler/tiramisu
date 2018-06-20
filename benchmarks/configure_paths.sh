8b030e75fb46a605d19e8683f1e04fd7a709e0b9
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-06-20 11:31:16 -0400

    Add configure_paths.sh back

---
+++ benchmarks/configure_paths.sh
@@ -0,0 +1,5 @@
+ISL_INCLUDE_DIRECTORY=3rdParty/isl/build/include/
+ISL_LIB_DIRECTORY=3rdParty/isl/build/lib/
+HALIDE_SOURCE_DIRECTORY=3rdParty/Halide
+HALIDE_LIB_DIRECTORY=3rdParty/Halide/lib
+MKL_PREFIX=/opt/intel/compilers_and_libraries/mac/mkl/
