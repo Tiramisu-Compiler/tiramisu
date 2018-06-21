3571adb4d552426ddb32ea1d55237148e7686142
Riyadh Baghdadi <baghdadi@lanka45-eth.csail.mit.edu>
2018-06-21 16:30:15 -0400

    update configure_paths.sh

--- benchmarks/configure_paths.sh
+++ benchmarks/configure_paths.sh
@@ -2,4 +2,6 @@ ISL_INCLUDE_DIRECTORY=3rdParty/isl/build/include/
 ISL_LIB_DIRECTORY=3rdParty/isl/build/lib/
 HALIDE_SOURCE_DIRECTORY=3rdParty/Halide
 HALIDE_LIB_DIRECTORY=3rdParty/Halide/lib
-MKL_PREFIX=/opt/intel/compilers_and_libraries/mac/mkl/
+#MKL_PREFIX=/data/scratch/baghdadi/libs/intel/mkl/
+#MKL_PREFIX=/opt/intel/compilers_and_libraries/mac/mkl/
+MKL_PREFIX=
