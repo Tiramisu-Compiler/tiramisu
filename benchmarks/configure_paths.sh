95bff90f17713a168b74c6b2692df78387f3f928
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-04 18:49:10 -0400

    Add CORES to configuration scripts

--- benchmarks/configure_paths.sh
+++ benchmarks/configure_paths.sh
@@ -1,12 +1,13 @@
 LANKA=0
 
 if [ $LANKA -eq 0 ]; then
+	TIRAMISU_ROOT=/Users/b/Documents/src/MIT/tiramisu/
 	MKL_FLAGS="-lcblas"
 	MKL_LIB_PATH_SUFFIX=
 	LANKA_OPTIONS=
 	USE_PERF=0
 	MKL_PREFIX=/opt/intel/compilers_and_libraries/mac/mkl/
-	TIRAMISU_ROOT=/Users/b/Documents/src/MIT/tiramisu/
+	CORES=4
 else
 	TIRAMISU_ROOT=/data/scratch/baghdadi/tiramisu/
 	MKL_FLAGS="-lmkl_intel_ilp64 -lmkl_gnu_thread -lmkl_core -lgomp -ldl -lm"
@@ -14,7 +15,7 @@ else
 	LANKA_OPTIONS="-DMKL_ILP64 -m64 -fopenmp"
 	USE_PERF=1
 	MKL_PREFIX=/data/scratch/baghdadi/libs/intel/mkl/
-	TIRAMISU_ROOT=/data/scratch/baghdadi/tiramisu/
+	CORES=32
 fi
 
 ISL_INCLUDE_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/isl/build/include/
