f355e7f23b6a57ab02955f891c6f9d36db556417
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-05 11:29:51 -0400

    Create baryong wrapper

--- benchmarks/tensors/baryon/baryon_wrapper.cpp
+++ benchmarks/tensors/baryon/baryon_wrapper.cpp
@@ -2,6 +2,7 @@
 #include <tiramisu/utils.h>
 #include <cstdlib>
 #include <iostream>
+#include "benchmarks.h"
 
 #include "baryon_wrapper.h"
 
@@ -14,26 +15,71 @@ extern "C" {
 #endif
 
 
+void ref(Halide::Buffer<float> &Res2, Halide::Buffer<float> &S, Halide::Buffer<float> &wp)
+{
+  int c1 = 0, c2 = 0, c3 = 0, t = 0, a1 = 0, a2 = 0, a3 = 0, xp0 = 0, b0 = 0, b1 = 0, b2 = 0;
+
+  Res2(0) = 0;
+  Halide::Buffer<float> Res0(1, "Res0");
+  Halide::Buffer<float> Res1(1, "Res1");
+
+  for (int x0 = 0; x0 < BARYON_N; x0++)
+    for (int x1 = 0; x1 < BARYON_N; x1++)
+      for (int x2 = 0; x2 < BARYON_N; x2++)
+       {
+         Res0(0) = S(c1, x0, x1, x2, t, a1, xp0) * S(c2, x0, x1, x2, t, a2, xp0) * S(c3, x0, x1, x2, t, a3, xp0)
+                  +S(c2, x0, x1, x2, t, a1, xp0) * S(c3, x0, x1, x2, t, a2, xp0) * S(c1, x0, x1, x2, t, a3, xp0)
+                  +S(c3, x0, x1, x2, t, a1, xp0) * S(c1, x0, x1, x2, t, a2, xp0) * S(c2, x0, x1, x2, t, a3, xp0)
+                  -S(c2, x0, x1, x2, t, a1, xp0) * S(c1, x0, x1, x2, t, a2, xp0) * S(c3, x0, x1, x2, t, a3, xp0)
+                  -S(c3, x0, x1, x2, t, a1, xp0) * S(c2, x0, x1, x2, t, a2, xp0) * S(c1, x0, x1, x2, t, a3, xp0)
+                  -S(c1, x0, x1, x2, t, a1, xp0) * S(c3, x0, x1, x2, t, a2, xp0) * S(c2, x0, x1, x2, t, a3, xp0);
+
+         Res1(0) = 0;
+         for (int k = 1; k <= BARYON_N; k++)
+           Res1(0) = Res1(0) + wp(c1, c2, c3, b0, b1, b2, k) * Res0(0);
+
+         Res2(0) = Res2(0) + Res1(0);
+       }
+}
+
 int main(int, char **)
 {
-    Halide::Buffer<float> a(1, "a");
-    Halide::Buffer<float> x(SIZE, "x");
-    Halide::Buffer<float> y_ref(SIZE, "y_ref");
-    Halide::Buffer<float> y(SIZE, "y");
+    std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
+    std::vector<std::chrono::duration<double,std::milli>> duration_vector_2;
 
-    init_buffer(x, (float)1);
-    init_buffer(y, (float)1);
-    init_buffer(y_ref, (float)1);
-    init_buffer(a, (float)1);
+    Halide::Buffer<float> buf_res2(1, "buf_res2");
+    Halide::Buffer<float> buf_res2_ref(1, "buf_res2_ref");
+    Halide::Buffer<float> buf_S(BARYON_P, BARYON_N, BARYON_N, BARYON_N, BARYON_P, BARYON_P,  BARYON_P, "buf_S");
+    Halide::Buffer<float> buf_wp(BARYON_P, BARYON_P, BARYON_P, BARYON_P, BARYON_P, BARYON_P, BARYON_N, "buf_wp");
 
-    tiramisu_timer timer;
+    init_buffer(buf_S, (float)1);
+    init_buffer(buf_wp, (float)1);
 
-    timer.start();
-    tiramisu_generated_code(a.raw_buffer(), x.raw_buffer(), y.raw_buffer());
-    timer.stop();
-    timer.print("Tiramisu baryon");
+    for (int i = 0; i < NB_TESTS; i++)
+    {
+    	    init_buffer(buf_res2_ref, (float)0);
+	    auto start2 = std::chrono::high_resolution_clock::now();
+	    ref(buf_res2_ref, buf_S, buf_wp);
+	    auto end2 = std::chrono::high_resolution_clock::now();
+	    std::chrono::duration<double,std::milli> duration2 = end2 - start2;
+	    duration_vector_2.push_back(duration2);
+    }
 
-    compare_buffers("benchmark_" + std::string(TEST_NAME_STR), y, y_ref);
+    for (int i = 0; i < NB_TESTS; i++)
+    {
+	    init_buffer(buf_res2, (float)0);
+	    auto start1 = std::chrono::high_resolution_clock::now();
+	    tiramisu_generated_code(buf_res2.raw_buffer(), buf_S.raw_buffer(), buf_wp.raw_buffer());
+	    auto end1 = std::chrono::high_resolution_clock::now();
+	    std::chrono::duration<double,std::milli> duration1 = end1 - start1;
+	    duration_vector_1.push_back(duration1);
+    }
+
+    compare_buffers("benchmark_" + std::string(TEST_NAME_STR), buf_res2, buf_res2_ref);
+
+    print_time("performance_CPU.csv", "baryon",
+               {"Ref", "Tiramisu"},
+               {median(duration_vector_2), median(duration_vector_1)});
 
     return 0;
 }
