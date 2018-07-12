96b6ff0db427909e24457be9680f8a28919f452e
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-11 22:32:41 -0400

    Update DNN conv benchmark

---
+++ benchmarks/DNN/conv/compile_tiramisu_code.sh
@@ -0,0 +1,18 @@
+#set -x
+
+if [ $# -eq 0 ]; then
+      echo "Usage: TIRAMISU_SMALL=1 script.sh <KERNEL_FOLDER>"
+      echo "Example: script.sh axpy"
+      exit
+fi
+
+source ../../configure_paths.sh
+
+s=$1
+b=${s%.*}
+
+CXXFLAGS="-std=c++11 -O3"
+
+echo "Compiling conv_layer_generator_tiramisu.cpp"
+
+g++ $CXXFLAGS ${INCLUDES} $s -ltiramisu ${LIBRARIES_DIR} ${LIBRARIES} -o $b
