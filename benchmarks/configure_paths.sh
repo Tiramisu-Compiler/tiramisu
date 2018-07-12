20b4b3cf314a4e002aa52fee35f2eaf4d6968284
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-11 22:34:28 -0400

    Update DNN conv benchmark

--- benchmarks/configure_paths.sh
+++ benchmarks/configure_paths.sh
@@ -1,25 +1,25 @@
 LANKA=0
 
 if [ $LANKA -eq 0 ]; then
-	TIRAMISU_ROOT=/Users/b/Documents/src/MIT/tiramisu/
-	MKL_FLAGS="-lcblas"
-	MKL_LIB_PATH_SUFFIX=
-	LANKA_OPTIONS=
-	USE_PERF=0
-	MKL_PREFIX=/opt/intel/compilers_and_libraries/mac/mkl/
-	CORES=4
+	export TIRAMISU_ROOT=/Users/b/Documents/src/MIT/tiramisu/
+	export MKL_FLAGS="-lcblas"
+	export MKL_LIB_PATH_SUFFIX=
+	export LANKA_OPTIONS=
+	export USE_PERF=0
+	export MKL_PREFIX=/opt/intel/compilers_and_libraries/mac/mkl/
+	export CORES=4
 else
-	TIRAMISU_ROOT=/data/scratch/baghdadi/tiramisu/
-	MKL_FLAGS="-lmkl_intel_ilp64 -lmkl_gnu_thread -lmkl_core -lgomp -ldl -lm"
-	MKL_LIB_PATH_SUFFIX=intel64/
-	LANKA_OPTIONS="-DMKL_ILP64 -m64 -fopenmp"
-	USE_PERF=1
-	MKL_PREFIX=/data/scratch/baghdadi/libs/intel/mkl/
-	CORES=32
+	export TIRAMISU_ROOT=/data/scratch/baghdadi/tiramisu/
+	export MKL_FLAGS="-lmkl_intel_ilp64 -lmkl_gnu_thread -lmkl_core -lgomp -ldl -lm"
+	export MKL_LIB_PATH_SUFFIX=intel64/
+	export LANKA_OPTIONS="-DMKL_ILP64 -m64 -fopenmp"
+	export USE_PERF=1
+	export MKL_PREFIX=/data/scratch/baghdadi/libs/intel/mkl/
+	export CORES=32
 fi
 
-ISL_INCLUDE_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/isl/build/include/
-ISL_LIB_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/isl/build/lib/
-HALIDE_SOURCE_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/Halide
-HALIDE_LIB_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/Halide/lib
+export ISL_INCLUDE_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/isl/build/include/
+export ISL_LIB_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/isl/build/lib/
+export HALIDE_SOURCE_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/Halide
+export HALIDE_LIB_DIRECTORY=${TIRAMISU_ROOT}/3rdParty/Halide/lib
 
