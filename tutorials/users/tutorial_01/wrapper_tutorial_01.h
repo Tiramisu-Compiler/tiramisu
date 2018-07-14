bef226437c591b50a40e01b905ce47131d9e1c30
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-14 16:19:51 -0400

    Add wrapper for user tuto 01

---
+++ tutorials/users/tutorial_01/wrapper_tutorial_01.h
@@ -0,0 +1,15 @@
+#ifndef HALIDE__generated_pgm_h
+#define HALIDE__generated_pgm_h
+
+#include <tiramisu/utils.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int function0(halide_buffer_t *_p0_buffer);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif
