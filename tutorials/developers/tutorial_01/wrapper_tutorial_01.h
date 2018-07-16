1ab76be8dcaba0dfaa2322c109d5aada14c3f146
Riyadh Baghdadi <rbaghdadi@users.noreply.github.com>
2018-07-15 22:22:59 -0400

    Update wrapper_tutorial_01.h

--- tutorials/developers/tutorial_01/wrapper_tutorial_01.h
+++ tutorials/developers/tutorial_01/wrapper_tutorial_01.h
@@ -8,9 +8,7 @@ extern "C" {
 #endif
 
 int function0(halide_buffer_t *_p0_buffer);
-int function0_argv(void **args);
 
-extern const struct halide_filter_metadata_t halide_pipeline_aot_metadata;
 #ifdef __cplusplus
 }  // extern "C"
 #endif
