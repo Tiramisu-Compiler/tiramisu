dcd060e6b338938606bdd69d59e3802092ec116f
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-21 17:43:55 -0400

    Fix test115

--- tests/test_115.cpp
+++ tests/test_115.cpp
@@ -10,13 +10,13 @@ void gen(std::string name, int size, int val0, int val1)
 
     tiramisu::constant N("N", tiramisu::expr((int32_t) size), p_int32, true, NULL, 0, function0);
 
-    tiramisu::var i("i", 0, N), j("j", 0, N);
+    tiramisu::var i("i", 0, 10), j("j", 0, N);
     tiramisu::var i0("i0"), j0("j0"), i1("i1"), j1("j1");
 
     tiramisu::computation S0(tiramisu::expr((uint8_t) (val0 + val1)), i, j);
 
     S0.tile(i, j, 2, 2, i0, j0, i1, j1);
-    S0.tag_parallel_level(i);
+    S0.tag_parallel_level(i0);
 
     tiramisu::buffer buf0("buf0", {size, size}, tiramisu::p_uint8, a_output, function0);
     S0.store_in(&buf0, {i ,j});
