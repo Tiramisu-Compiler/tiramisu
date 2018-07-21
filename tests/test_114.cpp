207cf8b75102a4638dac6221a59c3779f1b37859
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-21 17:25:48 -0400

    Add support for implicit function

--- tests/test_114.cpp
+++ tests/test_114.cpp
@@ -8,7 +8,7 @@ void gen(std::string name, int size, int val0, int val1)
 
     tiramisu::function *function0 = global::get_implicit_function();
 
-    tiramisu::constant N("N", tiramisu::expr((int32_t) size), p_int32, true, NULL, 0, &function0);
+    tiramisu::constant N("N", tiramisu::expr((int32_t) size), p_int32, true, NULL, 0, function0);
 
     tiramisu::var i("i", 0, 10), j("j", 0, 10);
     tiramisu::var i0("i0"), j0("j0"), i1("i1"), j1("j1");
@@ -18,10 +18,10 @@ void gen(std::string name, int size, int val0, int val1)
     //S0.tile(i, j, 2, 2, i0, j0, i1, j1);
     S0.tag_parallel_level(i);
 
-    tiramisu::buffer buf0("buf0", {size, size}, tiramisu::p_uint8, a_output, &function0);
+    tiramisu::buffer buf0("buf0", {size, size}, tiramisu::p_uint8, a_output, function0);
     S0.store_in(&buf0, {i ,j});
 
-    function0.codegen({&buf0}, "build/generated_fct_test_114.o");
+    function0->codegen({&buf0}, "build/generated_fct_test_114.o");
 }
 
 int main(int argc, char **argv)
