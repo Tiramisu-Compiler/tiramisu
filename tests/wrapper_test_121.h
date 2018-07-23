907daf97424aa9351df2f7a325cceea0fc70fb35
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-23 18:42:07 -0400

    Add test for the ISL-free API

---
+++ tests/wrapper_test_121.h
@@ -0,0 +1,13 @@
+#ifndef HALIDE__generated_h
+#define HALIDE__generated_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int func(halide_buffer_t *_p0_buffer);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif
