c7e0d10c2ceaabab73a5a3c6ded72bc57db4fdb3
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-23 18:47:22 -0400

    Add test for the ISL-free API

---
+++ tests/test_123.cpp
@@ -0,0 +1,28 @@
+#include <tiramisu/tiramisu.h>
+
+using namespace tiramisu;
+
+void gen(std::string name, int size, int val0, int val1)
+{
+    tiramisu::init(name);
+
+    tiramisu::constant N("N", tiramisu::expr((int32_t) size), p_int32, true, NULL, 0);
+
+    tiramisu::var i("i", 0, 1);
+
+    tiramisu::computation S0({i}, tiramisu::expr((uint8_t) (val0 + val1)));
+
+    S0.tag_parallel_level(i);
+
+    tiramisu::buffer buf0("buf0", {size}, tiramisu::p_uint8, a_output);
+    S0.store_in(&buf0, {i});
+
+    tiramisu::codegen({&buf0}, "build/generated_fct_test_123.o");
+}
+
+int main(int argc, char **argv)
+{
+    gen("func", 1, 3, 4);
+
+    return 0;
+}
