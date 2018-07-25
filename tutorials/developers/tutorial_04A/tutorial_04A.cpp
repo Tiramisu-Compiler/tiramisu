4a8ce50657edd5c6f8d24138a39138511668c089
Riyadh Baghdadi <rbaghdadi@users.noreply.github.com>
2018-07-24 22:51:41 -0400

    Update tutorial_04A.cpp

--- tutorials/developers/tutorial_04A/tutorial_04A.cpp
+++ tutorials/developers/tutorial_04A/tutorial_04A.cpp
@@ -17,38 +17,38 @@
 #include <tiramisu/tiramisu.h>
 
 #define SIZE0 1000
+
 using namespace tiramisu;
 
 int main(int argc, char **argv)
 {
-    // Set default tiramisu options.
-    tiramisu::init();
+    tiramisu::init("matmul");
 
     // -------------------------------------------------------
     // Layer I
     // -------------------------------------------------------
 
-    /*
-     * Declare a function matmul.
-     */
-    function matmul("matmul");
+    constant p0("N", expr((int32_t) SIZE0));
 
-    constant p0("N", expr((int32_t) SIZE0), p_int32, true, NULL, 0, &matmul);
+    var i("i", 0, N), j("j", 0, N), k("k", 0, N);
 
     // Declare computations that represents the input buffers (b_A and b_B)
-    computation c_A("[N]->{c_A[i,j]: 0<=i<N and 0<=j<N}", expr(), false, p_uint8, &matmul);
-    computation c_B("[N]->{c_B[i,j]: 0<=i<N and 0<=j<N}", expr(), false, p_uint8, &matmul);
-
-    // Declare loop iterators
-    var i("i"), j("j"), k("k"), i0("i0"), j0("j0"), i1("i1"), j1("j1");
+    computation c_A({i, j}, p_uint8);
+    computation c_B({i, j}, p_uint8);
 
     // Declare a computation to initialize the reduction c[i,j]
-    computation C_init("[N]->{C_init[i,j,-1]: 0<=i<N and 0<=j<N}", expr((uint8_t) 0), true, p_uint8, &matmul);
+    computation C_init({i,j}, expr((uint8_t) 0));
     
     // Declare the reduction operation.
-    computation c_C("[N]->{c_C[i,j,k]: 0<=i<N and 0<=j<N and 0<=k<N}", expr(), true, p_uint8, &matmul);
+    computation c_C({i,j,k}, expr());
     // Note that the previous computation has an empty expression (because we can only use c_C in an expression after its declaration)
     c_C.set_expression(c_C(i, j, k - 1) + c_A(i, k) * c_B(k, j));
+    
+    // In this example, c_C does not read the value of C_init, but later
+    // we indicate that C_init and c_C both are stored in the same buffer,
+    // therefore c_C will read values written by C_init.
+    // We are working on adding an operator for reduction to perform reduction
+    // in a straight forward way.
 
     // -------------------------------------------------------
     // Layer II
@@ -57,6 +57,8 @@ int main(int argc, char **argv)
     // Tile both computations: C_init and c_C
     // This tiles the loop levels i and j and produces the loop levels by a 32x32 tile.
     // i0, j0, i1 and j1 where i0 is the outermost loop level and j1 is the innermost.
+
+    var i0("i0"), j0("j0"), i1("i1"), j1("j1");
     C_init.tile(i, j, 32, 32, i0, j0, i1, j1);
     c_C.tile(i, j, 32, 32, i0, j0, i1, j1);
 
@@ -74,9 +76,9 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
 
     // Declare the buffers.
-    buffer b_A("b_A", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_input, &matmul);
-    buffer b_B("b_B", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_input, &matmul);
-    buffer b_C("b_C", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_output, &matmul);
+    buffer b_A("b_A", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_input);
+    buffer b_B("b_B", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_input);
+    buffer b_C("b_C", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_output);
 
     // Map the computations to a buffer.
     c_A.store_in(&b_A);
@@ -93,10 +95,7 @@ int main(int argc, char **argv)
     // Code Generation
     // -------------------------------------------------------
 
-    matmul.codegen({&b_A, &b_B, &b_C}, "build/generated_fct_developers_tutorial_04A.o");
-    
-    // Dump the generated Halide statement (just for debugging).
-    matmul.dump_halide_stmt();
+    tiramisu::codegen({&b_A, &b_B, &b_C}, "build/generated_fct_developers_tutorial_04A.o");
 
     return 0;
 }
