1f83b915dfdec08c2a20ed0d9400275065b71bdf
Riyadh Baghdadi <rbaghdadi@users.noreply.github.com>
2018-07-24 22:44:47 -0400

    Update tutorial_03.cpp

--- tutorials/developers/tutorial_03/tutorial_03.cpp
+++ tutorials/developers/tutorial_03/tutorial_03.cpp
@@ -14,34 +14,32 @@ for (i = 0; i < M; i++)
 */
 
 #include <tiramisu/tiramisu.h>
+
 #define SIZE0 10
 
 using namespace tiramisu;
 
 int main(int argc, char **argv)
 {
-    // Set default tiramisu options.
-    tiramisu::init();
+    tiramisu::init("sequence");
 
     // -------------------------------------------------------
     // Layer I
     // -------------------------------------------------------
 
-    function sequence("sequence");
-
-    constant M("M", expr((int32_t) SIZE0), p_int32, true, NULL, 0, &sequence);
+    constant M("M", expr((int32_t) SIZE0));
+  
+      var i("i", 0, M),  j("j", 0, M);
   
     // Declare the four computations: c0, c1, c2 and c3.
-    computation c0("[M]->{c0[i]: 0<=i<M}", expr((uint8_t) 4), true, p_uint8, &sequence);
-    computation c1("[M]->{c1[i]: 0<=i<M}", expr((uint8_t) 3), true, p_uint8, &sequence);
-    computation c2("[M]->{c2[i,j]: 0<=i<M and 0<=j<M}", expr((uint8_t) 2), true, p_uint8, &sequence);
-    computation c3("[M]->{c3[i]: 0<=i<M}", expr((uint8_t) 1), true, p_uint8, &sequence);
+    computation c0({i}, expr((uint8_t) 4));
+    computation c1({i}, expr((uint8_t) 3));
+    computation c2({i,j}, expr((uint8_t) 2));
+    computation c3({i}, expr((uint8_t) 1));
 
     // -------------------------------------------------------
     // Layer II
     // -------------------------------------------------------
-
-    var i("i");
   
     // By default computations are unordered in Tiramisu. The user has to specify
     // the order exlplicitely (automatic ordering is being developed and will be
@@ -59,10 +57,10 @@ int main(int argc, char **argv)
     // Layer III
     // -------------------------------------------------------
 
-    buffer b0("b0", {expr(SIZE0)}, p_uint8, a_output, &sequence);
-    buffer b1("b1", {expr(SIZE0)}, p_uint8, a_output, &sequence);
-    buffer b2("b2", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_output, &sequence);
-    buffer b3("b3", {expr(SIZE0)}, p_uint8, a_output, &sequence);
+    buffer b0("b0", {expr(SIZE0)}, p_uint8, a_output);
+    buffer b1("b1", {expr(SIZE0)}, p_uint8, a_output);
+    buffer b2("b2", {expr(SIZE0), expr(SIZE0)}, p_uint8, a_output);
+    buffer b3("b3", {expr(SIZE0)}, p_uint8, a_output);
 
     c0.store_in(&b0);
     c1.store_in(&b1);
@@ -73,10 +71,7 @@ int main(int argc, char **argv)
     // Code Generator
     // -------------------------------------------------------
 
-    sequence.codegen({&b0, &b1, &b2, &b3}, "build/generated_fct_developers_tutorial_03.o");
-
-    // Dump the generated Halide statement (just for debugging).
-    sequence.dump_halide_stmt();
+    tiramisu::codegen({&b0, &b1, &b2, &b3}, "build/generated_fct_developers_tutorial_03.o");
 
     return 0;
 }
