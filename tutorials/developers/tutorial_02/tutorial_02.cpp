04e910ae6a2d6982e8a02af9c41fbbb506bd313e
Riyadh Baghdadi <rbaghdadi@users.noreply.github.com>
2018-07-25 00:09:30 -0400

    Update tutorial_02.cpp

--- tutorials/developers/tutorial_02/tutorial_02.cpp
+++ tutorials/developers/tutorial_02/tutorial_02.cpp
@@ -4,7 +4,7 @@
 
   for (int i = 0; i < 10; i++)
     for (int j = 0; j < 20; j++)
-      output[i, j] = input[i, j] + i + 2;
+      output[i, j] = A[i, j] + i + 2;
 */
 
 #include <tiramisu/tiramisu.h>
@@ -29,19 +29,12 @@ int main(int argc, char **argv)
     // Declare iterator variables.
     var i("i", 0, N_const), j("j", 0, M_const);
 
-    // Declare a wrapper around the input.
-    // In Tiramisu, if a function reads an input buffer (or writes to it), that buffer
-    // is not accessed directly, but should first be wrapped in a computation.
-    // This is mainly because computations in Tiramisu do not access memory directly,
-    // since the algorithm is supposed to be expressed independently of how data is stored.
-    // Therefore computations (algorithms) access only other computations.  The actual data
-    // layout is only specified later in Layer III.
-    // A wrapper is usually declared by providing the iterators (that define the size of the
-    // buffer) and the type of the buffer elements.
-    computation input({i,j}, p_uint8);
+    // Declare an input.  The input is declared by providing iterators
+    // (that define the size of the buffer) and the type of the buffer elements.
+    input A({i,j}, p_uint8);
 
     // Declare the output computation.
-    computation output({i,j}, (input(i, j) + cast(p_uint8, i) + (uint8_t)4));
+    computation output({i,j}, (A(i, j) + cast(p_uint8, i) + (uint8_t)4));
 
     // -------------------------------------------------------
     // Layer II
@@ -62,14 +55,14 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
 
     // Declare input and output buffers.
-    buffer b_input("b_input", {expr(NN), expr(MM)}, p_uint8, a_input);
+    buffer b_A("b_A", {expr(NN), expr(MM)}, p_uint8, a_input);
     buffer b_output("b_output", {expr(NN), expr(MM)}, p_uint8, a_output);
 
     // Map the computations to a buffer.
-    // The following call indicates that each computation input[i,j]
-    // is stored in the buffer element b_input[i,j] (one-to-one mapping).
+    // The following call indicates that each computation A[i,j]
+    // is stored in the buffer element b_A[i,j] (one-to-one mapping).
     // This is the most common mapping to memory.
-    input.store_in(&b_input);
+    A.store_in(&b_A);
     output.store_in(&b_output);
 
     // -------------------------------------------------------
@@ -77,7 +70,7 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
 
     // Set the arguments and generate code
-    tiramisu::codegen({&b_input, &b_output}, "build/generated_fct_developers_tutorial_02.o");
+    tiramisu::codegen({&b_A, &b_output}, "build/generated_fct_developers_tutorial_02.o");
 
     return 0;
 }
