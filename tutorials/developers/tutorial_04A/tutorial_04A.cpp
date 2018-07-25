a171ac28eb4dcb208bbcef4453f47c4e9a93ee73
Riyadh Baghdadi <rbaghdadi@users.noreply.github.com>
2018-07-24 22:52:50 -0400

    Update tutorial_04A.cpp

--- tutorials/developers/tutorial_04A/tutorial_04A.cpp
+++ tutorials/developers/tutorial_04A/tutorial_04A.cpp
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 
     constant p0("N", expr((int32_t) SIZE0));
 
-    var i("i", 0, N), j("j", 0, N), k("k", 0, N);
+    var i("i", 0, p0), j("j", 0, p0), k("k", 0, p0);
 
     // Declare computations that represents the input buffers (b_A and b_B)
     computation c_A({i, j}, p_uint8);
