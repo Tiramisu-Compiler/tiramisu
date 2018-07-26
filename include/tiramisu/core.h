304255632cdaa3d43a41264771cee57e1fbe9300
Abdurrahman Akkas <akkas@mit.edu>
2018-07-26 14:03:26 -0400

    Fix input constructor

--- include/tiramisu/core.h
+++ include/tiramisu/core.h
@@ -3930,7 +3930,7 @@ public:
       * \overload
       */
     input(std::vector<var> iterator_variables, primitive_t t):
-	    input(generate_new_computation_name(), iterator_variables, expr(), false)
+	    input(generate_new_computation_name(), iterator_variables, t)
     {
     }
 
