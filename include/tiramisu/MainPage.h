dad599a4b721780c6b11122bf88c39bdda5c9eed
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-07-26 15:26:53 -0400

    Fix the documentation main page

--- include/tiramisu/MainPage.h
+++ include/tiramisu/MainPage.h
@@ -2,18 +2,13 @@
  * This file only exists to contain the front-page of the documentation
  */
 
-/** \mainpage Tiramisu Optimization Framework 
- * Tiramisu is a library that is designed to simplify code optimization and code generation.  The user can express his code in the Tiramisu intermediate representation (Tiramisu IR), he can use the Tiramisu API to perform different optimizations and finaly he can generate an LLVM code from the optimized Tiramisu IR.
+/** \mainpage Documentation of the API if the Tiramisu Compiler
  *
  * Tiramisu provides few classes to enable users to represent their program:
- * - The \ref tiramisu::function class: a function is composed of multiple computations and a vector of arguments (functions arguments).
- * - The \ref tiramisu::computation class: a computation is composed of an expression and an iteration space but is not associated with any memory location.
+ * - The \ref tiramisu::function class: a function in Tiramisu is equivalent to a function in C. It is composed of multiple computations. Each computation is the equivalent of a statement in C.
+ * - The \ref tiramisu::input class: an input is used to represent inputs passed to Tiramisu.  An input can represent a buffer or a scalar.
+ * - The \ref tiramisu::constant class: a constant is designed to represent constants that are supposed to be declared at the beginning of a Tiramisu function.
+ * - The \ref tiramisu::computation class: a computation in Tiramisu is the equivalent of a statement in C. It is composed of an expression and an iteration domain.
  * - The \ref tiramisu::buffer class: a class to represent memory buffers.
  *
- * \example tutorials/tutorial_01.cpp 
- * \example tutorials/tutorial_02.cpp 
- * \example tutorials/tutorial_03.cpp 
- * \example tutorials/tutorial_04.cpp 
- * \example tutorials/tutorial_05.cpp 
- * \example tutorials/tutorial_06.cpp 
  */
