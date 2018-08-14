7e804a4f7347877f41ed6059deda66930b54ea66
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-08-14 16:42:20 -0400

    Add cpu chain_mm

---
+++ benchmarks/linear_algebra/chain_mm/cpu/sgemm_wrapper.h
@@ -0,0 +1,22 @@
+#ifndef TIRAMISU_test_h
+#define TIRAMISU_test_h
+
+// --------------------------------------------------------
+// No need to modify anything in the following ------------
+// --------------------------------------------------------
+
+#include <tiramisu/utils.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int sgemm_tiramisu(halide_buffer_t *SIZES, halide_buffer_t *alpha, halide_buffer_t *beta, halide_buffer_t *A, halide_buffer_t *B, halide_buffer_t *C);
+int sgemm_halide(halide_buffer_t *alpha, halide_buffer_t *beta, halide_buffer_t *A, halide_buffer_t *B, halide_buffer_t *C);
+
+int tiramisu_generated_code_argv(void **args);
+
+extern const struct halide_filter_metadata_t halide_pipeline_aot_metadata;
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif
