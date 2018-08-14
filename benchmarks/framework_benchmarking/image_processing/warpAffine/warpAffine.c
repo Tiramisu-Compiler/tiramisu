ba664e9671d639b10c58731813d678d34dd37ed5
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-08-14 16:38:51 -0400

    Fix warpAffine

--- benchmarks/framework_benchmarking/image_processing/warpAffine/warpAffine.c
+++ benchmarks/framework_benchmarking/image_processing/warpAffine/warpAffine.c
@@ -1,8 +1,11 @@
 #include "warpAffine.h"
-#include <pencil.h>
+//#include <pencil.h>
 
-static void affine( const int src_rows, const int src_cols, const int src_step, const float src[static const restrict src_rows][src_step]
-                  , const int dst_rows, const int dst_cols, const int dst_step,       float dst[static const restrict dst_rows][dst_step]
+float mixf(float x, float y, float a) { return x + (x-y) * a; }
+int clamp(int x, int minval, int maxval) { if (x < minval) return minval; if (x > maxval) return maxval; return x; }
+
+static void affine( const int src_rows, const int src_cols, const int src_step, const float src[static const restrict src_rows][src_step][3]
+                  , const int dst_rows, const int dst_cols, const int dst_step,       float dst[static const restrict dst_rows][dst_step][3]
                   , const float a00, const float a01, const float a10, const float a11, const float b00, const float b10
                   )
 {
@@ -22,36 +25,39 @@ static void affine( const int src_rows, const int src_cols, const int src_step, 
         #pragma pencil independent
         for ( int n_c=0; n_c<dst_cols; n_c++ )
         {
-            float o_r = a11 * n_r + a10 * n_c + b00;
-            float o_c = a01 * n_r + a00 * n_c + b10;
+	    for (int cc = 0; cc < 3; cc++)
+	    {
+		float o_r = a11 * n_r + a10 * n_c + b00;
+		float o_c = a01 * n_r + a00 * n_c + b10;
 
-            float r = o_r - floorf(o_r);
-            float c = o_c - floorf(o_c);
+		float r = o_r - floorf(o_r);
+		float c = o_c - floorf(o_c);
 
-            int coord_00_r = floorf(o_r);
-            int coord_00_c = floorf(o_c);
-            int coord_01_r = coord_00_r;
-            int coord_01_c = coord_00_c + 1;
-            int coord_10_r = coord_00_r + 1;
-            int coord_10_c = coord_00_c;
-            int coord_11_r = coord_00_r + 1;
-            int coord_11_c = coord_00_c + 1;
+		int coord_00_r = floorf(o_r);
+		int coord_00_c = floorf(o_c);
+		int coord_01_r = coord_00_r;
+		int coord_01_c = coord_00_c + 1;
+		int coord_10_r = coord_00_r + 1;
+		int coord_10_c = coord_00_c;
+		int coord_11_r = coord_00_r + 1;
+		int coord_11_c = coord_00_c + 1;
 
-            coord_00_r = clamp(coord_00_r, 0, src_rows);
-            coord_00_c = clamp(coord_00_c, 0, src_cols);
-            coord_01_r = clamp(coord_01_r, 0, src_rows);
-            coord_01_c = clamp(coord_01_c, 0, src_cols);
-            coord_10_r = clamp(coord_10_r, 0, src_rows);
-            coord_10_c = clamp(coord_10_c, 0, src_cols);
-            coord_11_r = clamp(coord_11_r, 0, src_rows);
-            coord_11_c = clamp(coord_11_c, 0, src_cols);
+		coord_00_r = clamp(coord_00_r, 0, src_rows);
+		coord_00_c = clamp(coord_00_c, 0, src_cols);
+		coord_01_r = clamp(coord_01_r, 0, src_rows);
+		coord_01_c = clamp(coord_01_c, 0, src_cols);
+		coord_10_r = clamp(coord_10_r, 0, src_rows);
+		coord_10_c = clamp(coord_10_c, 0, src_cols);
+		coord_11_r = clamp(coord_11_r, 0, src_rows);
+		coord_11_c = clamp(coord_11_c, 0, src_cols);
 
-            float A00 = src[coord_00_r][coord_00_c];
-            float A10 = src[coord_10_r][coord_10_c];
-            float A01 = src[coord_01_r][coord_01_c];
-            float A11 = src[coord_11_r][coord_11_c];
+		float A00 = src[coord_00_r][coord_00_c][cc];
+		float A10 = src[coord_10_r][coord_10_c][cc];
+		float A01 = src[coord_01_r][coord_01_c][cc];
+		float A11 = src[coord_11_r][coord_11_c][cc];
 
-            dst[n_r][n_c] = mixf( mixf(A00, A10, r), mixf(A01, A11, r), c);
+		dst[n_r][n_c][cc] = mixf( mixf(A00, A10, r), mixf(A01, A11, r), c);
+	    }
         }
     }
     __pencil_kill(src);
@@ -63,8 +69,8 @@ void pencil_affine_linear( const int src_rows, const int src_cols, const int src
                          , const float a00, const float a01, const float a10, const float a11, const float b00, const float b10
                          )
 {
-    affine( src_rows, src_cols, src_step, (const float(*)[src_step])src
-          , dst_rows, dst_cols, dst_step, (      float(*)[dst_step])dst
+    affine( src_rows, src_cols, src_step, (const float(*)[src_step][3])src
+          , dst_rows, dst_cols, dst_step, (      float(*)[dst_step][3])dst
           , a00, a01, a10, a11, b00, b10
           );
 }
