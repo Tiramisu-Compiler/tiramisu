77ead24c4cdcbb1fe67af5073422213a3e4d3f83
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-08-17 19:28:57 -0400

    update edge

--- benchmarks/halide/edge_ref.cpp
+++ benchmarks/halide/edge_ref.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
 {
     ImageParam Img(UInt(8), 3, "Img");
     Func R("R"), Out("Out");
-    Var i("i"), j("j"), c("c");
+    Var i("i"), j("j"), c("c"), i1("i1"), j1("j1"), i2("i2"), j2("j2");
 
     /* Ring blur filter. */
     R(i, j, c) = (Img(i,   j, c) + Img(i,   j+1, c) + Img(i,   j+2, c)+
@@ -18,6 +18,10 @@ int main(int argc, char* argv[])
     Out(i, j, c) = (R(i+1, j+1, c)-R(i+2, j, c)) + (R(i+2, j+1, c)-R(i+1, j, c));
 
     R.compute_root();
+    R.tile(i,j, i1, j1, i2, j2, 32, 32);
+    Out.tile(i,j, i1, j1, i2, j2, 32, 32);
+    R.parallel(i1);
+    Out.parallel(i1);
     Out.compute_root();
 
     Out.compile_to_object("build/generated_fct_edge_ref.o", {Img}, "edge_ref");
