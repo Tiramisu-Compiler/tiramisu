06e6d5787a4a3e03776bad695b83fbe3844cde74
Riyadh Baghdadi <baghdadi@lanka47-eth.csail.mit.edu>
2018-09-05 11:37:35 -0400

    Update configuration file

--- benchmarks/framework_benchmarking/configure.sh
+++ benchmarks/framework_benchmarking/configure.sh
@@ -1,15 +1,27 @@
-TIRAMISU_ROOT=/Users/b/Documents/src/MIT/tiramisu/
-BENCHMARK_ROOT=${TIRAMISU_ROOT}/benchmarks/framework_benchmarking/
-LLVM_PREFIX=${TIRAMISU_ROOT}/3rdParty/llvm/prefix/
-#CC=${LLVM_PREFIX}/bin/clang
-CC=clang-omp
-OPENMP_LIB=iomp5
-OPENMP_DIR=/usr/local/Cellar/libiomp/20150701/
-OPENBLAS_DIR=/Volumes/ALL/extra/OpenBLAS/
-OpenBLAS_FLAGS="-lcblas"
+LANKA=1
+
+export TIRAMISU_ROOT=/data/scratch/baghdadi/tiramisu/
+export BENCHMARK_ROOT=${TIRAMISU_ROOT}/benchmarks/framework_benchmarking/
+export LLVM_PREFIX=${TIRAMISU_ROOT}/3rdParty/llvm/prefix/
+
+if [ $LANKA -eq 0 ]; then
+	export CXXFLAGS=""
+	export CC=clang-omp
+	export OPENMP_LIB=-liomp5
+	export OPENMP_DIR=/usr/local/Cellar/libiomp/20150701/
+	export OPENBLAS_DIR=/Volumes/ALL/extra/OpenBLAS/
+	export OpenBLAS_FLAGS="-lcblas"
+else
+	export CXXFLAGS="-fopenmp"
+	export CC=gcc
+	export OPENMP_LIB=""
+	export OPENMP_DIR=.
+	export OPENBLAS_DIR=.
+	export OpenBLAS_FLAGS=""
+fi
 
 PPCG=${BENCHMARK_ROOT}/software/ppcg/ppcg
-HALIDE_PREFIX=${TIRAMISU_ROOT}/3rdParty/Halide/
+HALIDE_PREFIX=${TIRAMISU_ROOT}/3rdParty/Halide
 PLUTO=/Users/b/Documents/src-not-saved/pluto-0.11.4/polycc
 PLUTO_OPTS="--tile --parallel"
 POLLY="/Volumes/ALL/extra/polly/llvm_build/bin/clang"
