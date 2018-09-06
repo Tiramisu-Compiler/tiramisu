4912c499d07b471e4e23e300eeaddcae373fffd7
Abdurrahman Akkas <akkas@mit.edu>
2018-09-06 18:19:00 -0400

    PPCG CUDA Benchmark fix

--- benchmarks/framework_benchmarking/image_processing/convolution/convolution.c
+++ benchmarks/framework_benchmarking/image_processing/convolution/convolution.c
@@ -1,22 +1,36 @@
 #include "convolution.h"
-#include <pencil.h>
 #include <assert.h>
+#include <stdint.h>
 
 //#if !__PENCIL__
 #include <stdlib.h>
 //#endif
 
+#ifdef __PENCIL_HEADER__
 static void convolution( const int rows
                     , const int cols
                     , const int step
                     , const unsigned char src[rows][step][3]
                     , const int kernelX_length
-                    , const float kernelX[kernelX_length]
+                    , float kernelX[kernelX_length]
                     , const int kernelY_length
-                    , const float kernelY[kernelY_length]
+                    , float kernelY[kernelY_length]
                     , uint8_t conv[rows][step][3]
-		    , uint8_t temp[rows][step][3]
+                    , uint8_t temp[rows][step][3]
                     )
+#else
+static void convolution( const int rows
+                    , const int cols
+                    , const int step
+                    , const unsigned char *src
+                    , const int kernelX_length
+                    , float *kernelX
+                    , const int kernelY_length
+                    , float *kernelY
+                    , uint8_t *conv
+                    , uint8_t *temp
+                    )
+#endif
 {
 #pragma scop
     {
@@ -24,15 +38,15 @@ static void convolution( const int rows
         {
             for ( int w = 0; w < cols; w++ )
             {
-		for (int cc = 0; cc < 3; cc++)
-		{
-		    float prod1 = 0.;
-		    for ( int r = 0; r < kernelX_length; r++ )
-		    {
-			prod1 += src[q][w][cc] * kernelX[r];
-		    }
-		    conv[q][w][cc] = prod1;
-		}
+                for (int cc = 0; cc < 3; cc++)
+                {
+                    float prod1 = 0.;
+                    for ( int r = 0; r < kernelX_length; r++ )
+                    {
+                        prod1 += src[q][w][cc] * kernelX[r];
+                    }
+                    conv[q][w][cc] = prod1;
+                }
             }
         }
     }
@@ -44,17 +58,17 @@ void pencil_convolution( const int rows
                     , const int step
                     , const uint8_t src[]
                     , const int kernelX_length
-                    , const float kernelX[]
+                    , float kernelX[]
                     , const int kernelY_length
-                    , const float kernelY[]
+                    , float kernelY[]
                     , uint8_t conv[]
-		    , uint8_t temp[]
+                    , uint8_t temp[]
                     )
 {
-    convolution( rows, cols, step, (const uint8_t(*)[step])src
+    convolution( rows, cols, step, src
             , kernelX_length, kernelX
             , kernelY_length, kernelY
-            , (uint8_t(*)[step])conv
-	    , temp
+            , conv
+            , temp
             );
 }
