3fb29366ff6d17edbe93fc8918b8268a6bf460e5
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-11-05 13:05:28 -0500

    Fix opencv optical flow

--- benchmarks/halide/optical_flow_opencv.sh
+++ benchmarks/halide/optical_flow_opencv.sh
@@ -1,6 +1,6 @@
 OPENCV_PREFIX=/Users/b/Documents/src-not-saved/opencv-3.2.0_prefix/
 
-g++ --std=c++11 py_optical_flow_opencv.cpp -L${OPENCV_PREFIX}/lib/ -I${OPENCV_PREFIX}/include -lopencv_core -lopencv_imgproc -lopencv_video -lopencv_imgcodecs -o py_optical_flow_opencv
+g++ --std=c++11 opencv_py_optical_flow.cpp -L${OPENCV_PREFIX}/lib/ -I${OPENCV_PREFIX}/include -lopencv_core -lopencv_imgproc -lopencv_video -lopencv_imgcodecs -o opencv_py_optical_flow
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OPENCV_PREFIX}/lib/
 
-./py_optical_flow_opencv  ../../utils/images/rgb.png  ../../utils/images/rgb.png
+./opencv_py_optical_flow  ../../utils/images/rgb.png  ../../utils/images/rgb.png
