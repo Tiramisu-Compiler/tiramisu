58a6173638598873ea3f6eb94c303087ab7ea272
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-11-07 14:58:04 -0500

    First commit for py optical flow

---
+++ benchmarks/halide/wrapper_py_optical_flow.h
@@ -0,0 +1,23 @@
+#ifndef HALIDE__build___wrapper_optical_flow_o_h
+#define HALIDE__build___wrapper_optical_flow_o_h
+
+#include <tiramisu/utils.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Corner number
+#define _NC 8
+
+// Window size
+#define w 128
+
+int py_optical_flow_tiramisu(halide_buffer_t *, halide_buffer_t *, halide_buffer_t *,
+			  halide_buffer_t *, halide_buffer_t *, halide_buffer_t *,
+			  halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *i, halide_buffer_t *, halide_buffer_t *);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif
