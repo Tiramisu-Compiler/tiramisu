165dd4f54b3e3138d888f0bbe8acc302d97aafe4
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-11-08 07:59:00 -0500

    Remove py_optical_flow_Matlab

--- benchmarks/halide/py_optical_flow_Matlab/imInit.m
+++
@@ -1,5 +0,0 @@
-function [ Io ] = imInit( str )
-
-Io = im2double( rgb2gray( imread(str) ) );
-
-end
--- benchmarks/halide/py_optical_flow_Matlab/imS.m
+++
@@ -1,9 +0,0 @@
-function [] = imS( Iin, figNo, varargin )
-
-if nargin == 2
-    figure(figNo), imshow(Iin)
-else
-    figure(figNo), imshow(Iin, varargin{:})
-end
-
-end
