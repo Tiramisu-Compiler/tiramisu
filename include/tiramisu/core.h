62b98d681df78517a757180fcb65dee7d41fccd3
Thinhinane Ihadadene <et_ihadadene@esi.dz>
2018-11-12 15:38:13 +0100

    add documentation of the view class in core.h

--- include/tiramisu/core.h
+++ include/tiramisu/core.h
@@ -3981,10 +3981,8 @@ public:
       * input A("A", {i,j}, p_uint8);
       * \endcode
       *
-      * \code 
       * Later in the code (in Layer III), we need to actually declare
       * the buffer and map this input to that buffer.
-      * \endcode
       *
       * An example is provided in tutorial 02.
       *
@@ -4039,8 +4037,9 @@ public:
       * \endcode
       *
       * Later in the code (in Layer III), we need to map the view to that buffer.
+      * \code
       * V.store_in(&buf);
-      *
+      * \endcode
      */
     view(std::string name, std::vector<var> iterator_variables):
     computation(name,iterator_variables,expr(),false){}
