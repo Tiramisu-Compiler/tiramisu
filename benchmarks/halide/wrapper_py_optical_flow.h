db45619e43a7a70ff54f2df26df8aa9dd2ed4f33
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-11-12 16:08:52 -0500

    update py of

--- benchmarks/halide/wrapper_py_optical_flow.h
+++ benchmarks/halide/wrapper_py_optical_flow.h
@@ -12,25 +12,23 @@ extern "C" {
 
 #ifdef SYNTHETIC_INPUT
     // Window size
-    #define w 2
-    #define _NC 2
+    #define w 4
 #else
     // Window size
     #define w 128
-    #define _NC 8
 #endif
 
 // Corner number
 
 // Number of pyramid levels
-#define npyramids 3
+#define npyramids 2
 
 // Number of refinement iterations
-#define niterations 3
+#define niterations 1
 
 int py_optical_flow_tiramisu(halide_buffer_t *, halide_buffer_t *, halide_buffer_t *,
 			  halide_buffer_t *, halide_buffer_t *, halide_buffer_t *,
-			  halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *i, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *);
+			  halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *i, halide_buffer_t *, halide_buffer_t *, halide_buffer_t *);
 
 #ifdef __cplusplus
 }  // extern "C"
