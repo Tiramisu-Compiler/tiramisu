5f8ea87f0188a3eae0cf1554e2db517b9a4b5e0f
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2018-12-27 14:33:52 +0100

    Add skewing test

---
+++ tests/wrapper_test_140.h
@@ -0,0 +1,29 @@
+#ifndef TIRAMISU_test_h
+#define TIRAMISU_test_h
+
+
+// Define these values for each new test
+#define TEST_NAME_STR       "skew"
+#define TEST_NUMBER_STR     "140"
+// Data size
+#define SIZE0 1
+#define SIZE1 10
+
+
+// --------------------------------------------------------
+// No need to modify anything in the following ------------
+// --------------------------------------------------------
+
+#include <tiramisu/utils.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int tiramisu_generated_code(halide_buffer_t *_p0_buffer);
+int tiramisu_generated_code_argv(void **args);
+
+extern const struct halide_filter_metadata_t halide_pipeline_aot_metadata;
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif
