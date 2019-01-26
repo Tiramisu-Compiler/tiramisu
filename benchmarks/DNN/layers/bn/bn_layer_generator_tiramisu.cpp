5d3fc96bf7bcf113e063bd669d0db6e92f5663ef
Radja <er_hachilif@esi.dz>
2019-01-26 01:39:35 +0100

    Batch normalization layer added

---
+++ benchmarks/DNN/layers/bn/bn_layer_generator_tiramisu.cpp
@@ -0,0 +1,84 @@
+#include <tiramisu/tiramisu.h>
+
+using namespace tiramisu;
+
+int main(int argc, char **argv)
+{
+    tiramisu::init("bn_tiramisu");
+
+    // N: parameters[0]
+    // FIn: parameters[1]
+    // BATCH_SIZE: parameters[2]
+
+    var i("i", 0, 3);
+    input parameters("parameters", {i}, p_int32);
+    constant C_N("C_N", parameters(0));
+    constant C_NX("C_NX", parameters(0));
+    constant C_FIn("C_FIn", parameters(1));
+    constant C_BATCH_SIZE("C_BATCH_SIZE", parameters(2));
+    constant C_NB_ELEMENTS("C_NB_ELEMENTS", parameters(0) * parameters(0) * parameters(2));
+
+    var x("x", 0, C_N), x1("x1", 1, C_N), y("y", 0, C_N), y1("y1", 1, C_N), z("z", 0, C_FIn), n("n", 0, C_BATCH_SIZE), n1("n1", 1, C_BATCH_SIZE);
+    var x_m("x_m", C_N - 1, C_N), y_m("y_m", C_N - 1, C_N);
+
+    input c_input("c_input", {n, z, y, x}, p_float32);
+
+    computation init_mean("init_mean", {n, z, y, x}, c_input(n, z, y, x));
+    computation x_mean("x_mean", {n, z, y, x1}, init_mean(n, z, y, x1) + init_mean(n, z, y, x1 - 1));
+    computation y_mean("y_mean", {n, z, y1, x_m}, x_mean(n, z, y1, x_m) + x_mean(n, z, y1 - 1, x_m));
+    computation mean("mean", {n1, z, y_m, x_m}, y_mean(n1, z, y_m, x_m) + y_mean(n1 - 1, z, y_m, x_m));
+
+    computation init_variance("init_variance", {n, z, y, x}, (c_input(n, z, y, x) - expr(o_div, mean(C_BATCH_SIZE - 1, z, C_N - 1, C_NX - 1), cast(p_float32, C_NB_ELEMENTS))) * (c_input(n, z, y, x) - expr(o_div, mean(C_BATCH_SIZE - 1, z, C_N - 1, C_NX - 1), cast(p_float32, C_NB_ELEMENTS))));
+
+    computation x_variance("x_variance", {n, z, y, x1}, init_variance(n, z, y, x1) + init_variance(n, z, y, x1 - 1));
+    computation y_variance("y_variance", {n, z, y1, x_m}, x_variance(n, z, y1, x_m) + x_variance(n, z, y1 - 1, x_m));
+    computation variance("variance", {n1, z, y_m, x_m}, y_variance(n1, z, y_m, x_m) + y_variance(n1 - 1, z, y_m, x_m));
+
+    computation output("output", {n, z, y, x}, expr(o_div, (c_input(n, z, y, x) - expr(o_div, mean(C_BATCH_SIZE - 1, z, C_N - 1, C_NX - 1), cast(p_float32, C_NB_ELEMENTS))), expr(o_sqrt, expr(o_div, variance(C_BATCH_SIZE - 1, z, C_N - 1, C_NX - 1), cast(p_float32, C_NB_ELEMENTS)))));
+
+    init_mean.tag_parallel_level(0);
+
+    x_mean.tag_parallel_level(0);
+    x_mean.after(init_mean, 3);
+
+    y_mean.tag_parallel_level(0);
+    y_mean.after(x_mean, 2);
+
+    mean.after(y_mean, computation::root_dimension);
+
+    init_variance.tag_parallel_level(0);
+    init_variance.after(mean, computation::root_dimension);
+
+    x_variance.tag_parallel_level(0);
+    x_variance.after(init_variance, computation::root_dimension);
+
+    y_variance.tag_parallel_level(0);
+    y_variance.after(x_variance, computation::root_dimension);
+
+    variance.after(y_variance, computation::root_dimension);
+
+    output.tag_parallel_level(0);
+    output.after(variance, computation::root_dimension);
+    output.tag_unroll_level(3);
+
+    buffer input_buf("input_buf", {C_BATCH_SIZE, C_FIn, C_N, C_N}, p_float32, a_input);
+    buffer parameters_buf("parameters_buf", {expr(3)}, p_int32, a_input);
+    buffer output_buf("output_buf", {C_BATCH_SIZE, C_FIn, C_N, C_N}, p_float32, a_output);
+    buffer mean_buff("mean_buff", {C_BATCH_SIZE, C_FIn, C_N, C_N}, p_float32, a_output);
+    buffer variance_buff("variance_buff", {C_BATCH_SIZE, C_FIn, C_N, C_N}, p_float32, a_output);
+
+    parameters.store_in(&parameters_buf);
+    c_input.store_in(&input_buf);
+    init_mean.store_in(&mean_buff);
+    mean.store_in(&mean_buff);
+    x_mean.store_in(&mean_buff);
+    y_mean.store_in(&mean_buff);
+    init_variance.store_in(&variance_buff);
+    x_variance.store_in(&variance_buff);
+    y_variance.store_in(&variance_buff);
+    variance.store_in(&variance_buff);
+    output.store_in(&output_buf);
+
+    tiramisu::codegen({&input_buf, &parameters_buf, &mean_buff, &variance_buff, &output_buf}, "bn_layer_generator_tiramisu.o");
+    return 0;
+}
---
+++ benchmarks/DNN/layers/bn/compile_and_run_mkldnn.sh
@@ -0,0 +1,10 @@
+#set -x
+
+source ../../../configure_paths.sh
+
+echo
+echo
+echo "MKL-DNN BN "
+
+g++ -std=c++11 -fopenmp -I${MKL_PREFIX}/include -L${MKL_PREFIX}/lib bn_layer_generator_mkldnn.cpp -lmkldnn  -o bn_layer_mkldnn_result
+LD_LIBRARY_PATH=${MKL_PREFIX}/build/src/:$LD_LIBRARY_PATH  ./bn_layer_mkldnn_result
