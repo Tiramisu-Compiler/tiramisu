f3fa4080b7808373e951ca7b13e698ba5360e365
Radja <er_hachilif@esi.dz>
2019-01-26 13:09:51 +0100

    Delete clean.sh

--- benchmarks/DNN/blocks/fusedresNet/clean.sh
+++
@@ -1,1 +0,0 @@
-rm -rf fused_resnet_block_generator_tiramisu  generated_fused_resnet_block.o generated_fused_resnet_block.o.h wrapper_nn_block_fused_resnet fused_resnet_block_generator_tiramisu fused_resnetBlock_mkldnn_result tiramisu_result.txt mkldnn_result.txt
