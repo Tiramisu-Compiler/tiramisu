b76474b12401b57532db5964b9a02421e8494db5
Radja <er_hachilif@esi.dz>
2019-01-26 02:05:58 +0100

    Comments added

--- benchmarks/DNN/blocks/fusedresNet/fused_resnet_block_generator_tiramisu.cpp
+++ benchmarks/DNN/blocks/fusedresNet/fused_resnet_block_generator_tiramisu.cpp
@@ -1,3 +1,14 @@
+/* 
+    This benchmark represents a ResNet Block which includes the next layers in order:
+    - convolution layer
+    - 2D batch normalization
+    - relu
+    - convolution 
+    - 2D batch normalization
+
+    Each convolution funtion is fused with the BN layer that follows it.
+*/
+
 #include <tiramisu/tiramisu.h>
 
 using namespace tiramisu;
@@ -41,18 +52,19 @@ int main(int argc, char **argv)
   computation conv1("conv1", {n, k_z, y, x, z, k_y, k_x}, init_conv1(n, k_z, y, x) + filter(k_z, z, k_y, k_x) * inputPadd(n, z, y + k_y, x + k_x));
 
   // BN1 computation
-  computation init_mean("init_mean", {n, k_z, y, x}, conv1(n, k_z, y, x, 0, 0, 0));
-  computation x_mean("x_mean", {n, k_z, y, x11}, init_mean(n, k_z, y, x11) + init_mean(n, k_z, y, x11 - 1));
-  computation y_mean("y_mean", {n, k_z, y11, x_m}, x_mean(n, k_z, y11, x_m) + x_mean(n, k_z, y11 - 1, x_m));
-  computation mean("mean", {n11, k_z, y_m, x_m}, y_mean(n11, k_z, y_m, x_m) + y_mean(n11 - 1, k_z, y_m, x_m));
+  //calculate the sum of the input values in parallel
+  computation init_sum("init_sum", {n, k_z, y, x}, conv1(n, k_z, y, x, 0, 0, 0));
+  computation x_sum("x_sum", {n, k_z, y, x11}, init_sum(n, k_z, y, x11) + init_sum(n, k_z, y, x11 - 1));
+  computation y_sum("y_sum", {n, k_z, y11, x_m}, x_sum(n, k_z, y11, x_m) + x_sum(n, k_z, y11 - 1, x_m));
+  computation sum("sum", {n11, k_z, y_m, x_m}, y_sum(n11, k_z, y_m, x_m) + y_sum(n11 - 1, k_z, y_m, x_m));
 
-  computation init_variance("init_variance", {n, k_z, y, x}, conv1(n, k_z, y, x, 0, 0, 0) * conv1(n, k_z, y, x, 0, 0, 0));
+  //calculate the sum of the input values squared in parallel
+  computation init_sumSquared("init_sumSquared", {n, k_z, y, x}, conv1(n, k_z, y, x, 0, 0, 0) * conv1(n, k_z, y, x, 0, 0, 0));
+  computation x_sumSquared("x_sumSquared", {n, k_z, y, x11}, init_sumSquared(n, k_z, y, x11) + init_sumSquared(n, k_z, y, x11 - 1));
+  computation y_sumSquared("y_sumSquared", {n, k_z, y11, x_m}, x_sumSquared(n, k_z, y11, x_m) + x_sumSquared(n, k_z, y11 - 1, x_m));
+  computation sumSquared("sumSquared", {n11, k_z, y_m, x_m}, y_sumSquared(n11, k_z, y_m, x_m) + y_sumSquared(n11 - 1, k_z, y_m, x_m));
 
-  computation x_variance("x_variance", {n, k_z, y, x11}, init_variance(n, k_z, y, x11) + init_variance(n, k_z, y, x11 - 1));
-  computation y_variance("y_variance", {n, k_z, y11, x_m}, x_variance(n, k_z, y11, x_m) + x_variance(n, k_z, y11 - 1, x_m));
-  computation variance("variance", {n11, k_z, y_m, x_m}, y_variance(n11, k_z, y_m, x_m) + y_variance(n11 - 1, k_z, y_m, x_m));
-
-  computation bn1("bn1", {n, k_z, y, x}, (conv1(n, k_z, y, x, 0, 0, 0) - mean(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS)) / expr(o_sqrt, variance(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS) - (mean(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS)) * (mean(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS))));
+  computation bn1("bn1", {n, k_z, y, x}, (conv1(n, k_z, y, x, 0, 0, 0) - sum(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS)) / expr(o_sqrt, sumSquared(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS) - (sum(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS)) * (sum(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS))));
 
   // Relu computation
   computation relu("relu", {n, k_z, y, x}, expr(o_max, cast(p_float64, 0), bn1(n, k_z, y, x)));
@@ -64,48 +76,49 @@ int main(int argc, char **argv)
   computation conv2("conv2", {n, k_z, y, x, k_z1, k_y, k_x}, init_conv2(n, k_z, y, x) + filter2(k_z, k_z1, k_y, k_x) * reluPadd(n, k_z1, y + k_y, x + k_x));
 
   // BN2 computation
-  computation init_mean2("init_mean2", {n, k_z, y, x}, conv2(n, k_z, y, x, 0, 0, 0));
-  computation x_mean2("x_mean2", {n, k_z, y, x11}, init_mean2(n, k_z, y, x11) + init_mean2(n, k_z, y, x11 - 1));
-  computation y_mean2("y_mean2", {n, k_z, y11, x_m}, x_mean2(n, k_z, y11, x_m) + x_mean2(n, k_z, y11 - 1, x_m));
-  computation mean2("mean2", {n11, k_z, y_m, x_m}, y_mean2(n11, k_z, y_m, x_m) + y_mean2(n11 - 1, k_z, y_m, x_m));
+  //calculate the sum of the input values in parallel
+  computation init_sum2("init_sum2", {n, k_z, y, x}, conv2(n, k_z, y, x, 0, 0, 0));
+  computation x_sum2("x_sum2", {n, k_z, y, x11}, init_sum2(n, k_z, y, x11) + init_sum2(n, k_z, y, x11 - 1));
+  computation y_sum2("y_sum2", {n, k_z, y11, x_m}, x_sum2(n, k_z, y11, x_m) + x_sum2(n, k_z, y11 - 1, x_m));
+  computation sum2("sum2", {n11, k_z, y_m, x_m}, y_sum2(n11, k_z, y_m, x_m) + y_sum2(n11 - 1, k_z, y_m, x_m));
 
-  computation init_variance2("init_variance2", {n, k_z, y, x}, conv2(n, k_z, y, x, 0, 0, 0) * conv2(n, k_z, y, x, 0, 0, 0));
+  //calculate the sum of the input values squared in parallel
+  computation init_sumSquared2("init_sumSquared2", {n, k_z, y, x}, conv2(n, k_z, y, x, 0, 0, 0) * conv2(n, k_z, y, x, 0, 0, 0));
+  computation x_sumSquared2("x_sumSquared2", {n, k_z, y, x11}, init_sumSquared2(n, k_z, y, x11) + init_sumSquared2(n, k_z, y, x11 - 1));
+  computation y_sumSquared2("y_sumSquared2", {n, k_z, y11, x_m}, x_sumSquared2(n, k_z, y11, x_m) + x_sumSquared2(n, k_z, y11 - 1, x_m));
+  computation sumSquared2("sumSquared2", {n11, k_z, y_m, x_m}, y_sumSquared2(n11, k_z, y_m, x_m) + y_sumSquared2(n11 - 1, k_z, y_m, x_m));
 
-  computation x_variance2("x_variance2", {n, k_z, y, x11}, init_variance2(n, k_z, y, x11) + init_variance2(n, k_z, y, x11 - 1));
-  computation y_variance2("y_variance2", {n, k_z, y11, x_m}, x_variance2(n, k_z, y11, x_m) + x_variance2(n, k_z, y11 - 1, x_m));
-  computation variance2("variance2", {n11, k_z, y_m, x_m}, y_variance2(n11, k_z, y_m, x_m) + y_variance2(n11 - 1, k_z, y_m, x_m));
-
-  computation bn2("bn2", {n, k_z, y, x}, (conv2(n, k_z, y, x, 0, 0, 0) - mean2(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS)) / expr(o_sqrt, variance2(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS) - (mean2(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS)) * (mean2(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS))));
+  computation bn2("bn2", {n, k_z, y, x}, (conv2(n, k_z, y, x, 0, 0, 0) - sum2(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS)) / expr(o_sqrt, sumSquared2(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS) - (sum2(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS)) * (sum2(C_BATCH_SIZE - 1, k_z, C_N - 1, C_NX - 1) / cast(p_float64, C_NB_ELEMENTS))));
   init_input.after(inputPadd, 3);
 
   init_conv1.after(init_input, 3);
 
   conv1.after(init_conv1, 0);
 
-  init_mean.tag_parallel_level(0);
-  init_mean.after(conv1, 0);
+  init_sum.tag_parallel_level(0);
+  init_sum.after(conv1, 0);
 
-  x_mean.tag_parallel_level(0);
-  x_mean.after(init_variance, 3);
+  x_sum.tag_parallel_level(0);
+  x_sum.after(init_sumSquared, 3);
 
-  y_mean.tag_parallel_level(0);
-  y_mean.after(x_variance, 2);
+  y_sum.tag_parallel_level(0);
+  y_sum.after(x_sumSquared, 2);
 
-  mean.after(y_variance, computation::root_dimension);
+  sum.after(y_sumSquared, computation::root_dimension);
 
-  init_variance.tag_parallel_level(0);
-  init_variance.after(init_mean, 3);
+  init_sumSquared.tag_parallel_level(0);
+  init_sumSquared.after(init_sum, 3);
 
-  x_variance.tag_parallel_level(0);
-  x_variance.after(x_mean, 3);
+  x_sumSquared.tag_parallel_level(0);
+  x_sumSquared.after(x_sum, 3);
 
-  y_variance.tag_parallel_level(0);
-  y_variance.after(y_mean, 2);
+  y_sumSquared.tag_parallel_level(0);
+  y_sumSquared.after(y_sum, 2);
 
-  variance.after(mean, computation::root_dimension);
+  sumSquared.after(sum, computation::root_dimension);
 
   //bn1.tag_parallel_level(0);
-  bn1.after(variance, computation::root_dimension);
+  bn1.after(sumSquared, computation::root_dimension);
   bn1.tag_unroll_level(3);
   relu.after(bn1, 3);
 
@@ -121,30 +134,30 @@ int main(int argc, char **argv)
   conv2.tag_parallel_level(0);
   conv2.after(init_conv2, 2);
 
-  init_mean2.tag_parallel_level(0);
-  init_mean2.after(conv2, 3);
+  init_sum2.tag_parallel_level(0);
+  init_sum2.after(conv2, 3);
 
-  x_mean2.tag_parallel_level(0);
-  x_mean2.after(init_variance2, 3);
+  x_sum2.tag_parallel_level(0);
+  x_sum2.after(init_sumSquared2, 3);
 
-  //y_mean2.tag_parallel_level(0);
-  y_mean2.after(x_variance2, 2);
+  //y_sum2.tag_parallel_level(0);
+  y_sum2.after(x_sumSquared2, 2);
 
-  mean2.after(y_variance2, computation::root_dimension);
+  sum2.after(y_sumSquared2, computation::root_dimension);
 
-  //init_variance2.tag_parallel_level(0);
-  init_variance2.after(init_mean2, computation::root_dimension);
+  //init_sumSquared2.tag_parallel_level(0);
+  init_sumSquared2.after(init_sum2, computation::root_dimension);
 
-  //x_variance2.tag_parallel_level(0);
-  x_variance2.after(x_mean2, 3);
+  //x_sumSquared2.tag_parallel_level(0);
+  x_sumSquared2.after(x_sum2, 3);
 
-  //y_variance2.tag_parallel_level(0);
-  y_variance2.after(y_mean2, 2);
+  //y_sumSquared2.tag_parallel_level(0);
+  y_sumSquared2.after(y_sum2, 2);
 
-  variance2.after(mean2, computation::root_dimension);
+  sumSquared2.after(sum2, computation::root_dimension);
 
   bn2.tag_parallel_level(0);
-  bn2.after(variance2, computation::root_dimension);
+  bn2.after(sumSquared2, computation::root_dimension);
   bn2.tag_unroll_level(3);
 
   buffer parameters_buf("parameters_buf", {2}, p_int32, a_input);
@@ -155,9 +168,9 @@ int main(int argc, char **argv)
   buffer conv1_buf("conv1_buf", {C_BATCH_SIZE, 64, C_N, C_N}, p_float64, a_output);
   buffer reluPadd_buf("reluPadd_buf", {C_BATCH_SIZE, 64, C_N_PAD, C_N_PAD}, p_float64, a_output);
   buffer conv2_buf("conv2_buf", {C_BATCH_SIZE, 64, C_N, C_N}, p_float64, a_output);
-  buffer mean_buff("mean_buff", {C_BATCH_SIZE, 64, C_N, C_N}, p_float64, a_output);
+  buffer sum_buff("sum_buff", {C_BATCH_SIZE, 64, C_N, C_N}, p_float64, a_output);
   buffer bn1_buf("bn1_buf", {C_BATCH_SIZE, 64, C_N, C_N}, p_float64, a_output);
-  buffer variance_buff("variance_buff", {C_BATCH_SIZE, 64, C_N, C_N}, p_float64, a_output);
+  buffer sumSquared_buff("sumSquared_buff", {C_BATCH_SIZE, 64, C_N, C_N}, p_float64, a_output);
   buffer bn2_buf("bn2_buf", {C_BATCH_SIZE, 64, C_N, C_N}, p_float64, a_output);
 
   c_input.store_in(&input_buf);
@@ -170,14 +183,14 @@ int main(int argc, char **argv)
   init_conv1.store_in(&conv1_buf);
   conv1.store_in(&conv1_buf, {n, k_z, y, x});
 
-  init_mean.store_in(&mean_buff);
-  mean.store_in(&mean_buff);
-  x_mean.store_in(&mean_buff);
-  y_mean.store_in(&mean_buff);
-  init_variance.store_in(&variance_buff);
-  x_variance.store_in(&variance_buff);
-  y_variance.store_in(&variance_buff);
-  variance.store_in(&variance_buff);
+  init_sum.store_in(&sum_buff);
+  sum.store_in(&sum_buff);
+  x_sum.store_in(&sum_buff);
+  y_sum.store_in(&sum_buff);
+  init_sumSquared.store_in(&sumSquared_buff);
+  x_sumSquared.store_in(&sumSquared_buff);
+  y_sumSquared.store_in(&sumSquared_buff);
+  sumSquared.store_in(&sumSquared_buff);
   bn1.store_in(&bn1_buf);
   relu.store_in(&bn1_buf);
 
@@ -186,16 +199,16 @@ int main(int argc, char **argv)
   init_conv2.store_in(&conv2_buf);
   conv2.store_in(&conv2_buf, {n, k_z, y, x});
 
-  init_mean2.store_in(&mean_buff);
-  mean2.store_in(&mean_buff);
-  x_mean2.store_in(&mean_buff);
-  y_mean2.store_in(&mean_buff);
-  init_variance2.store_in(&variance_buff);
-  x_variance2.store_in(&variance_buff);
-  y_variance2.store_in(&variance_buff);
-  variance2.store_in(&variance_buff);
+  init_sum2.store_in(&sum_buff);
+  sum2.store_in(&sum_buff);
+  x_sum2.store_in(&sum_buff);
+  y_sum2.store_in(&sum_buff);
+  init_sumSquared2.store_in(&sumSquared_buff);
+  x_sumSquared2.store_in(&sumSquared_buff);
+  y_sumSquared2.store_in(&sumSquared_buff);
+  sumSquared2.store_in(&sumSquared_buff);
   bn2.store_in(&bn2_buf);
 
-  tiramisu::codegen({&parameters_buf, &filter_buf, &filter2_buf, &input_buf, &inputPadd_buf, &conv1_buf, &mean_buff, &variance_buff, &bn1_buf, &reluPadd_buf, &conv2_buf, &bn2_buf}, "generated_fused_resnet_block.o");
+  tiramisu::codegen({&parameters_buf, &filter_buf, &filter2_buf, &input_buf, &inputPadd_buf, &conv1_buf, &sum_buff, &sumSquared_buff, &bn1_buf, &reluPadd_buf, &conv2_buf, &bn2_buf}, "generated_fused_resnet_block.o");
   return 0;
 }
