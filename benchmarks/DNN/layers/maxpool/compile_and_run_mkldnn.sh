261c7f9bb2414f0e8cb9d686e54d8d22e6728aa0
Radja <er_hachilif@esi.dz>
2019-01-26 01:27:20 +0100

    newline deleted

--- benchmarks/DNN/layers/maxpool/compile_and_run_mkldnn.sh
+++ benchmarks/DNN/layers/maxpool/compile_and_run_mkldnn.sh
@@ -10,4 +10,3 @@ echo "MKL maxpool "
 
 g++ -std=c++11 -fopenmp -I${MKL_PREFIX}/include -L${MKL_PREFIX}/lib maxpool_layer_generator_mkldnn.cpp -lmkldnn  -o maxpool_layer_mkldnn_result
 LD_LIBRARY_PATH=${MKL_PREFIX}/build/src/:$LD_LIBRARY_PATH  ./maxpool_layer_mkldnn_result
- 
