d61f29257fd2456a3d6b1db93d35656df6aec7ed
Radja <er_hachilif@esi.dz>
2019-01-26 13:09:57 +0100

    Delete compile_and_run_mkldnn.sh

--- benchmarks/DNN/blocks/fusedresNet/compile_and_run_mkldnn.sh
+++
@@ -1,7 +0,0 @@
-#set -x
-
-source ../../../configure_paths.sh
-
-echo "MKL-DNN fused_resnet"
-g++ -std=c++11 -fopenmp -I${MKL_PREFIX}/include -L${MKL_PREFIX}/lib fused_resnet_block_generator_mkldnn.cpp -lmkldnn -o fused_resnetBlock_mkldnn_result
-LD_LIBRARY_PATH=${MKL_PREFIX}/build/src/:$LD_LIBRARY_PATH ./fused_resnetBlock_mkldnn_result
