f3fbc0b93ef45e436e3ab4baca083b5576b3ce39
Radja <er_hachilif@esi.dz>
2019-01-26 01:30:05 +0100

    Update compile_and_run_mkldnn.sh

--- benchmarks/DNN/layers/maxpool/compile_and_run_mkldnn.sh
+++ benchmarks/DNN/layers/maxpool/compile_and_run_mkldnn.sh
@@ -2,11 +2,9 @@
 
 source ../../../configure_paths.sh
 
-rm -rf _results
-
 echo
 echo
-echo "MKL maxpool "
+echo "MKL-DNN maxpool "
 
 g++ -std=c++11 -fopenmp -I${MKL_PREFIX}/include -L${MKL_PREFIX}/lib maxpool_layer_generator_mkldnn.cpp -lmkldnn  -o maxpool_layer_mkldnn_result
 LD_LIBRARY_PATH=${MKL_PREFIX}/build/src/:$LD_LIBRARY_PATH  ./maxpool_layer_mkldnn_result
