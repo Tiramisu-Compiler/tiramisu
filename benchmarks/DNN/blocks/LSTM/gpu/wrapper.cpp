951690482fa20d560f8bb607ee2b88f6c3beb420
Abdurrahman Akkas <akkas@mit.edu>
2019-02-13 12:41:40 -0500

    LSTM gpu correctness progress

--- benchmarks/DNN/blocks/LSTM/gpu/wrapper.cpp
+++ benchmarks/DNN/blocks/LSTM/gpu/wrapper.cpp
@@ -25,41 +25,67 @@ int main(int argc, char *argv[])
     float *raw_biases = (float*) malloc(4 * feature_size * num_layers * sizeof(float));
     float *raw_x = (float*) malloc(feature_size * batch_size * seq_length * sizeof(float));
 
-    // Initialize inputs
-    // TODO: Test correctness with non-zero inputs
-    for (int i = 0; i < num_layers; i++) {
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 4 * feature_size; k++) {
-                for (int l = 0; l < feature_size; l++) {
-                    raw_Weights[((i * 2 + j) * 4 * feature_size + k) * feature_size + l] = 0;
-                }
-            }
-        }
-    }
-    for (int i = 0; i < num_layers; i++) {
-        for (int j = 0; j < 4 * feature_size; j++) {
-            raw_biases[i * 4 * feature_size + j] = 0;
-        }
-    }
-    for (int i = 0; i < seq_length; i++) {
-        for (int j = 0; j < batch_size; j++) {
-            for (int k = 0; k < feature_size; k++) {
-                raw_Weights[(i * batch_size + j) * feature_size + k] = 0;
-            }
-        }
-    }
-
     // Note that indices are flipped (see tutorial 2)
     Halide::Buffer<int32_t> buf_params(4);
     Halide::Buffer<float> buf_Weights(raw_Weights, {feature_size, 4 * feature_size, 2, num_layers});
     Halide::Buffer<float> buf_biases(raw_biases, {4 * feature_size, num_layers});
     Halide::Buffer<float> buf_x(raw_x, {feature_size, batch_size, seq_length});
     Halide::Buffer<float> buf_y(feature_size, batch_size, seq_length);
+    Halide::Buffer<float> buf_ref_y(feature_size, batch_size, seq_length);
 
+    // Initialize inputs
     buf_params(0) = feature_size;
     buf_params(1) = batch_size;
     buf_params(2) = num_layers;
     buf_params(3) = seq_length;
+
+    std::srand(0);
+    for (int i = 0; i < num_layers; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 4 * feature_size; k++) {
+                for (int l = 0; l < feature_size; l++) {
+                    buf_Weights(l, k, j, i) = (std::rand() % 200 - 100) / 100.;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < num_layers; i++) {
+        for (int j = 0; j < 4 * feature_size; j++) {
+            buf_biases(j, i) = (std::rand() % 200 - 100) / 100.;
+        }
+    }
+    for (int i = 0; i < seq_length; i++) {
+        for (int j = 0; j < batch_size; j++) {
+            for (int k = 0; k < feature_size; k++) {
+                buf_x(k, j, i) = (std::rand() % 200 - 100) / 100.;
+            }
+        }
+    }
+
+    lstm(buf_params.raw_buffer(),
+         buf_Weights.raw_buffer(),
+         buf_biases.raw_buffer(),
+         buf_x.raw_buffer(),
+         buf_y.raw_buffer());
+
+    lstm_ref(buf_params.raw_buffer(),
+         buf_Weights.raw_buffer(),
+         buf_biases.raw_buffer(),
+         buf_x.raw_buffer(),
+         buf_ref_y.raw_buffer());
+
+    /* Todo: result mismatch
+    int nn = 0;
+    for (int i = 0; i < seq_length; i++) {
+        for (int j = 0; j < batch_size; j++) {
+            for (int k = 0; k < feature_size; k++) {
+                if (buf_y(k, j, i) != buf_ref_y(k, j, i) && nn++ < 100) {
+                    std::cout << i << " " << j << " " << k << " " << buf_y(k, j, i) << " " << buf_ref_y(k, j, i) << std::endl;
+                }
+            }
+        }
+    }
+    */
 
     for (int i = 0; i < warmupN; i++) {
         lstm(buf_params.raw_buffer(),
--- benchmarks/DNN/blocks/LSTM/gpu/wrapper.h
+++ benchmarks/DNN/blocks/LSTM/gpu/wrapper.h
@@ -9,6 +9,12 @@ int lstm(halide_buffer_t *b1,
          halide_buffer_t *b4,
          halide_buffer_t *b5);
 
+int lstm_ref(halide_buffer_t *b1,
+         halide_buffer_t *b2,
+         halide_buffer_t *b3,
+         halide_buffer_t *b4,
+         halide_buffer_t *b5);
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
