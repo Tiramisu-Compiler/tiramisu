4193a2a2e91721cdfedbeb6365923233d6560f08
Thinhinane Ihadadene <et_ihadadene@esi.dz>
2019-03-05 17:22:47 +0100

    update blurxyautodist bench

--- benchmarks/halide/wrapper_blurxyautodist.cpp
+++ benchmarks/halide/wrapper_blurxyautodist.cpp
@@ -1,78 +1,74 @@
 #include "wrapper_blurxyautodist.h"
-#include "../benchmarks.h"
-#include "../../include/tiramisu/mpi_comm.h"
-#include "tiramisu/utils.h"
+#include "Halide.h"
+
+#include <tiramisu/utils.h>
+#include <tiramisu/mpi_comm.h>
 #include <cstdlib>
 #include <iostream>
 
-#define NB_TESTS 30
-#define CHECK_CORRECTNESS 1
-#define NODES 10
+int main() {
 
-int main(int, char**) {
+#ifdef WITH_MPI
 
-    int rank = tiramisu_MPI_init();
-    std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
-    std::vector<std::chrono::duration<double,std::milli>> duration_vector_2;
+  std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
+  std::vector<std::chrono::duration<double,std::milli>> duration_vector_2;
 
-    // Allocate buffers for each rank and fill as appropriate
-    Halide::Buffer<uint32_t> input(_COLS, _ROWS/NODES + 2, "input");
-    Halide::Buffer<uint32_t> output(_COLS, _ROWS/NODES, "output");
+  int rank = tiramisu_MPI_init();
 
-    for (int r = 0; r < _ROWS/NODES; r++) {
-      // Repeat data at the edge of the columns
-      for (int c = 0; c < _COLS; c++) {
-        input(c,r) = r + c; // could fill this with anything
-      }
+  Halide::Buffer<uint32_t> input(_COLS + 2, _ROWS/10, "input");
+
+  for (int r = 0; r < _ROWS/10; r++) {
+    for (int c = 0; c < _COLS+2; c++) {
+      input(c,r) = r + c;
     }
-    if (rank == NODES -1) {
-      // Since the last node doesn't receive any data, we need to repeat the data at the edge of the rows
-      uint32_t v = _ROWS/NODES;
-      for (int r = _ROWS/NODES; r < _ROWS/NODES + 2; r++) {
+  }
+  if (rank == 9) {
+    uint32_t v = _ROWS/10;
+    for (int r = _ROWS/10; r < _ROWS/10 + 2; r++) {
+      for (int c = 0; c < _COLS + 2; c++) {
+        input(c,r) = v + c;
+      }
+      v++;
+    }
+  }
+
+  Halide::Buffer<uint32_t> output(_COLS, _ROWS/10, "output");
+  Halide::Buffer<uint32_t> ref(_COLS, _ROWS/10, "ref");
+  init_buffer(output, (uint32_t)0);
+
+  blurxyautodist_tiramisu(input.raw_buffer(), output.raw_buffer());
+  // Tiramisu
+  for (int i=0; i<NB_TESTS; i++)
+  {
+      MPI_Barrier(MPI_COMM_WORLD);
+      auto start1 = std::chrono::high_resolution_clock::now();
+      ticket_tiramisu(output1.raw_buffer());
+      auto end1 = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double,std::milli> duration1 = end1 - start1;
+      duration_vector_1.push_back(duration1);
+  }
+   MPI_Barrier(MPI_COMM_WORLD);
+
+  if(CHECK_CORRECTNESS){
+      for (int r = 0; r < _ROWS/10; r++) {
         for (int c = 0; c < _COLS; c++) {
-          // mimic transferred data
-          input(c,r) = v + c;
+          ref(c,r) = (input(c,r) + input(c,r+1) + input(c,r+2) + input(c+1, r) + input(c+1, r+1) + input(c+1, r+2) + input(c+2, r)
+                      + input(c+2, r+1) + input(c+2, r+2)) / 9;
         }
-        v++;
       }
-    }
 
-    // Warm up code.
-    blurxyautodist_tiramisu(input.raw_buffer(), output.raw_buffer());
-    // Tiramisu
-    for (int i=0; i<NB_TESTS; i++)
-    {
-        MPI_Barrier(MPI_COMM_WORLD);
-        auto start1 = std::chrono::high_resolution_clock::now();
-        blurxyautodist_tiramisu(input.raw_buffer(), output.raw_buffer());
-        auto end1 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double,std::milli> duration1 = end1 - start1;
-        duration_vector_1.push_back(duration1);
-    }
-    MPI_Barrier(MPI_COMM_WORLD);
+      compare_buffers("blurxyautodist_" + std::to_string(rank), output, ref);
+  }
 
-    if(CHECK_CORRECTNESS) {
+  if(rank == 0) {
+      print_time("performance_CPU.csv", "blurxyautodist",
+                 {"Tiramisu", "Halide"},
+                 {median(duration_vector_1),0});
+  }
 
-        Halide::Buffer<uint32_t> ref(_COLS, _ROWS/NODES, "ref");
-        for (int r = 0; r < _ROWS/NODES; r++) {
-          for (int c = 0; c < _COLS; c++) {
-            ref(c,r) = (input(c,r) + input(c,r+1) + input(c,r+2) + input(c+1, r) + input(c+1, r+1) + input(c+1, r+2) + input(c+2, r)
-                        + input(c+2, r+1) + input(c+2, r+2)) / 9;
-          }
-        }
 
-        // Compare the results for this rank
-        compare_buffers("Distributed blurxy " + std::to_string(rank), output, ref);
-    }
+  tiramisu_MPI_cleanup();
+#endif
 
-    if (rank == 0) {
-        print_time("performance_CPU.csv", "cvtcolor",
-                   {"Tiramisu", "Node"},
-                   {median(duration_vector_1), 0});
-
-        std::cout << "Distributed blurxy passed" << std::endl;
-    }
-
-    tiramisu_MPI_cleanup();
-    return 0;
+  return 0;
 }
--- benchmarks/halide/wrapper_blurxyautodist.h
+++ benchmarks/halide/wrapper_blurxyautodist.h
@@ -1,29 +1,21 @@
-#ifndef HALIDE__build___wrapper_blurxyautodist_o_h
-#define HALIDE__build___wrapper_blurxyautodist_o_h
+#ifndef HALIDE__build___blurxyautodist_o_h
+#define HALIDE__build___blurxyautodist_o_h
 
 #include <tiramisu/utils.h>
+
 #define _ROWS 100
 #define _COLS 10
-
-#ifndef NODES
-#define NODES 10
-#endif
-
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int blurxyautodist_tiramisu(halide_buffer_t *_b_input_buffer, halide_buffer_t *_b_blury_buffer);
-int blurxyautodist_tiramisu_argv(void **args);
-int blurxyautodist_ref(halide_buffer_t *_b_input_buffer, halide_buffer_t *_b_blury_buffer);
-int blurxyautodist_ref_argv(void **args);
+int blurxyautodist_tiramisu(halide_buffer_t *_p0_buffer, halide_buffer_t *_p1_buffer);
+int blurxyautodist_ref(void **args);
 
-// Result is never null and points to constant static data
-const struct halide_filter_metadata_t *blurxyautodist_tiramisu_metadata();
-const struct halide_filter_metadata_t *blurxyautodist_ref_metadata();
-
+extern const struct halide_filter_metadata_t halide_pipeline_aot_metadata;
 #ifdef __cplusplus
 }  // extern "C"
 #endif
-#endif
+
+#endif //TIRAMISU_WRAPPER_TUTORIAL_12_H
