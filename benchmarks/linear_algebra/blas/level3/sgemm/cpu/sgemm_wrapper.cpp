17192efcb69ec66989e07acf243bd5e8ff15c44e
Radja <er_hachilif@esi.dz>
2019-03-12 00:46:49 +0100

    Update sgemm_wrapper.cpp

--- benchmarks/linear_algebra/blas/level3/sgemm/cpu/sgemm_wrapper.cpp
+++ benchmarks/linear_algebra/blas/level3/sgemm/cpu/sgemm_wrapper.cpp
@@ -21,7 +21,7 @@ int main(int, char **)
     std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
     std::vector<std::chrono::duration<double,std::milli>> duration_vector_2;
 
-float a = 3, b = 3;
+    float a = 3, b = 3;
 
 
 #if 1
@@ -38,6 +38,7 @@ float a = 3, b = 3;
     bool run_mkl = true;
     bool run_tiramisu = true;
 #endif
+
 
 	int sizes[27][4], D = 1060 * 1060; 
 
@@ -223,56 +224,56 @@ float a = 3, b = 3;
 		}
 		rmaxc = local_M + 1;
 		cmaxc = local_N;
-		if (layout == CblasRowMajor) {
-			lda=cmaxa;
-			ldb=cmaxb;
-			ldc=cmaxc;
-		} else {
-			lda=rmaxa;
-			ldb=rmaxb;
-			ldc=rmaxc;
-		}
-
-		for (int i = 0; i < NB_TESTS; i++)
-		{
-			init_buffer(C_mkl, (float)1);
-			auto start1 = std::chrono::high_resolution_clock::now();
-			if (run_mkl == true)
-				cblas_sgemm(layout, transA, transB, local_M, local_N, local_K, a, (float *) A.raw_buffer()->host, lda, (float *) B.raw_buffer()->host, ldb, b, (float *) C_mkl.raw_buffer()->host, ldc);
-			auto end1 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double,std::milli> duration1 = end1 - start1;
-			duration_vector_1.push_back(duration1);
-		}
+	if (layout == CblasRowMajor) {
+		lda=cmaxa;
+		ldb=cmaxb;
+		ldc=cmaxc;
+	} else {
+		lda=rmaxa;
+		ldb=rmaxb;
+		ldc=rmaxc;
 	}
 
-	for (int i = 0; i < NB_TESTS; i++)
+        for (int i = 0; i < NB_TESTS; i++)
 	{
-		init_buffer(C, (float)1);
-		auto start2 = std::chrono::high_resolution_clock::now();
-		if (run_tiramisu == true)
-			sgemm_tiramisu(SIZES.raw_buffer(), alpha.raw_buffer(), beta.raw_buffer(), A.raw_buffer(), B.raw_buffer(), C.raw_buffer());
-		auto end2 = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double,std::milli> duration2 = end2 - start2;
-		duration_vector_2.push_back(duration2);
+	    init_buffer(C_mkl, (float)1);
+	    auto start1 = std::chrono::high_resolution_clock::now();
+	    if (run_mkl == true)
+	    	cblas_sgemm(layout, transA, transB, local_M, local_N, local_K, a, (float *) A.raw_buffer()->host, lda, (float *) B.raw_buffer()->host, ldb, b, (float *) C_mkl.raw_buffer()->host, ldc);
+	    auto end1 = std::chrono::high_resolution_clock::now();
+	    std::chrono::duration<double,std::milli> duration1 = end1 - start1;
+	    duration_vector_1.push_back(duration1);
 	}
+    }
 
-	print_time("performance_CPU.csv", "sgemm",
-			{"MKL", "Tiramisu"},
-			{median(duration_vector_1), median(duration_vector_2)});
+    for (int i = 0; i < NB_TESTS; i++)
+    {
+	    init_buffer(C, (float)1);
+	    auto start2 = std::chrono::high_resolution_clock::now();
+ 	    if (run_tiramisu == true)
+	    	sgemm_tiramisu(SIZES.raw_buffer(), alpha.raw_buffer(), beta.raw_buffer(), A.raw_buffer(), B.raw_buffer(), C.raw_buffer());
+	    auto end2 = std::chrono::high_resolution_clock::now();
+	    std::chrono::duration<double,std::milli> duration2 = end2 - start2;
+	    duration_vector_2.push_back(duration2);
+    }
 
-	if (CHECK_CORRECTNESS)
-	if (run_mkl == 1 && run_tiramisu == 1)
+    print_time("performance_CPU.csv", "sgemm",
+               {"MKL", "Tiramisu"},
+               {median(duration_vector_1), median(duration_vector_2)});
+
+    if (CHECK_CORRECTNESS)
+ 	if (run_mkl == 1 && run_tiramisu == 1)
 	{
 		compare_buffers("sgemm", C, C_mkl);
-	}
+        }
 
-	if (PRINT_OUTPUT)
-	{
+    if (PRINT_OUTPUT)
+    {
 	std::cout << "Tiramisu sgemm " << std::endl;
 	print_buffer(C);
 	std::cout << "MKL sgemm " << std::endl;
 	print_buffer(C_mkl);
-	}
-	}
-	return 0;
+    }
+    }
+    return 0;
 }
