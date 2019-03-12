6b95737e9f84e57b36da7d497f2f37bf4d60635d
Thinhinane Ihadadene <et_ihadadene@esi.dz>
2019-03-12 18:50:42 +0100

    update documentation on core.h

--- include/tiramisu/core.h
+++ include/tiramisu/core.h
@@ -2366,23 +2366,23 @@ private:
 
     /**
       * \brief Return a unique identifier for the communication based on the type of
-      * the rank and a sequential number.
+      * ank and a sequential number.
       *
-      * All communication of a computation c will have the same prefix b_c followed
-      * by the sequential number i and then the type of communication r_snd or r_rcv
+      * All communications of a computation c will have the same prefix b_c followed
+      * by the sequential number i and then the type of communication r_snd or r_rcv.
       *
       */
     std::string get_comm_id(rank_t rank_type, int i);
 
     /**
-      * Return the itreation domain of the communication set which can be either
+      * Return the iteration domain of the communication set which can be either
       * a send iteration domain or a receive iteration domain.
-      * set is an exchange set which we transform to a recv/send it_dom
+      * set is an exchange set which we transform to a recv/send it_dom.
       */
     isl_set* construct_comm_set(isl_set* set, rank_t rank_type, int communication_id);
 
     /**
-      * \brief Return a unordred_map comp_name, set_to_exchange
+      * \brief Return an unordred_map comp_name, set_to_exchange.
       *
       * The set to exchange expresses a subset of the iteration domain of the computation comp_name
       * that needs to be sent by r_sender which owns the data and received by r_receiver
@@ -2390,9 +2390,14 @@ private:
       */
     std::unordered_map<std::string, isl_set*> construct_exchange_sets();
 
-
+    /**
+      * \brief Generate distributed communication code.
+      *
+      * Given the iteration domain of send and receive, this function creates xfers, schedules them,
+      * and handles the storage of the receives.
+      * Currently, this process works for programs that distribute the outermost loop.
+      */
     void gen_communication_code(isl_set*recv_it, isl_set* send_it, int communication_id, std::string computation_name);
-    std::string create_send_access_string(int send_id, std::vector<tiramisu::expr> iterators, std::string computation_name);
 
 protected:
 
