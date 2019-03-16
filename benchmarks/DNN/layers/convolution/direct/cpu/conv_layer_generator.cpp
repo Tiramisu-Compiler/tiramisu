99bf011fd12b596a445e0d128cf06650875ce92f
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2019-03-16 10:45:49 -0400

    Add different sizes to conv

--- benchmarks/DNN/layers/convolution/direct/cpu/conv_layer_generator.cpp
+++ benchmarks/DNN/layers/convolution/direct/cpu/conv_layer_generator.cpp
@@ -1,29 +1,13 @@
-#include <isl/set.h>
-#include <isl/union_map.h>
-#include <isl/union_set.h>
-#include <isl/ast_build.h>
-#include <isl/schedule.h>
-#include <isl/schedule_node.h>
-
-#include <tiramisu/debug.h>
-#include <tiramisu/core.h>
-#include <tiramisu/utils.h>
+#include <tiramisu/tiramisu.h>
 
 #include <string.h>
 #include "configure.h"
-
-#define SCHEDULE_CPU 1
 
 using namespace tiramisu;
 
 int main(int argc, char **argv)
 {
-
     init("conv_tiramisu");
-
-    // -------------------------------------------------------
-    // Layer I
-    // -------------------------------------------------------
 
     // N: parameters[0]
     // K: parameters[1]
@@ -34,27 +18,26 @@ int main(int argc, char **argv)
     var i("i", 0, 5);
     input parameters("parameters",{i}, p_int32);
 
-    constant C_N("C_N", parameters(0)+ parameters(1));
-    constant C_N1("C_N1",parameters(0));
+    constant N_INPUT("N_INPUT", parameters(0) + parameters(1)); // The input is padded, so its size is N + K instead of N.
+    constant C_N("C_N", parameters(0));
     constant C_K("C_K", parameters(1));
     constant C_FIn("C_FIn", parameters(2));
     constant C_FOut("C_FOut", parameters(3));
     constant C_BATCH_SIZE("C_BATCH_SIZE", parameters(4));
 
-    var x("x", 0, C_N ), y("y", 0, C_N),  z("z", 0, C_FOut), n("n", 0, C_BATCH_SIZE ); // input
-    var k_x("k_x",0,C_K), k_y("k_y",0,C_K), k_z("k_z",0,C_FIn); // filter variables
-    var x1("x1", 0, C_N1), y1("y1", 0, C_N1); // conv
+    var x("x", 0, C_N), y("y", 0, C_N), z("z", 0, C_FOut), n("n", 0, C_BATCH_SIZE ); // Iterators for the conv computations.
+    var k_x("k_x", 0, C_K), k_y("k_y", 0, C_K), k_z("k_z", 0, C_FIn); // Iterators for the kernel (filter).
 
     // Input computations
-    input c_input("c_input",{n, k_z, y, x} , p_float32);
-    input bias("bias", {z}, p_float32);
+    input c_input("c_input",{"n", "k_z", "y", "x"}, {C_BATCH_SIZE, C_FIn, N_INPUT, N_INPUT} , p_float32);
+    input bias("bias", {"z"}, {C_FOut}, p_float32);
     input filter("filter", {z, k_z , k_y, k_x}, p_float32);
 
     // First conv computations
-    computation conv_init("conv_init",{n, z, y1, x1}, bias(z) );
-    computation conv("conv",{n, z, y1, x1, k_z, k_y, k_x }, conv_init(n, z, y1, x1) + filter(z, k_z, k_y, k_x) * c_input(n, k_z, y1 + k_y, x1 + k_x));
+    computation conv_init("conv_init",{n, z, y, x}, bias(z));
+    computation conv("conv",{n, z, y, x, k_z, k_y, k_x }, conv_init(n, z, y, x) + filter(z, k_z, k_y, k_x) * c_input(n, k_z, y + k_y, x + k_x));
     
-    global::get_implicit_function()->add_context_constraints("[C_N, C_K, C_FIn, C_FOut, C_BATCH_SIZE]->{:C_N>1 and C_K>1 and C_FOut>1 and C_FIn>0 and C_BATCH_SIZE>1 and C_K=5 and C_FIn%16=0 and C_N%16=0}");
+    global::get_implicit_function()->add_context_constraints("[C_N, C_K, C_FIn, C_FOut, C_BATCH_SIZE]->{:C_N>1 and C_K>1 and C_FOut>1 and C_FIn>0 and C_BATCH_SIZE>1 and C_K=5}"); // C_FIn%16=0 and C_N%16=0}");
 
     // Layer II
     int vec_len;
@@ -74,38 +57,40 @@ int main(int argc, char **argv)
             o_block = 4;
     }
 
-    conv_init.tag_parallel_level(0);
-    conv.after(conv_init, 2);
+    conv_init.then(conv, y);
+
+    conv_init.tag_parallel_level(n);
 
     // 0, 1,   2,   3,   4,   5,     6,
-    // n, z,   y,   x, r_z, r_y,   r_x,
-    conv.interchange(3, 4);
-    // n, z,   y, (r_z,   x), r_y,   r_x,
-    conv.interchange(3, 2);
-    // n, z, (r_z,   y),   x, r_y,   r_x,
+    // n, z,   y,   x, k_z, k_y,   k_x,
+    conv.interchange(x, k_z);
+    // n, z,   y, (k_z,   x), k_y, k_x,
+    conv.interchange(y, k_z);
+    // n, z, (k_z,  y),   x,  k_y, k_x,
 
-    conv.split(1, o_block);
-    conv_init.split(1, o_block);
-    // n, (z, z_t), r_z,   y,       x, r_y,   r_x,
+    var z1("z1"), z2("z2");
+    conv.split(z, o_block, z1, z2);
+    conv_init.split(z, o_block, z1, z2);
+    // n, (z1, z2), k_z,   y,   x, k_y, k_x,
 
     conv.split(3, y_block);
     conv.split(6, vec_len);
-    conv.tag_vector_level(7, vec_len);
+////    conv.tag_vector_level(7, vec_len);
 
-    // n,  z, z_t,  r_z,  (y, y_t), x, r_y,   r_x,
+    // n,  z1, z2,  k_z,  (y, y_t), x, k_y, k_x,
     conv_init.split(4, vec_len);
-    conv_init.tag_vector_level(5, vec_len);
+////    conv_init.tag_vector_level(5, vec_len);
 
     if (LARGE_DATA_SET)
     {
-	    conv.tag_unroll_level(8);
-	    conv.tag_unroll_level(9);
+////	    conv.tag_unroll_level(8);
+////	    conv.tag_unroll_level(9);
     }
 
     // Layer III
     buffer parameters_buf("parameters_buf", {expr(5)}, p_int32, a_input);
-    buffer input_buf("input_buf", {expr(C_BATCH_SIZE), expr(C_FIn), expr(C_N), expr(C_N)}, p_float32, a_input);
-    buffer conv_buf("conv_buf", {expr(C_BATCH_SIZE), expr(C_FOut), expr(C_N1), expr(C_N1)}, p_float32, a_output);
+    buffer input_buf("input_buf", {expr(C_BATCH_SIZE), C_FIn, N_INPUT, N_INPUT}, p_float32, a_input);
+    buffer conv_buf("conv_buf", {expr(C_BATCH_SIZE), expr(C_FOut), expr(C_N), expr(C_N)}, p_float32, a_output);
     buffer filter_buf("filter_buf", {expr(C_FOut), expr(C_FIn), expr(C_K), expr(C_K)}, p_float32, a_input);
     buffer bias_buf("bias_buf", {expr(C_FOut)}, p_float32, a_input);
 
@@ -114,7 +99,7 @@ int main(int argc, char **argv)
     bias.store_in(&bias_buf);
     filter.store_in(&filter_buf);
     conv_init.store_in(&conv_buf);
-    conv.store_in(&conv_buf,{n, z, y1, x1});
+    conv.store_in(&conv_buf,{n, z, y, x});
 
     tiramisu::codegen({&parameters_buf, &input_buf, &filter_buf, &bias_buf, &conv_buf},"generated_conv_layer.o");
 
