b7d152abe3cd2d99753e49937b56b96e49e5cb17
Sachin D Shinde <shinde@mit.edu>
2019-05-16 22:18:32 -0400

    Remove commented code in test_1000 since shifted spmm is functional.

--- tests/test_1000.cpp
+++ tests/test_1000.cpp
@@ -5,6 +5,10 @@
 
 using namespace tiramisu;
 
+
+
+
+
 // For computation C(i, k) = A(i, j) * B(j, k)
 void gen(std::string name, int num_ranks, uint64_t i_dim_size, uint64_t j_dim_size, uint64_t k_dim_size, uint64_t comm_shift) {
 
@@ -29,7 +33,6 @@ void gen(std::string name, int num_ranks, uint64_t i_dim_size, uint64_t j_dim_si
   constant NUM_TASKS_C("NUM_TASKS_C", expr(((MAP_C_SIZE - 1) / NUM_RANKS) + 1));
   constant NUM_TASKS_A("NUM_TASKS_A", expr(((MAP_A_SIZE - 1) / NUM_RANKS) + 1));
   constant NUM_TASKS_B("NUM_TASKS_B", expr(((MAP_B_SIZE - 1) / NUM_RANKS) + 1));
-
 
   // Create vars for distributing computation of C.
   var j("j", expr((int32_t) 0), expr(J));
@@ -101,18 +104,9 @@ void gen(std::string name, int num_ranks, uint64_t i_dim_size, uint64_t j_dim_si
 
 
 
-
-
   // -------------------------------------------------------
   // Layer II
   // -------------------------------------------------------
-
-  // Distribute input blocks.
-  // a_in.split(map_a, num_ranks, tsk_a, rnk_a);
-  // b_in.split(map_b, num_ranks, tsk_b, rnk_b);
-
-  // a_in.tag_distribute_level(rnk_a);
-  // b_in.tag_distribute_level(rnk_b);
 
   // Distribute computation of output blocks.
   a_local.split(map_c, num_ranks, tsk_c, rnk_c);
@@ -154,23 +148,11 @@ void gen(std::string name, int num_ranks, uint64_t i_dim_size, uint64_t j_dim_si
   c_local.before(wait_a_send, j);
   wait_a_send.before(wait_b_send, j);
 
-  // c_local_init.before(*comm_a.s, computation::root);
-  // comm_a.s->before(*comm_b.s, j);
-  // comm_b.s->before(*comm_a.r, j);
-  // comm_a.r->before(*comm_b.r, rnk_c);
-  // comm_b.r->before(wait_a_recv, rnk_c);
-  // wait_a_recv.before(wait_b_recv, rnk_c);
-  // wait_b_recv.before(c_local, rnk_c);
-  // c_local.before(wait_a_send, j);
-  // wait_a_send.before(wait_b_send, j);
-
   // Shift the sends/recvs backward in j so they overlap with computation.
   comm_a.r->shift(j, (int32_t) -comm_shift);
   comm_b.r->shift(j, (int32_t) -comm_shift);
   comm_a.s->shift(j, (int32_t) -comm_shift);
   comm_b.s->shift(j, (int32_t) -comm_shift);
-
-
 
 
 
@@ -226,187 +208,3 @@ int main(int argc, char **argv)
   gen("spmm", _NUM_RANKS, _I_DIM_SIZE, _J_DIM_SIZE, _K_DIM_SIZE, _COMM_SHIFT);
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Code that almost worked and then didn't, but may be useful later
-////////////////////////////////////////////////////////////////////////////////
-
-  /*
-  // Manually form iteration domain point-wise for A block MPI send/recv.
-  std::string a_iter;
-  for (uint64_t j_str = 0; j_str < j_dim_size; j_str++) {
-    for (uint64_t map_c_str = 0; map_c_str < i_dim_size * k_dim_size; map_c_str++) {
-      uint64_t i_str = map_c_str / k_dim_size;
-      uint64_t k_str = map_c_str % k_dim_size;
-      uint64_t map_a_str = i_str * j_dim_size + j_str;
-      
-      uint64_t tsk_c_str = map_c_str / num_ranks;
-      uint64_t rnk_c_str = map_c_str % num_ranks;
-
-      uint64_t tsk_a_str = map_a_str / num_ranks;
-      uint64_t rnk_a_str = map_a_str % num_ranks;
-
-      if (!a_iter.empty()) {
-        a_iter += " or ";
-      }
-      a_iter += "j=" + std::to_string(j_str) + " and ";
-      a_iter += "tsk_c=" + std::to_string(tsk_c_str) + " and ";
-      a_iter += "rnk_c=" + std::to_string(rnk_c_str) + " and ";
-      a_iter += "tsk_a=" + std::to_string(tsk_a_str) + " and ";
-      a_iter += "rnk_a=" + std::to_string(rnk_a_str);
-    }
-  }
-
-  std::string a_iter_send("{a_send[j,tsk_c,rnk_c,tsk_a,rnk_a]: ");
-  a_iter_send += a_iter;
-  a_iter_send += "}";
-  std::string a_iter_recv("{a_recv[j,tsk_c,rnk_c,tsk_a,rnk_a]: ");
-  a_iter_recv += a_iter;
-  a_iter_recv += "}";
-  */
-
-  /*
-  std::string a_iter_constants("[NUM_RANKS,J,K,MAP_C_SIZE]");
-  std::string a_iter_constraints("[j,map_c,tsk_c_derived,rnk_c_derived,tsk_a_derived,rnk_a_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and tsk_c_derived=[map_c/NUM_RANKS] and rnk_c_derived=map_c%NUM_RANKS and tsk_a_derived=[([map_c/K]*J+j)/NUM_RANKS] and rnk_a_derived=([map_c/K]*J+j)%NUM_RANKS");
-  std::string a_iter_send(a_iter_constants + "->{a_send" + a_iter_constraints + "}");
-  std::string a_iter_recv(a_iter_constants + "->{a_recv" + a_iter_constraints + "}");
-  */
-
-
-
-
-
-  /*
-  std::string map_a_str("[map_c/" + std::to_string(k_dim_size) + "]*" + std::to_string(j_dim_size) + "+j");
-  std::string rnk_c_derived_str("map_c%" + std::to_string(num_ranks));
-  std::string rnk_a_derived_str("(" + map_a_str + ")%" + std::to_string(num_ranks));
-  // [J,K,NUM_RANKS,MAP_C_SIZE]->{a_send[j,map_c,map_a,rnk_c_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and map_a=[map_c/K]*J+j and rnk_c_derived=map_c%NUM_RANKS}
-  std::string a_iter_send("[J,K,NUM_RANKS,MAP_C_SIZE]->{a_send[j,map_c,map_a,rnk_c_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and map_a=" + map_a_str + " and rnk_c_derived=" + rnk_c_derived_str + "}");
-  // [J,K,NUM_RANKS,MAP_C_SIZE]->{a_recv[j,map_c,rnk_a_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and rnk_a_derived=([map_c/K]*J+j)%NUM_RANKS}
-  std::string a_iter_recv("[J,K,NUM_RANKS,MAP_C_SIZE]->{a_recv[j,map_c,rnk_a_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and rnk_a_derived=" + rnk_a_derived_str + "}");
-  // std::cout << "A_ITER_SEND\n";
-  // std::cout << a_iter_send;
-  // std::cout << "\n\n";
-  // std::cout << "A_ITER_RECV\n";
-  // std::cout << a_iter_recv;
-  // std::cout << "\n\n";
-
-  // Create (non-blocking, non-synchronous) MPI send/recv for A blocks.
-  xfer comm_a = computation::create_xfer(
-      a_iter_send,
-      a_iter_recv,
-      rnk_c_derived,
-      rnk_a_derived,
-      xfer_prop(p_int64, {MPI, BLOCK, ASYNC}),
-      xfer_prop(p_int64, {MPI, BLOCK, ASYNC}),
-      a_in(map_a),
-      fn0);
-  */
-
-
-
-
-
-  /*
-  // Manually form iteration domain point-wise for B block MPI send/recv.
-  std::string b_iter;
-  for (uint64_t j_str = 0; j_str < j_dim_size; j_str++) {
-    for (uint64_t map_c_str = 0; map_c_str < i_dim_size * k_dim_size; map_c_str++) {
-      uint64_t i_str = map_c_str / k_dim_size;
-      uint64_t k_str = map_c_str % k_dim_size;
-      uint64_t map_b_str = j_str * k_dim_size + k_str;
-      
-      uint64_t tsk_c_str = map_c_str / num_ranks;
-      uint64_t rnk_c_str = map_c_str % num_ranks;
-
-      uint64_t tsk_b_str = map_b_str / num_ranks;
-      uint64_t rnk_b_str = map_b_str % num_ranks;
-
-      if (!b_iter.empty()) {
-        b_iter += " or ";
-      }
-      b_iter += "j=" + std::to_string(j_str) + " and ";
-      b_iter += "tsk_c=" + std::to_string(tsk_c_str) + " and ";
-      b_iter += "rnk_c=" + std::to_string(rnk_c_str) + " and ";
-      b_iter += "tsk_b=" + std::to_string(tsk_b_str) + " and ";
-      b_iter += "rnk_b=" + std::to_string(rnk_b_str);
-    }
-  }
-
-  std::string b_iter_send("{b_send[j,tsk_c,rnk_c,tsk_b,rnk_b]: ");
-  b_iter_send += b_iter;
-  b_iter_send += "}";
-  std::string b_iter_recv("{b_recv[j,tsk_c,rnk_c,tsk_b,rnk_b]: ");
-  b_iter_recv += b_iter;
-  b_iter_recv += "}";
-  */
-
-  /*
-  std::string b_iter_constants("[NUM_RANKS,J,K,MAP_C_SIZE]");
-  std::string b_iter_constraints("[j,map_c,tsk_c,rnk_c,tsk_b,rnk_b]: 0<=j<J and 0<=map_c<MAP_C_SIZE and tsk_c=[map_c/NUM_RANKS] and rnk_c=map_c%NUM_RANKS and tsk_b=[(j*K+[map_c%K])/NUM_RANKS] and rnk_b=(j*K+[map_c%K])%NUM_RANKS");
-  std::string b_iter_send(b_iter_constants + "->{b_send" + b_iter_constraints + "}");
-  std::string b_iter_recv(b_iter_constants + "->{b_recv" + b_iter_constraints + "}");
-  */
-
-
-
-
-
-  /*
-  std::string map_b_str("j*" + std::to_string(k_dim_size) + "+(map_c%" + std::to_string(k_dim_size) + ")");
-  std::string rnk_b_derived_str("(" + map_b_str + ")%" + std::to_string(num_ranks));
-  // [J,K,NUM_RANKS,MAP_C_SIZE]->{b_send[j,map_c,map_b,rnk_c_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and map_b=j*K+(map_c%K) and rnk_c_derived=map_c%NUM_RANKS}
-  std::string b_iter_send("[J,K,NUM_RANKS,MAP_C_SIZE]->{b_send[j,map_c,map_b,rnk_c_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and map_b=" + map_b_str + " and rnk_c_derived=" + rnk_c_derived_str + "}");
-  // [J,K,NUM_RANKS,MAP_C_SIZE]->{b_recv[j,map_c,rnk_b_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and rnk_b_derived=(j*K+(map_c%K))%NUM_RANKS}
-  std::string b_iter_recv("[J,K,NUM_RANKS,MAP_C_SIZE]->{b_recv[j,map_c,rnk_b_derived]: 0<=j<J and 0<=map_c<MAP_C_SIZE and rnk_b_derived=" + rnk_b_derived_str + "}");
-
-  // Create (non-blocking, non-synchronous) MPI send/recv for B blocks.
-  xfer comm_b = computation::create_xfer(
-      b_iter_send,
-      b_iter_recv,
-      rnk_c_derived,
-      rnk_b_derived,
-      xfer_prop(p_int64, {MPI, BLOCK, ASYNC}),
-      xfer_prop(p_int64, {MPI, BLOCK, ASYNC}),
-      b_in(map_b),
-      fn0);
-  */
-
-
-
-
-
-/*
-// Binary GCD, adapted from Daniel Lemire's blog. Assumes non-zero inputs.
-static inline uint32_t gcd(uint32_t x, uint32_t y)
-{
-  uint32_t shift = __builtin_ctz(x | y);
-  x >>= __builtin_ctz(x);
-  do {
-    y >>= __builtin_ctz(y);
-    if (x > y) {
-      uint32_t swap = x;
-      x = y;
-      y = swap;
-    }
-    y -= x;
-  } while (y != 0);
-  return x << shift;
-}
-
-static inline uint32_t lcm(uint32_t x, uint32_t y) {
-  if (x == 0 || y == 0) return 0;
-  return (x * y) / gcd(x, y);
-}
-*/
