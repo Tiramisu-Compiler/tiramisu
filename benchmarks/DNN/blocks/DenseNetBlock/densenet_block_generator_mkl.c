31ed2d58c8059d8c272372f6aa9f21c55c39beed
Kamel Abdous <fk_abdous@esi.dz>
2019-06-18 00:47:50 +0100

    Switch DenseNet block MKL to floats

--- benchmarks/DNN/blocks/DenseNetBlock/densenet_block_generator_mkl.c
+++ benchmarks/DNN/blocks/DenseNetBlock/densenet_block_generator_mkl.c
@@ -18,6 +18,28 @@
 
 #define TENSOR_DIMENSION 4
 
+static dnnError_t init_conversion(dnnPrimitive_t *cv, float **ptr_out,
+                                  dnnLayout_t lt_pr, dnnLayout_t lt_us, float *ptr_us)
+{
+    dnnError_t err;
+    *ptr_out = NULL;
+    if (!dnnLayoutCompare_F32(lt_pr, lt_us))
+    {
+        CHECK_ERR(dnnConversionCreate_F32(cv, lt_us, lt_pr), err);
+        CHECK_ERR(dnnAllocateBuffer_F32((void **)ptr_out, lt_pr), err);
+    }
+    else
+    {
+        *ptr_out = ptr_us;
+    }
+    return E_SUCCESS;
+
+bail_out:
+    if (*ptr_out)
+        dnnReleaseBuffer_F32(*ptr_out);
+    return err;
+}
+
 int main()
 {
     srand(1);
@@ -26,85 +48,108 @@ int main()
     // Define some parameters
     size_t input_size[] = {N, N, 4*GR, BATCH_SIZE};
     size_t input_strides[] = {1, N, N*N, N*N*4*GR};
+
     size_t output_size[] = {N, N, GR, BATCH_SIZE};
+    size_t output_strides[] = {1, N, N*N, N*N*GR};
 
-    double bn_scale_shift[2*4*GR];
-    double bn_workspace[2*4*GR];
+    float bn_scale_shift[2*4*GR];
+    float bn_workspace[2*4*GR];
 
-    double conv_filter_param[GR][4*GR][K_Y][K_X];
-    double conv_bias_param[GR];
+    float conv_filter_param[GR][4*GR][K_Y][K_X];
+    float conv_bias_param[GR];
 
     size_t conv_filter_size[] = {K_X, K_Y, 4*GR, GR};
+    size_t conv_filter_strides[] = {1, K_X, K_X*K_Y, K_X*K_Y*4*GR};
+
     size_t conv_strides[] = {1, 1};
     int conv_offset[] = {-1, -1};
 
     for (int z = 0; z < 4*GR; ++z) {
-        bn_scale_shift[z] = ((double)(rand()%256)) / 255.f;
+        bn_scale_shift[z] = ((float)(rand()%256)) / 255.f;
         if (bn_scale_shift[z] == 0.f)
             bn_scale_shift[z] = 1.f;
 
-        bn_scale_shift[z + 4*GR] = ((double)(rand()%256 - 128)) / 127.f;
+        bn_scale_shift[z + 4*GR] = ((float)(rand()%256 - 128)) / 127.f;
     }
 
     for (int fout = 0; fout < GR; ++fout)
         for (int z = 0; z < 4*GR; ++z)
             for (int k_y = 0; k_y < K_Y; ++k_y)
                 for (int k_x = 0; k_x < K_X; ++k_x)
-                    conv_filter_param[fout][z][k_y][k_x] = ((double)(rand()%256 - 128)) / 127.f;
+                    conv_filter_param[fout][z][k_y][k_x] = ((float)(rand()%256 - 128)) / 127.f;
 
     for (int fout = 0; fout < GR; ++fout)
-        conv_bias_param[fout] = ((double)(rand()%256 - 128)) / 127.f;
-
-    // Create the DenseNet block
-    dnnLayout_t input_layout;
-    CHECK_ERR(dnnLayoutCreate_F64(&input_layout, TENSOR_DIMENSION, input_size, input_strides), err);
-
-    dnnPrimitiveAttributes_t attributes;
-    CHECK_ERR(dnnPrimitiveAttributesCreate_F64(&attributes), err);
-
-    dnnPrimitive_t bn_primitive;
-    CHECK_ERR(dnnBatchNormalizationCreateForward_F64(&bn_primitive, attributes, input_layout, EPSILON), err);
-
-    dnnPrimitive_t relu_primitive;
-    CHECK_ERR(dnnReLUCreateForward_F64(&relu_primitive, attributes, input_layout, 0.f), err);
-
-    dnnPrimitive_t conv_primitive;
-    CHECK_ERR(dnnConvolutionCreateForwardBias_F64(&conv_primitive, attributes, dnnAlgorithmConvolutionDirect, TENSOR_DIMENSION, input_size, output_size, conv_filter_size, conv_strides, conv_offset, dnnBorderZeros), err);
+        conv_bias_param[fout] = ((float)(rand()%256 - 128)) / 127.f;
 
     // Allocate buffers
-    double* input_buf = malloc(sizeof(double) * N * N * 4*GR * BATCH_SIZE);
-    double* bn_buf = malloc(sizeof(double) * N * N * 4*GR * BATCH_SIZE);
-    double* output_buf = malloc(sizeof(double) * N * N * GR * BATCH_SIZE);
+    float* input_buf = malloc(sizeof(float) * N * N * 4*GR * BATCH_SIZE);
+    float* output_buf = malloc(sizeof(float) * N * N * GR * BATCH_SIZE);
 
     for (int n = 0; n < BATCH_SIZE; ++n)
         for (int z = 0; z < 4*GR; ++z)
             for (int y = 0; y < N; ++y)
                 for (int x = 0; x < N; ++x)
-                    input_buf[x + y*N + z*N*N + n*N*N*4*GR] = ((double)(rand() % 256)) / 255.f;
+                    input_buf[x + y*N + z*N*N + n*N*N*4*GR] = ((float)(rand() % 256)) / 255.f;
+
+    // Create the DenseNet block
+    float* res_bn_relu[dnnResourceNumber] = {0};
+    float* res_conv[dnnResourceNumber] = {0};
+
+    dnnPrimitiveAttributes_t attributes;
+    CHECK_ERR(dnnPrimitiveAttributesCreate_F64(&attributes), err);
+
+    // Create convolution (begin by convolution to get optimized data layout)
+    dnnLayout_t lt_user_input, lt_user_filt, lt_user_output;
+    dnnLayout_t lt_conv_input, lt_conv_filt, lt_conv_output;
+
+    CHECK_ERR(dnnLayoutCreate_F32(&lt_user_input, TENSOR_DIMENSION, input_size, input_strides), err);
+    CHECK_ERR(dnnLayoutCreate_F32(&lt_user_filt, TENSOR_DIMENSION, conv_filter_size, conv_filter_strides), err);
+    CHECK_ERR(dnnLayoutCreate_F32(&lt_user_output, TENSOR_DIMENSION, output_size, output_strides), err);
+
+    dnnPrimitive_t cv_usr_to_conv_input, cv_usr_to_conv_filt;
+    dnnPrimitive_t conv_primitive;
+    CHECK_ERR(dnnConvolutionCreateForwardBias_F32(&conv_primitive, attributes, dnnAlgorithmConvolutionDirect, TENSOR_DIMENSION, input_size, output_size, conv_filter_size, conv_strides, conv_offset, dnnBorderZeros), err);
+
+    CHECK_ERR(dnnLayoutCreateFromPrimitive_F32(&lt_conv_input, conv_primitive, dnnResourceSrc), err);
+    CHECK_ERR(dnnLayoutCreateFromPrimitive_F32(&lt_conv_filt, conv_primitive, dnnResourceFilter), err);
+    CHECK_ERR(dnnLayoutCreateFromPrimitive_F32(&lt_conv_output, conv_primitive, dnnResourceDst), err);
+
+    CHECK_ERR(init_conversion(&cv_usr_to_conv_input, &res_bn_relu[dnnResourceSrc], lt_conv_input, lt_user_input, input_buf), err);
+    CHECK_ERR(init_conversion(&cv_usr_to_conv_filt, &res_conv[dnnResourceFilter], lt_conv_filt, lt_user_filt, (void*)conv_filter_param), err);
+    CHECK_ERR(dnnAllocateBuffer_F32((void **)&res_conv[dnnResourceDst], lt_conv_output), err);
+
+    CHECK_ERR(dnnConversionExecute_F32(cv_usr_to_conv_filt, (void*)conv_filter_param, res_conv[dnnResourceFilter]), err);
+
+    res_conv[dnnResourceSrc] = res_bn_relu[dnnResourceSrc];
+    res_conv[dnnResourceBias] = conv_bias_param;
+
+    // Create BN and ReLU
+    dnnPrimitive_t bn_primitive;
+    CHECK_ERR(dnnBatchNormalizationCreateForward_F32(&bn_primitive, attributes, lt_conv_input, EPSILON), err);
+
+    dnnPrimitive_t relu_primitive;
+    CHECK_ERR(dnnReLUCreateForward_F32(&relu_primitive, attributes, lt_conv_input, 0.f), err);
+
+    res_bn_relu[dnnResourceDst] = res_bn_relu[dnnResourceSrc];
+    res_bn_relu[dnnResourceScaleShift] = bn_scale_shift;
+    res_bn_relu[dnnResourceWorkspace] = bn_workspace;
+
+    // Create output conversions
+    dnnPrimitive_t cv_conv_to_usr_output;
+    CHECK_ERR(init_conversion(&cv_conv_to_usr_output, &output_buf, lt_user_output, lt_conv_output, res_conv[dnnResourceDst]), err);
 
     // Execute the block
-    double* resources[dnnResourceNumber] = {0};
     double times[NB_TESTS];
     clock_t start, end;
 
     for (int i = 0; i < NB_TESTS; ++i) {
+        CHECK_ERR(dnnConversionExecute_F32(cv_usr_to_conv_input, input_buf, res_bn_relu[dnnResourceSrc]), err);
+        
         start = clock();
 
-        resources[dnnResourceSrc] = input_buf;
-        resources[dnnResourceScaleShift] = bn_scale_shift;
-        resources[dnnResourceWorkspace] = bn_workspace;
-        resources[dnnResourceDst] = bn_buf;
-        CHECK_ERR(dnnExecute_F64(bn_primitive, (void**)resources), err);
-
-        resources[dnnResourceSrc] = bn_buf;
-        resources[dnnResourceDst] = bn_buf;
-        CHECK_ERR(dnnExecute_F64(relu_primitive, (void**)resources), err);
-
-        resources[dnnResourceSrc] = bn_buf;
-        resources[dnnResourceFilter] = (void*)conv_filter_param;
-        resources[dnnResourceBias] = conv_bias_param;
-        resources[dnnResourceDst] = output_buf;
-        CHECK_ERR(dnnExecute_F64(conv_primitive, (void**)resources), err);
+        CHECK_ERR(dnnExecute_F32(bn_primitive, (void**)res_bn_relu), err);
+        CHECK_ERR(dnnExecute_F32(relu_primitive, (void**)res_bn_relu), err);
+        CHECK_ERR(dnnExecute_F32(conv_primitive, (void**)res_conv), err);
 
         end = clock();
         double time_taken = ((double)(end - start) / CLOCKS_PER_SEC) * 1000;
@@ -112,6 +157,8 @@ int main()
     }
 
     printf("\n\n\tDenseNet block time: %f ms.\n", median(NB_TESTS, times));
+
+    CHECK_ERR(dnnConversionExecute_F32(cv_conv_to_usr_output, res_conv[dnnResourceDst], output_buf), err);
 
     // Write results to file
     FILE* f = fopen("mkl_result.txt", "w");
@@ -128,14 +175,27 @@ int main()
 
     fclose(f);
     
-    free(output_buf);
-    free(bn_buf);
     free(input_buf);
     dnnDelete_F64(conv_primitive);
     dnnDelete_F64(relu_primitive);
     dnnDelete_F64(bn_primitive);
+
+    dnnDelete_F32(cv_usr_to_conv_filt);
+    dnnDelete_F32(cv_usr_to_conv_input);
+    dnnDelete_F32(cv_conv_to_usr_output);
+
     dnnPrimitiveAttributesDestroy_F64(attributes);
-    dnnLayoutDelete_F64(input_layout);
+    dnnLayoutDelete_F32(lt_user_input);
+    dnnLayoutDelete_F32(lt_user_filt);
+    dnnLayoutDelete_F32(lt_user_output);
+    dnnLayoutDelete_F32(lt_conv_input);
+    dnnLayoutDelete_F32(lt_conv_filt);
+    dnnLayoutDelete_F32(lt_conv_output);
+
+    dnnReleaseBuffer_F32(res_bn_relu[dnnResourceSrc]);
+    dnnReleaseBuffer_F32(res_conv[dnnResourceFilter]);
+    dnnReleaseBuffer_F32(res_conv[dnnResourceDst]);
+
     
     return 0;
 }
