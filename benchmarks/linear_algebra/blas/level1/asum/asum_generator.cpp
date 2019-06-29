383446ad841e962de7a06314d7dcea841c4db457
Mekki Mohamed <fm_mekki@esi.com>
2019-06-29 18:34:47 -0400

    add asum blas function without optimization

---
+++ benchmarks/linear_algebra/blas/level1/asum/asum_generator.cpp
@@ -0,0 +1,81 @@
+
+#include <tiramisu/tiramisu.h>
+#include "benchmarks.h"
+#include "math.h"
+
+
+/*
+    ASUM:
+    ----
+    ASUM takes the sum of the absolute values.
+
+        SUM(ABS(X))
+
+    X is an ( 1 + ( N - 1 )*abs( incx ) ) vector    
+    incx storage spacing between elements of X
+        
+   
+    The C version of this function is as follow : 
+
+        result = 0;
+        for(int i=0; i < N :i++){
+	            result += x[i * incx]; 
+        }
+   
+*/
+
+
+
+
+using namespace tiramisu;
+
+int main(int argc, char **argv)
+{
+    tiramisu::init("asum");
+
+    // -------------------------------------------------------
+    // Layer I
+    // -------------------------------------------------------
+
+    constant NN("NN", expr(N));
+
+    // Iterator
+    var i("i", 0, NN);
+
+    input incx("incx", {}, p_float64);
+    input x("x", {i}, p_float64);
+
+    // Computations
+    computation init("init", {}, cast(p_float64, 0));
+    computation sum("sum", {i}, p_float64);
+    
+    sum.set_expression(sum(i - 1) + expr(o_abs, x(cast(p_float64, i) * cast(p_float64, incx(0)))));
+
+    // -------------------------------------------------------
+    // Layer II
+    // -------------------------------------------------------
+
+    // -------------------------------------------------------
+    // Layer III
+    // -------------------------------------------------------
+
+    // Inputs
+    buffer b_incx("b_incx", {expr(1)}, p_float64, a_input);
+    buffer b_x("b_x", {expr(NN)}, p_float64, a_input);   
+    
+    // Output
+    buffer b_result("b_result", {expr(1)}, p_float64, a_output);
+
+    incx.store_in(&b_incx);
+    x.store_in(&b_x);
+    init.store_in(&b_result, {});
+    sum.store_in(&b_result, {});
+
+    // -------------------------------------------------------
+    // Code Generation
+    // -------------------------------------------------------
+
+    tiramisu::codegen({&b_x, &b_incx, &b_result}, "generated_asum.o");
+
+    return 0;
+}
--- benchmarks/linear_algebra/blas/level1/asum/asum_wrapper.cpp
+++ benchmarks/linear_algebra/blas/level1/asum/asum_wrapper.cpp
@@ -1,22 +1,21 @@
 #include <Halide.h>
 #include <tiramisu/tiramisu.h>
 #include <iostream>
-#include "generated_syr.o.h"
+#include "generated_asum.o.h"
 #include "benchmarks.h"
 #include <tiramisu/utils.h>
 
-int syr_ref(
-    const int NN, 
-    const double * A, 
+int asum_ref(
     const double * x, 
-    const double * alpha,
+    const double * incx,
     double * result 
 )
 {
-    for(int i = 0; i < NN; i++){
-        for(int j = 0; j < NN; j++){
-	        result[i*NN +j] = alpha[0] * x[i] * x[j] + A[i*NN + j];
-        }
+    result[0]=0;
+
+    int inc = (int) incx[0];
+    for(int i = 0; i < N; i++){
+        result[0]+=abs(x[i * inc]);
     }
 	return 0;
 }
@@ -39,11 +38,10 @@ int main(int argc, char** argv)
     // ---------------------------------------------------------------------
     // ---------------------------------------------------------------------
 
-    Halide::Buffer<double> b_alpha(1), b_x(N), b_A(N, N), b_result(N, N), b_result_ref(N, N);
+    Halide::Buffer<double> b_x(2 * N), b_incx(1), b_result(1), b_result_ref(1);
     
-    init_buffer(b_alpha, (double) 2);
-    init_buffer(b_x, (double) 2);
-    init_buffer(b_A, (double) 1);
+    init_buffer(b_incx, (double) 2);
+    init_buffer(b_x, (double) 5);
     init_buffer(b_result, (double) 0);
     init_buffer(b_result_ref, (double) 0);
 
@@ -58,7 +56,7 @@ int main(int argc, char** argv)
             auto start = std::chrono::high_resolution_clock::now();
 
             if (run_ref)
-	    	syr_ref(N, b_A.data(), b_x.data(), b_alpha.data(), b_result_ref.data());
+	    	asum_ref(b_x.data(), b_incx.data(), b_result_ref.data());
 
             auto end = std::chrono::high_resolution_clock::now();
             duration_vector_1.push_back(end - start);
@@ -72,19 +70,19 @@ int main(int argc, char** argv)
             auto start = std::chrono::high_resolution_clock::now();
 
             if (run_tiramisu)
-	    	syr(b_A.raw_buffer(), b_x.raw_buffer(), b_alpha.raw_buffer(), b_result.raw_buffer());
+	    	asum(b_x.raw_buffer(), b_incx.raw_buffer(), b_result.raw_buffer());
 
             auto end = std::chrono::high_resolution_clock::now();
             duration_vector_2.push_back(end - start);
         }
     }
 
-    print_time("performance_cpu.csv", "syr",
+    print_time("performance_cpu.csv", "asum",
 	       {"Ref", "Tiramisu"},
 	       {median(duration_vector_1), median(duration_vector_2)});
 
     if (CHECK_CORRECTNESS && run_ref && run_tiramisu)
-        compare_buffers("syr", b_result, b_result_ref);
+        compare_buffers("asum", b_result, b_result_ref);
 
     if (PRINT_OUTPUT)
     {
