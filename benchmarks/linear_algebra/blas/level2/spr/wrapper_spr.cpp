0d81630967acb032aa96f5c13c9cabe4d09382a1
Abdelkader Mekrache <fa_mekrache@esi.dz>
2019-07-02 00:15:11 +0100

    Update wrapper_spr.cpp

--- benchmarks/linear_algebra/blas/level2/spr/wrapper_spr.cpp
+++ benchmarks/linear_algebra/blas/level2/spr/wrapper_spr.cpp
@@ -15,7 +15,6 @@ int main(int, char **)
 {
     Halide::Buffer<uint8_t> A_buf(NN, NN);
     Halide::Buffer<uint8_t> x_buf(NN);
-    
     //output spr
     Halide::Buffer<uint8_t> output1_buf(NN, NN);
 
@@ -25,7 +24,6 @@ int main(int, char **)
             A_buf(j, i) = (i + 3) * (j + 1);
         }
     }
-
     // Initialize Vector X with pseudorandom values:
     for (int i = 0; i < NN; i++) {
         x_buf(i) = 2;
@@ -33,9 +31,7 @@ int main(int, char **)
 
     // TRAMISU CODE EXECUTION STARTS:
     auto start1 = std::chrono::high_resolution_clock::now();
-
     spr(A_buf.raw_buffer(), x_buf.raw_buffer(), output1_buf.raw_buffer() );
-
     auto end1 = std::chrono::high_resolution_clock::now();
     auto  duration1 =duration_cast<microseconds>(end1 - start1);
     // TRAMISU CODE EXECUTION ENDS.
@@ -43,7 +39,6 @@ int main(int, char **)
     // REFERENCE Output buffer
     Halide::Buffer<uint8_t> output2_buf(NN, NN);
     init_buffer(output2_buf, (uint8_t)0);
-
     // REFERENCE C++ CODE EXECUTION STARTS
     auto start2 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < NN; i++) {
@@ -55,13 +50,12 @@ int main(int, char **)
 
     auto end2 = std::chrono::high_resolution_clock::now();
     auto  duration2 =duration_cast<microseconds>(end2 - start2);
-
     //===== printing REFERECE EXEC TIME: =====
     std::cout << "\n REF RESOLUTION TIME : " << duration2.count() << "microseconds";
     //===== printing TIRAMISU EXEC TIME: =====
     std::cout << "\n TIRAMISU RESOLUTION TIME : " << duration1.count() << "microseconds";
     printf("\n");
-
+	
     //===== Verify if TIRAMISU output is correct: =====
     compare_buffers("spr", output1_buf, output2_buf);
 
