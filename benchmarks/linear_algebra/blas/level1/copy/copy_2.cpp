5200387c569073cd4603c2f2e8d6d470226cdd2f
Abdelkader Mekrache <fa_mekrache@esi.dz>
2019-07-01 23:25:18 +0100

    Update and rename copy.cpp to copy_2.cpp

--- benchmarks/linear_algebra/blas/level1/copy/copy_2.cpp
+++ benchmarks/linear_algebra/blas/level1/copy/copy_2.cpp
@@ -19,28 +19,21 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
     // Layer I
     // -------------------------------------------------------
-
     // Declare the constant N.
     constant N("N", NN);
-
     // Declare iterator variable.
     var i("i", 0, N);
-
     // Declare the input.
     input A("A", {i}, p_uint8);
-
     // Declare the output computation.
     computation output("output", {i}, A(i));
-
 
     // -------------------------------------------------------
     // Layer III
     // -------------------------------------------------------
-
     // Declare input and output buffers.
     buffer b_A("b_A", {expr(NN)}, p_uint8, a_input);
     buffer b_output("b_output", {expr(NN)}, p_uint8, a_output);
-
     // Map the computations to a buffer.
     A.store_in(&b_A);
     output.store_in(&b_output);
@@ -48,7 +41,6 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
     // Code Generation
     // -------------------------------------------------------
-
     // Set the arguments and generate code
     tiramisu::codegen({&b_A, &b_output}, "build/generated_fct_developers_copy.o");
 
