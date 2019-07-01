8d6daf1ad431d114c3b9ad2c72514d96b031d548
Abdelkader Mekrache <fa_mekrache@esi.dz>
2019-07-02 00:15:46 +0100

    Update wrapper_syr2_2.cpp

--- benchmarks/linear_algebra/blas/level2/syr2/wrapper_syr2_2.cpp
+++ benchmarks/linear_algebra/blas/level2/syr2/wrapper_syr2_2.cpp
@@ -16,7 +16,6 @@ int main(int, char **)
     Halide::Buffer<uint8_t> A_buf(NN, NN);
     Halide::Buffer<uint8_t> x_buf(NN);
     Halide::Buffer<uint8_t> y_buf(NN);
-
     //output syr2
     Halide::Buffer<uint8_t> output1_buf(NN, NN);
 
@@ -26,12 +25,10 @@ int main(int, char **)
             A_buf(j, i) = (i + 3) * (j + 1);
         }
     }
-
     // Initialize Vector X with pseudorandom values:
     for (int i = 0; i < NN; i++) {
         x_buf(i) = 2;
     }
-
     // Initialize Vector Y with pseudorandom values:
     for (int j = 0; j < NN; j++) {
     	y_buf(j) = j;
@@ -47,7 +44,6 @@ int main(int, char **)
     // REFERENCE Output buffer
     Halide::Buffer<uint8_t> output2_buf(NN, NN);
     init_buffer(output2_buf, (uint8_t)0);
-
     // REFERENCE C++ CODE EXECUTION STARTS
     auto start2 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < NN; i++) {
@@ -59,14 +55,13 @@ int main(int, char **)
 
     auto end2 = std::chrono::high_resolution_clock::now();
     auto  duration2 =duration_cast<microseconds>(end2 - start2);
-
     //===== printing REFERECE EXEC TIME: =====
     std::cout << "\n REF RESOLUTION TIME : " << duration2.count() << "microseconds";
-   //===== printing TIRAMISU EXEC TIME: =====
+    //===== printing TIRAMISU EXEC TIME: =====
     std::cout << "\n TIRAMISU RESOLUTION TIME : " << duration1.count() << "microseconds";
     printf("\n");
 
-   //===== Verify if TIRAMISU output is correct: =====
+    //===== Verify if TIRAMISU output is correct: =====
     compare_buffers("syr2", output1_buf, output2_buf);
 
     return 0;
