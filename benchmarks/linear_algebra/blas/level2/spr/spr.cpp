8f7958ded133d1237b1c4800b3d862951b75064c
Abdelkader Mekrache <fa_mekrache@esi.dz>
2019-07-01 23:48:43 +0100

    Update spr.cpp

--- benchmarks/linear_algebra/blas/level2/spr/spr.cpp
+++ benchmarks/linear_algebra/blas/level2/spr/spr.cpp
@@ -8,10 +8,8 @@
 */
 
 #include <tiramisu/tiramisu.h>
-
 #define NN 100
 #define alpha 3 
-
 using namespace tiramisu;
 
 int main(int argc, char **argv)
@@ -23,11 +21,9 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
     constant N("N", NN);
     var i("i", 0, N), j("j", 0, N);
-    
     // Declare inputs : A(Matrix N*N) , X(Vector dim=N)
     input A("A", {i, j}, p_uint8);
     input x("B", {i}, p_uint8);
-    
     // Declare output which is result of computation
     computation output("output", {i,j}, expr((uint8_t)alpha) * x(j) * x(i)  + A(i, j) );
    
@@ -38,7 +34,6 @@ int main(int argc, char **argv)
     buffer b_A("b_A", {N,N}, p_uint8, a_input);
     buffer b_x("b_x", {N}, p_uint8, a_input);
     buffer b_output("b_output", {N,N}, p_uint8, a_output);
-    
     // Map the computations to a buffer.
     A.store_in(&b_A);
     x.store_in(&b_x);
