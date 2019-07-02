9a44cfe178443d43bea6c104b36b52d7cae48c05
KhadidjaRabea <32750358+KhadidjaRabea@users.noreply.github.com>
2019-07-02 13:44:16 +0200

    Fix code style

--- benchmarks/linear_algebra/blas/level1/nrm2/nrm2_wrapper.cpp
+++ benchmarks/linear_algebra/blas/level1/nrm2/nrm2_wrapper.cpp
@@ -1,28 +1,29 @@
+#include "generated_nrm2.o.h"
+
 #include <Halide.h>
 #include <tiramisu/tiramisu.h>
-#include <iostream>
-#include "generated_nrm2.o.h"
-#include "benchmarks.h"
 #include <tiramisu/utils.h>
+
+#include <iostream>
+#include "benchmarks.h"
 #include <math.h>
 
 #define M_DIM M
 #define N_DIM N
 
-
-
-int nrm2_ref(int n, double * X,double  *nrm)
-{   double sum = 0;
+int nrm2_ref(int n, double * X, double *nrm)
+{   
+    double sum = 0;
+	
     for (int i = 0; i < n; ++i)
-    	  sum += X[i]*X[i]; 
+    	 sum += X[i] * X[i]; 
+	
     nrm[0] = sqrt(sum);
     return 0;
 }
 
-
 int main(int argc, char** argv)
 {
-
     std::vector<std::chrono::duration<double, std::milli>> duration_vector_1, duration_vector_2;
 
     bool run_ref = false, run_tiramisu = false;
@@ -39,29 +40,23 @@ int main(int argc, char** argv)
     // ---------------------------------------------------------------------
     // ---------------------------------------------------------------------
   
-    
     Halide::Buffer<double> b_result(1), b_result_ref(1);
-  
+	
     Halide::Buffer<double> b_X(N_DIM);
-
-	/**
-		X vector is initialized to 1
-
-	*/
-
+    init_buffer(b_X, (double) 1);
+	
+    /**
+       X vector is initialized to 1
+    **/
+	
     {
         for (int i = 0; i < NB_TESTS; ++i)
-        {
-
-            init_buffer(b_X, (double) 1);
+        {   
             auto start = std::chrono::high_resolution_clock::now();
-      
-	
-
+		
             if (run_ref)
-	    	nrm2_ref(N_DIM,b_X.data(), b_result_ref.data() );
-      
-
+	    	nrm2_ref(N_DIM, b_X.data(), b_result_ref.data() );
+		
             auto end = std::chrono::high_resolution_clock::now();
             duration_vector_1.push_back(end - start);
         }
@@ -69,17 +64,12 @@ int main(int argc, char** argv)
 
     {
         for (int i = 0; i < NB_TESTS; ++i)
-        {
-            
-	    init_buffer(b_X, (double) 1);
-	    
-			
+        {	
             auto start = std::chrono::high_resolution_clock::now();
 
             if (run_tiramisu)
-	    	nrm2(b_X.raw_buffer(),b_result.raw_buffer());
-
-	
+	    	nrm2(b_X.raw_buffer(), b_result.raw_buffer());
+		
             auto end = std::chrono::high_resolution_clock::now();
             duration_vector_2.push_back(end - start);
         }
@@ -88,7 +78,6 @@ int main(int argc, char** argv)
     print_time("performance_cpu.csv", "nrm2",
 	       {"Ref", "Tiramisu"},
 	       {median(duration_vector_1), median(duration_vector_2)});
-
 
     if (CHECK_CORRECTNESS && run_ref && run_tiramisu)
         compare_buffers("nrm2", b_result, b_result_ref);
