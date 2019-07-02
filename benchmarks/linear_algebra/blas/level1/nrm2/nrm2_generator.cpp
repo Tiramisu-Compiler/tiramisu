8f422ffa4ebaed3d220a937096b833c96fb02bdb
KhadidjaRabea <32750358+KhadidjaRabea@users.noreply.github.com>
2019-07-02 13:38:04 +0200

    Fix code style

--- benchmarks/linear_algebra/blas/level1/nrm2/nrm2_generator.cpp
+++ benchmarks/linear_algebra/blas/level1/nrm2/nrm2_generator.cpp
@@ -1,21 +1,19 @@
-
-
 #include <tiramisu/tiramisu.h>
+
 #include "benchmarks.h"
+
 #include "math.h"
-/*
-  IMPLEMENTATION OF THE NMR2 FUNCTION IN TIRAMISU 
-  
-   out = sqrt(x*'x) 
-
-   where : 
-   x : is a size N vector and 'x is the transpose of x 
-   
-*/
-
-
 
 using namespace tiramisu;
+
+/***
+  Benchmark for the BLAS NRM2 
+ 
+  out = sqrt(x*'x) 
+  where : 
+  x : is a size N vector
+  x': is the transpose of x 
+***/
 
 int main(int argc, char **argv)
 {
@@ -24,45 +22,52 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
     // Layer I
     // -------------------------------------------------------
-
+  
     constant NN("N", expr(N));
-
-    var i("i", 0, NN);
-    input x("x", {"i"}, {NN},p_float64);
-    computation S_init("S_init", {}, expr(cast(p_float64,0)));
-    computation S("S", {i},p_float64);
-    S.set_expression( S(i-1) + x(i)*x(i));
-    computation result("result", {},p_float64);
-    result.set_expression(cast(p_float64,expr(o_sqrt,S(NN),p_float64))); 
-     
     
-
-
-
+    //Iteration variable
+    var i("i", 0, NN);
+  
+    //Input
+    input x("x", {"i"}, {NN}, p_float64);
+  
+    //Computations
+    computation S_init("S_init", {}, expr(cast(p_float64, 0)));
+    computation S("S", {i}, p_float64);
+    computation result("result", {}, p_float64);
+    S.set_expression(S(i-1) + x(i ) * x(i));
+    result.set_expression(cast(p_float64, expr(o_sqrt, S(NN), p_float64))); 
+  
     // -------------------------------------------------------
     // Layer II
     // -------------------------------------------------------
-
-     S.after(S_init,{}); //
-     result.after(S,computation::root); //
-
+  
+     S.after(S_init, {}); 
+     result.after(S, computation::root); 
+  
     // -------------------------------------------------------
     // Layer III
     // -------------------------------------------------------
-
-    buffer b_x("b_x", {expr(NN)}, p_float64, a_input);   
+  
+    //Input Buffer
+    buffer b_x("b_x", {expr(NN)}, p_float64, a_input); 
+  
+    //Output Buffer
     buffer b_result("b_result", {expr(1)}, p_float64, a_output);
-
+    
+    //Store input
     x.store_in(&b_x);
-    S_init.store_in(&b_result,{});
-    S.store_in(&b_result,{});
-    result.store_in(&b_result,{});
+  
+    //Store computations
+    S_init.store_in(&b_result, {});
+    S.store_in(&b_result, {});
+    result.store_in(&b_result, {});
 
     // -------------------------------------------------------
     // Code Generation
     // -------------------------------------------------------
 
-    tiramisu::codegen({&b_x,&b_result}, "generated_nrm2.o");
+    tiramisu::codegen({&b_x, &b_result}, "generated_nrm2.o");
 
     return 0;
 }
