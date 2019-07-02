aae422ad15ce5b4a9b7b013fc653d26e31b6d9db
Nadji09 <32777994+Nadji09@users.noreply.github.com>
2019-07-02 10:52:17 +0100

    coding style fixed in sger_wrapper

--- benchmarks/linear_algebra/blas/level2/sger/sger_wrapper.cpp
+++ benchmarks/linear_algebra/blas/level2/sger/sger_wrapper.cpp
@@ -1,9 +1,11 @@
+#include "generated_sger.o.h"
+
 #include <Halide.h>
 #include <tiramisu/tiramisu.h>
-#include <iostream>
-#include "generated_sger.o.h"
-#include "benchmarks.h"
 #include <tiramisu/utils.h>
+
+#include <iostream>
+#include "benchmarks.h"
 
 #define M_DIM M
 #define N_DIM N
@@ -11,8 +13,8 @@
 int sger_ref(int n, int m, double alpha, double * A, double* x, double * y)
 {
     for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; j++)
-    	A[i * m + j] +=  alpha * x[i] * y[j];  
+        for (int j = 0; j < m; ++j)
+            A[i * m + j] +=  alpha * x[i] * y[j];
 
     return 0;
 }
@@ -38,33 +40,40 @@ int main(int argc, char** argv)
   
     double alpha = 2.0;
   
-    
     Halide::Buffer<double> b_alpha(1);
     b_alpha(0) = alpha;
   
-    Halide::Buffer<double> b_X(N_DIM), b_Y(M_DIM), b_A(N_DIM, M_DIM), b_A_ref(N_DIM, M_DIM);
+    // b_A and b_A_ref needs to be initialized in each iteration test
+    Halide::Buffer<double> b_A(N_DIM, M_DIM), b_A_ref(N_DIM, M_DIM);
+    
+    Halide::Buffer<double> b_X(N_DIM);
+    init_buffer(b_X, (double) 2);
 
-
-
-    // ---------------------------------------------------------------------
-    // ---------------------------------------------------------------------
-    // ---------------------------------------------------------------------
+    Halide::Buffer<double> b_Y(M_DIM);
+    init_buffer(b_Y, (double) 3);
+    
+    /**
+    * We have :
+    * 
+    * alpha : equals 2
+    * b_X : size N_DIM vector with all values set to 2
+    * b_Y : size M_DIM vector with all values set to 3
+    * b_A : N_DIM by M_DIM matrix with all values set to 1 (initialized in each loop)
+    * b_A_ref : N_DIM by M_DIM matrix with all values set to 1 (initialized in each loop)
+    *
+    * The result must be a N_DIM by M_DIM with all values equal to 13 
+    */
 
     {
         for (int i = 0; i < NB_TESTS; ++i)
         {
-
+            // b_A_ref initialized with 1
             init_buffer(b_A_ref, (double) 1);
-	    init_buffer(b_X, (double) 2);
-	init_buffer(b_Y, (double) 3);
             auto start = std::chrono::high_resolution_clock::now();
       
-	
-
             if (run_ref)
 	    	sger_ref(N_DIM, M_DIM, alpha, b_A_ref.data(), b_X.data(), b_Y.data() );
       
-
             auto end = std::chrono::high_resolution_clock::now();
             duration_vector_1.push_back(end - start);
         }
@@ -73,16 +82,14 @@ int main(int argc, char** argv)
     {
         for (int i = 0; i < NB_TESTS; ++i)
         {
-            init_buffer(b_A, (double)1);
-	    init_buffer(b_X, (double) 2);
-	    init_buffer(b_Y, (double)3);
+            // b_A initialized with 1
+            init_buffer(b_A, (double) 1);
 			
             auto start = std::chrono::high_resolution_clock::now();
 
             if (run_tiramisu)
 	    	sger(b_A.raw_buffer(), b_X.raw_buffer(), b_Y.raw_buffer(), b_alpha.raw_buffer());
 
-	
             auto end = std::chrono::high_resolution_clock::now();
             duration_vector_2.push_back(end - start);
         }
