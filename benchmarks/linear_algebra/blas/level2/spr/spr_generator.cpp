791941c9646908837fccdbc7c721cb311f524873
Abdelkader Mekrache <fa_mekrache@esi.dz>
2019-07-04 00:03:50 +0100

    Update spr_generator.cpp

--- benchmarks/linear_algebra/blas/level2/spr/spr_generator.cpp
+++ benchmarks/linear_algebra/blas/level2/spr/spr_generator.cpp
@@ -1,5 +1,5 @@
 /* 
-    This program shows how to process BLAS LEVEL2 spr
+    This program shows how to process BLAS LEVEL2 SPR
 
     for i = 0 .. N
         for j = 0 .. N
@@ -8,9 +8,7 @@
 */
 
 #include <tiramisu/tiramisu.h>
-
-#define NN 100
-#define alpha 3 
+#include "benchmarks.h"
 
 using namespace tiramisu;
 
@@ -21,33 +19,36 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
     // Layer I
     // -------------------------------------------------------
-    constant N("N", NN);
-    var i("i", 0, N), j("j", 0, N);
+    constant NN("NN", N);
+    var i("i", 0, NN), j("j", 0, NN);
 
-    // Declare inputs : A(Matrix N*N) , X(Vector dim=N)
-    input A("A", {i, j}, p_uint8);
-    input x("B", {i}, p_uint8);
+    // Declare inputs : A(Matrix N*N) , X(Vector dim=N), alpha(scalar)
+    input A("A", {i, j}, p_float64);
+    input x("B", {i}, p_float64);
+    input alpha("alpha", {}, p_float64);
 
     // Declare output which is result of computation
-    computation output("output", {i,j}, expr((uint8_t)alpha) * x(j) * x(i)  + A(i, j) );
+    computation output("output", {i,j}, cast(p_float64, alpha(0) * x(j) * x(i)  + A(i, j) ));
    
     // -------------------------------------------------------
     // Layer III
     // -------------------------------------------------------
     // Declare the buffers.
-    buffer b_A("b_A", {N,N}, p_uint8, a_input);
-    buffer b_x("b_x", {N}, p_uint8, a_input);
-    buffer b_output("b_output", {N,N}, p_uint8, a_output);
+    buffer b_A("b_A", {expr(NN),expr(NN)}, p_float64, a_input);
+    buffer b_x("b_x", {expr(NN)}, p_float64, a_input);
+    buffer b_alpha("b_alpha", {expr(1)}, p_float64, a_input);
+    buffer b_output("b_output", {expr(NN),expr(NN)}, p_float64, a_output);
 
     // Map the computations to a buffer.
     A.store_in(&b_A);
     x.store_in(&b_x);
+    alpha.store_in(&b_alpha);
     output.store_in(&b_output,{i,j});
  
     // -------------------------------------------------------
     // Code Generation
     // -------------------------------------------------------
-    tiramisu::codegen({&b_A, &b_x, &b_output}, "build/generated_spr.o");
+    tiramisu::codegen({&b_A, &b_x, &b_alpha, &b_output}, "generated_spr.o");
 
     return 0;
 }
