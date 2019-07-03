eb9ceb5eeced4ebe8d470ebe7f710a9f9484aa0a
Riyadh Baghdadi <rbaghdadi@users.noreply.github.com>
2019-07-02 21:28:17 -0400

    Update swap_wrapper.cpp

--- benchmarks/linear_algebra/blas/level1/swap/swap_wrapper.cpp
+++ benchmarks/linear_algebra/blas/level1/swap/swap_wrapper.cpp
@@ -5,17 +5,17 @@
 #include <iostream>
 #include "benchmarks.h"
 
-int swap_ref(int n,
-             Halide::Buffer<float> a,
-             Halide::Buffer<float> b)
+int swap_ref(int n, Halide::Buffer<float> a, Halide::Buffer<float> b)
 {
     Halide::Buffer<float> b_temp(N);
+
     for (int i = 0; i < n; ++i)
     {
       b_temp(i) = a(i);  
       a(i) = b(i); 
       b(i) = b_temp(i);
     }
+
     return 0;
 }
 
@@ -25,11 +25,15 @@ int main(int argc, char** argv)
     bool run_ref = false, run_tiramisu = false;
 
     const char* env_ref = std::getenv("RUN_REF");
+
     if (env_ref != NULL && env_ref[0] == '1')
         run_ref = true;
+
     const char* env_tiramisu = std::getenv("RUN_TIRAMISU");
+
     if (env_tiramisu != NULL && env_tiramisu[0] == '1')
         run_tiramisu = true;
+
     // ---------------------------------------------------------------------
     // ---------------------------------------------------------------------
     // ---------------------------------------------------------------------
@@ -46,9 +50,11 @@ int main(int argc, char** argv)
             init_buffer(b_b_ref, (float) 2);
 	    init_buffer(b_a_ref, (float) 1);
             auto start = std::chrono::high_resolution_clock::now();
-            if (run_ref)
+
+	    if (run_ref)
 	    	swap_ref(N, b_a_ref, b_b_ref );
-            auto end = std::chrono::high_resolution_clock::now();
+
+	    auto end = std::chrono::high_resolution_clock::now();
             duration_vector_1.push_back(end - start);
         }
     }
@@ -60,12 +66,15 @@ int main(int argc, char** argv)
             init_buffer(b_b, (float) 2);
 	    init_buffer(b_a, (float) 1);
             auto start = std::chrono::high_resolution_clock::now();
-            if (run_tiramisu)
+
+	    if (run_tiramisu)
 	    	swap(b_a.raw_buffer(), b_b.raw_buffer());
-            auto end = std::chrono::high_resolution_clock::now();
+
+	    auto end = std::chrono::high_resolution_clock::now();
             duration_vector_2.push_back(end - start);
         }
     }
+
     print_time("performance_cpu.csv", "swap",
 	       {"Ref", "Tiramisu"},
 	       {median(duration_vector_1), median(duration_vector_2)});
