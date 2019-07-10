466a50f65dd8c2bfab452cfcb4ebebf5e77b1ccf
Riyadh Baghdadi <baghdadi@lanka47-eth.csail.mit.edu>
2019-07-10 19:08:25 -0400

    dibaryon: add Vtune to the compilation script

--- benchmarks/compile_and_run_benchmarks.sh
+++ benchmarks/compile_and_run_benchmarks.sh
@@ -29,9 +29,13 @@ CXXFLAGS="-std=c++11 -O3 -fno-rtti -mavx2"
 #   HL_LLVM_ARGS="-help" 
 # - Set thread number for Halide
 #   HL_NUM_THREADS=32
-# Execution env variables
+# - Execution env variables
 #   OMP_NUM_THREADS=48
 #   to set the number of threads to use by OpenMP.
+# - Command to run Vtune
+#   source /data/scratch/yunming/intel_parallel_studio_cluster/parallel_studio_xe_2017/install/vtune_amplifier_xe/amplxe-vars.sh
+#   amplxe-cl -collect hpc-performance -result-dir vtune_results -quiet ./binary
+#   Guide: https://software.intel.com/en-us/vtune-amplifier-help-amplxe-cl-command-syntax
 
 INCLUDES="-I${MKL_PREFIX}/include/ -I${TIRAMISU_ROOT}/include/ -I${HALIDE_SOURCE_DIRECTORY}/include/ -I${ISL_INCLUDE_DIRECTORY} -I${TIRAMISU_ROOT}/benchmarks/"
 LIBRARIES="-ltiramisu ${MKL_FLAGS} -lHalide -lisl -lz -lpthread"
@@ -57,6 +61,7 @@ fi
 echo "Compiling ${KERNEL} wrapper"
 ${CXX} ${LANKA_OPTIONS} $CXXFLAGS ${INCLUDES} ${DEFINED_SIZE} ${KERNEL}_wrapper.cpp   ${LIBRARIES_DIR} ${LIBRARIES} generated_${KERNEL}.o ${LIBRARIES} -o ${KERNEL}_wrapper
 echo "Running ${KERNEL} wrapper"
+#rm -rf vtune_results
 RUN_REF=1 RUN_TIRAMISU=1 HL_NUM_THREADS=$CORES LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HALIDE_LIB_DIRECTORY}:${ISL_LIB_DIRECTORY}:${TIRAMISU_ROOT}/build/:${MKL_PREFIX}/lib/${MKL_LIB_PATH_SUFFIX} DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${HALIDE_LIB_DIRECTORY}:${TIRAMISU_ROOT}/build/:${MKL_PREFIX}/lib/${MKL_LIB_PATH_SUFFIX} ./${KERNEL}_wrapper
 
 rm -rf ${KERNEL}_generator ${KERNEL}_wrapper generated_${KERNEL}.o generated_${KERNEL}.o.h
