59d18014ed8fd6fb1461190cdeeaec25bbb3c4bd
Nadir Debbagh <nadir199@live.fr>
2019-07-13 22:33:58 +0100

    Fix tiramisu wrapper indent

--- benchmarks/DNN/layers/transpose_conv/transpose_conv_wrapper_tiramisu.cpp
+++ benchmarks/DNN/layers/transpose_conv/transpose_conv_wrapper_tiramisu.cpp
@@ -9,35 +9,35 @@
 
 int main(int, char**)
 {
-    int OUTPUT_N = (N-1)*STRIDE + K;
-    Halide::Buffer<float> input(N, N, FIn, BATCH_SIZE);
-    Halide::Buffer<float> filter(K, K, FIn, FOut);
-    Halide::Buffer<float> bias(FOut);
-    Halide::Buffer<float> result(OUTPUT_N, OUTPUT_N, FOut, BATCH_SIZE);
+    	int OUTPUT_N = (N-1)*STRIDE + K;
+    	Halide::Buffer<float> input(N, N, FIn, BATCH_SIZE);
+    	Halide::Buffer<float> filter(K, K, FIn, FOut);
+    	Halide::Buffer<float> bias(FOut);
+    	Halide::Buffer<float> result(OUTPUT_N, OUTPUT_N, FOut, BATCH_SIZE);
 	init_buffer(result, (float) 0);
 	
-    std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
-    std::vector<std::chrono::duration<double,std::milli>> duration_vector_2;
+    	std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
+    	std::vector<std::chrono::duration<double,std::milli>> duration_vector_2;
 	
-    for (int n=0; n < BATCH_SIZE; n++)
+    	for (int n=0; n < BATCH_SIZE; n++)
 		for (int z=0; z < FIn; z++)
 			for (int y=0; y < N; y++)
 				for (int x=0; x < N; x++)
 					input(x, y, z, n) = 1;
 	
-    for (int z = 0; z < FOut; z++)
-	bias(z) = 1;
+    	for (int z = 0; z < FOut; z++)
+		bias(z) = 1;
 	
-    for (int q=0; q < FOut; q++)
+    	for (int q=0; q < FOut; q++)
 		for (int z=0; z < FIn; z++)
 			for (int y=0; y < K; y++)
 				for (int x=0; x < K; x++)
 					filter(x, y, z, q) = 1;
 	
-    std::cout << "\t\tBuffers initialized" << std::endl;
+    	std::cout << "\t\tBuffers initialized" << std::endl;
 	
-    for (int i=0; i < NB_TESTS; i++)
-    {
+    	for (int i=0; i < NB_TESTS; i++)
+    	{
 		auto start1 = std::chrono::high_resolution_clock::now();
 		transpose_conv(input.raw_buffer(), filter.raw_buffer(), bias.raw_buffer(), result.raw_buffer());
 		auto end1 = std::chrono::high_resolution_clock::now();
@@ -45,10 +45,11 @@ int main(int, char**)
 		duration_vector_2.push_back(duration);
 	}
 	
-    std::cout << "\t\tTiramisu Deconv time : " << median(duration_vector_2) << "; " << std::endl;
+    	std::cout << "\t\tTiramisu Deconv time : " << median(duration_vector_2) << "; " << std::endl;
 	
 	// Print the output
-    if(SHOW_OUTPUT){
+    	if(SHOW_OUTPUT)
+	{
 		std::cout << "\t\tResult" << ": "<< std::endl;
   		for(int n=0; n < BATCH_SIZE; n++)
 		{
@@ -85,7 +86,8 @@ int main(int, char**)
 	}
 	
 	// Print result to a file and compare results with MKLDNN
-    if(SAVE_TO_FILE_AND_COMPARE){
+    	if(SAVE_TO_FILE_AND_COMPARE)
+	{
 		// Write results to file
 		FILE* f = fopen("tiramisu_result.txt", "w");
 		if (f == NULL) {
@@ -120,10 +122,7 @@ int main(int, char**)
 		<< ":\n\n";
 		
 		std::cout << "\t\tPercentage of correctness " << 100*(((double)nb_correct)/(BATCH_SIZE*FOut*(OUTPUT_N-2*PADDING)*(OUTPUT_N-2*PADDING))) << "%" << std::endl << std::endl;
-		
 	}
-	
-	
 	
     return 0;
 }
