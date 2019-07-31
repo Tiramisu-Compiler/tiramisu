49892caf8a3cec4810d30d11721ea46b647e6a21
Kamel Abdous <fk_abdous@esi.dz>
2019-07-31 18:59:50 +0100

    Resize-Conv : switch to 32 output channels

--- benchmarks/DNN/blocks/Resize-Conv/configure.h
+++ benchmarks/DNN/blocks/Resize-Conv/configure.h
@@ -25,7 +25,7 @@
 // Number of features in the input
 #define FIn 3
 // Number of features in the output
-#define FOut 64
+#define FOut 32
 
 // Size of convolution filter (K_YxK_X)
 #define K_X 3
@@ -54,7 +54,7 @@
 // If this is defined, print 10 array elements only
 #define PRINT_ONLY_10 0
 
-#define NB_TESTS 51
+#define NB_TESTS 101
 
 #ifdef __cplusplus
 double median(std::vector<double> scores)
