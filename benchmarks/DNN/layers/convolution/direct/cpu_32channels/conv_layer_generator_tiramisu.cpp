8e3c6ca5f5e26e06acad2fdea4182f4f6fe821fd
Kamel Abdous <fk_abdous@esi.dz>
2019-08-06 23:39:25 +0100

    Conv 32channels : apply register blocking

--- benchmarks/DNN/layers/convolution/direct/cpu_32channels/conv_layer_generator_tiramisu.cpp
+++ benchmarks/DNN/layers/convolution/direct/cpu_32channels/conv_layer_generator_tiramisu.cpp
@@ -33,55 +33,59 @@ int main(int argc, char **argv)
     // -------------------------------------------------------
     // Layer II
     // -------------------------------------------------------
-    var y_b("y_b", 0, Y_NB_BLOCKS), x_b("x_b", 0, X_NB_BLOCKS);
-    var yy, xx;
-    
-    // Loop through weights to load them into cache
-    computation prefetch_weights(
-        "prefetch_weights",
-        {n, fout_b, y_b, x_b, fin_b, k_y, k_x, ffin, ffout},
-        filter(fout_b, fin_b, k_y, k_x, ffin, ffout),
-        SCHEDULE_PREFETCH_WEIGHTS
+
+    // This computation is here to apply register blocking.
+    // Convolution intermediate results will be stored in a small buffer that
+    // will be mapped to CPU registers (more precisely, CPU vector registers) 
+    // instead of being mapped to memory.
+    // This computation moves data from our small buffer to the output buffer
+    computation reg_store(
+        "reg_store",
+        {n, fout_b, y, x, ffout},
+        conv(n, fout_b, y, x, 0, 0, 0, 0, ffout)
     );
-        
-    conv_init.tile(y, x, Y_BLOCKING, X_BLOCKING);
-    conv.tile(y, x, Y_BLOCKING, X_BLOCKING, y_b, x_b, yy, xx);
-        
-    // n, fout_b, y_b, x_b, yy, xx, fin_b, k_y, k_x, ffin, ffout
+    
+    // We split computations over dimension x to apply register blocking
+    var x_b, xx;
+    
+    conv_init.split(x, X_BLOCKING, x_b, xx);
+    conv.split(x, X_BLOCKING, x_b, xx);
+    reg_store.split(x, X_BLOCKING, x_b, xx);
+    
+    // n, fout_b, y, x_b, xx, fin_b, k_y, k_x, fin, ffout
     conv.interchange(xx, fin_b);
     conv.interchange(xx, k_y);
     conv.interchange(xx, k_x);
-    // n, fout_b, y_b, x_b, yy, fin_b, k_y, k_x, xx, ffin, ffout
-    conv.interchange(yy, fin_b);
-    conv.interchange(yy, k_y);
-    conv.interchange(yy, k_x);
-    // n, fout_b, y_b, x_b, fin_b, k_y, k_x, yy, xx, ffin, ffout
-    
+    conv.interchange(xx, ffin);
+    conv.interchange(xx, ffout);
+    // n, fout_b, y, x_b, fin_b, k_y, k_x, fin, ffout, xx
+
     conv.tag_parallel_level(fout_b);
     conv.tag_parallel_level(n);
     
-    conv_init.vectorize(ffout, VEC_LEN);
-    conv.vectorize(ffout, VEC_LEN);
-
-    if (SCHEDULE_PREFETCH_WEIGHTS)
-        conv_init.then(prefetch_weights, x_b)
-                 .then(conv, fin_b);
-                 
-    else
-        conv_init.then(conv, x_b);
-
+    conv_init.vectorize(ffout, FOUT_BLOCKING);
+    conv.vectorize(ffout, FOUT_BLOCKING);
+    reg_store.vectorize(ffout, FOUT_BLOCKING);
+    
+    // Note that reg_store is scheduled after that convolution intermediate results are computed
+    conv_init.then(conv, x_b)
+             .then(reg_store, x_b);
 
     // -------------------------------------------------------
     // Layer III
     // -------------------------------------------------------
     buffer conv_buf("conv_buf", {BATCH_SIZE, FOUT_NB_BLOCKS, N, N, FOUT_BLOCKING}, p_float32, a_output);
-    buffer prefetch_w_buf("prefetch_w_buf", {1}, p_float32, a_temporary);
+    
+    // This is where intermediate results of convolution will be stored.
+    // We rely on the compiler to detect that this buffer can be mapped to CPU registers.
+    buffer reg_buf("reg_buf", {X_BLOCKING, FOUT_BLOCKING}, p_float32, a_temporary);
 
-    if (SCHEDULE_PREFETCH_WEIGHTS)
-        prefetch_weights.store_in(&prefetch_w_buf, {});
+    // Convolution intermediate results are stored in reg_buf.
+    conv_init.store_in(&reg_buf, {x%X_BLOCKING, ffout});
+    conv.store_in(&reg_buf, {x%X_BLOCKING, ffout});
 
-    conv_init.store_in(&conv_buf);
-    conv.store_in(&conv_buf, {n, fout_b, y, x, ffout});
+    // reg_store computation moves data from reg_buf to conv_buf.
+    reg_store.store_in(&conv_buf, {n, fout_b, y, x, ffout});
 
     // -------------------------------------------------------
     // Code Generation
