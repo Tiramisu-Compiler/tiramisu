fcff7493d29ffe82282bcdf9b74f044d32b14818
Nadir <nadir199@live.fr>
2019-08-24 04:38:54 +0100

    Sparse LSTM : Add benchmark + mkl sparse implementation

---
+++ benchmarks/DNN/blocks/LSTM/cpu_lib_sparse/clean.sh
@@ -0,0 +1,1 @@
+rm lstm_sparse_generator_tiramisu lstm_mkldnn lstm_mkl_sparse wrapper_lstm_sparse lstm_sparse.o lstm_sparse.o.h mkl_result.txt tiramisu_result.txt
---
+++ benchmarks/DNN/blocks/LSTM/cpu_lib_sparse/configure.h
@@ -0,0 +1,99 @@
+#ifndef __LSTM_CPULIB_CONF_HEADER_
+#define __LSTM_CPULIB_CONF_HEADER_
+
+#include <sys/time.h>
+#include <vector>
+
+#define VEC_LEN 8
+#define WEIGHTS_DENSITY 0.2
+
+#define LARGE_DATA_SET	0
+#define MEDIUM_DATA_SET	0
+#define SMALL_DATA_SET	0
+#define NO_BATCH        1
+
+#if LARGE_DATA_SET
+	#define BATCH_SIZE 100
+#elif MEDIUM_DATA_SET
+	#define BATCH_SIZE 32
+#elif SMALL_DATA_SET
+	#define BATCH_SIZE 8
+#elif NO_BATCH
+    #define BATCH_SIZE 1
+#endif
+
+#define FEATURE_SIZE 1024
+#define SEQ_LENGTH 100
+#define NUM_LAYERS 4
+
+// Parameters for Tiramisu code
+#define GEMM_BATCH SEQ_LENGTH
+
+#define NB_TESTS 101
+
+#if 1  // Flip to use double precision
+    #define DATA_TYPE float
+    #define DATA_TYPE_P p_float32
+    #define DATA_TYPE_CUDNN CUDNN_DATA_FLOAT
+#else
+    #define DATA_TYPE double
+    #define DATA_TYPE_P p_float64
+    #define DATA_TYPE_CUDNN CUDNN_DATA_DOUBLE
+#endif
+
+#ifdef __cplusplus
+double median(std::vector<double> scores)
+{
+    double median;
+    size_t size = scores.size();
+
+    sort(scores.begin(), scores.end());
+
+    if (size % 2 == 0)
+    {
+        median = (scores[size / 2 - 1] + scores[size / 2]) / 2;
+    }
+    else
+    {
+        median = scores[size / 2];
+    }
+
+    return median;
+}
+#else
+double median(int n, double x[])
+{
+    double temp;
+    int i, j;
+
+    // The following two loops sort the array x in ascending order
+    for(i=0; i<n-1; i++) {
+        for(j=i+1; j<n; j++) {
+            if(x[j] < x[i]) {
+                // Swap elements
+                temp = x[i];
+                x[i] = x[j];
+                x[j] = temp;
+            }
+        }
+    }
+
+    if(n%2==0) {
+        // If there is an even number of elements, return mean of the two elements in the middle
+        return((x[n/2] + x[n/2 - 1]) / 2.0);
+    } else {
+        // Else return the element in the middle
+        return x[n/2];
+    }
+}
+#endif
+
+double rtclock()
+{
+    struct timeval Tp;
+    gettimeofday(&Tp, NULL);
+
+    return (Tp.tv_sec + Tp.tv_usec * 1.0e-6);
+}
+
+#endif
