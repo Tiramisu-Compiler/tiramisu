c34c35b5afd3f3b66fbb7bf56c208834ca97c72f
Merouani Massinissa <fm_merouani@esi.dz>
2019-10-18 16:41:14 +0100

    Added a classifier model and notebook, Added functions for filtering and transforming data on loading

--- utils/speedup_model/src/data/load_data.py
+++ utils/speedup_model/src/data/load_data.py
@@ -166,14 +166,9 @@ def serialize(programs, schedules, exec_times, filename='speedup_dataset.pkl'):
 if __name__=='__main__':
     st = Stats('/data/scratch/mmerouani/data/batch2501-3000/')
 
-    print("loading data")
-<<<<<<< HEAD
+    print("loading data")   
 #     programs, schedules, exec_times = st.load_data()
     programs, schedules, exec_times = st.load_data_separate_exec_times('/data/scratch/mmerouani/data/batch2501-3000/final_exec_times_batch2501-3000.pickle')
-=======
-    programs, schedules, exec_times = st.load_data()
-#     programs, schedules, exec_times = st.load_data_separate_exec_times('/data/scratch/mmerouani/data/batch2001-2500/final_exec_times_batch2001-2500.pickle')
->>>>>>> 426e83b1ba4ee8916ddd597afee34182b0c97a96
     print("data loaded")
     print("calculating model input")
     #data_to_h5(programs, schedules, exec_times)
