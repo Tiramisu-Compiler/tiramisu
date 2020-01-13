6c805b889202fadf2152e25d6c1091b3ff883bd7
Kamel Abdous <fk_abdous@esi.dz>
2020-01-13 15:21:04 +0100

    Autoscheduler : build a simple version of the computation graph

--- src/auto_scheduler/tiramisu_auto_scheduler.cpp
+++ src/auto_scheduler/tiramisu_auto_scheduler.cpp
@@ -9,17 +9,25 @@ computation_graph::computation_graph(tiramisu::function *fct)
 {
     const std::vector<computation*> computations = fct->get_computations();
     
-    for (computation* comp : computations) 
+    for (computation *comp : computations) 
     {
-        cg_node* node = new cg_node();
+        cg_node *node = new cg_node();
         node->comp = comp;
         
         // Get computation iterators
+        isl_set *iter_domain = comp->get_iteration_domain();
+        
+        for (int i = 0; i < isl_set_dim(iter_domain, isl_dim_set); ++i)
+        {
+            std::string iter_name = isl_set_get_dim_name(iter_domain, isl_dim_set, i);
+            int lower_bound = utility::get_bound(iter_domain, i, false).get_int_val();
+            int upper_bound = utility::get_bound(iter_domain, i, true).get_int_val();
+            
+            node->iterators.push_back(iterator(iter_name, lower_bound, upper_bound));
+        }
         
         roots.push_back(node);
     }
-    
-    // Build the computation graph
 }
 
 auto_scheduler::auto_scheduler(search_method *searcher, evaluator *eval_func, 
