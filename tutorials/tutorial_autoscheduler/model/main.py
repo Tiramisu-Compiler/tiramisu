b56e2bffe34fa762ef148a00f56f2aac07b7568b
Kamel Abdous <fk_abdous@esi.dz>
2020-08-26 10:41:33 +0100

    Autoscheduler tutorial : use try/catch in model/main.py

--- tutorials/tutorial_autoscheduler/model/main.py
+++ tutorials/tutorial_autoscheduler/model/main.py
@@ -5,37 +5,42 @@ from hier_lstm import Model_hier_LSTM
 from json_to_tensor import *
 
 import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning) 
-warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings('ignore', category=DeprecationWarning) 
+warnings.filterwarnings('ignore', category=UserWarning)
 
-model_path = "/data/tiramisu/tutorials/tutorial_autoscheduler/model/hier_LSTM_fusion_tree_tagLo_transfer_5bl.pkl"
+model_path = '/data/tiramisu/tutorials/tutorial_autoscheduler/model/hier_LSTM_fusion_tree_tagLo_transfer_5bl.pkl'
 
 with torch.no_grad():
-    device = "cpu"
-    torch.device("cpu")
+    device = 'cpu'
+    torch.device('cpu')
     
     environ['layers'] = '600 350 200 180'
-    environ['dropouts'] = '0.225 '*4
+    environ['dropouts'] = '0.225 ' * 4
     
-    input_size = 1267*2
+    input_size = 1267 * 2
     output_size = 1
     
     layers_sizes = list(map(int, environ.get('layers', '300 200 120 80 30').split()))
     drops = list(map(float, environ.get('dropouts', '0.2 0.2 0.1 0.1 0.1').split()))
 
     model = Model_hier_LSTM(input_size, output_size, hidden_sizes=layers_sizes, drops=drops)
-    model.load_state_dict(torch.load(model_path, map_location="cpu"))
+    model.load_state_dict(torch.load(model_path, map_location='cpu'))
     model.to(device)
     model.eval()
 
-    while True:
-        prog_json = input()
-        sched_json = input()
+    try:
+        while True:
+            prog_json = input()
+            sched_json = input()
 
-        prog_json = json.loads(prog_json)
-        sched_json = json.loads(sched_json)
+            prog_json = json.loads(prog_json)
+            sched_json = json.loads(sched_json)
 
-        tree_tensor = get_representation(prog_json, sched_json)
+            tree_tensor = get_representation(prog_json, sched_json)
+            
+            speedup = model.forward(tree_tensor)
+            print(float(speedup.item()))
+            
+    except EOFError:
+        exit()
         
-        speedup = model.forward(tree_tensor)
-        print(float(speedup.item()))
