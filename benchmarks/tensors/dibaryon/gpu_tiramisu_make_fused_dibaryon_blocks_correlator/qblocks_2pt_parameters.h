add83c74650f38c999661d2568f067127de6c6a4
NEDJIMAbelgacem <gb_nedjima@esi.dz>
2021-07-25 07:40:05 +0100

    change dataset 1 requirements

--- benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator/qblocks_2pt_parameters.h
+++ benchmarks/tensors/dibaryon/gpu_tiramisu_make_fused_dibaryon_blocks_correlator/qblocks_2pt_parameters.h
@@ -18,7 +18,7 @@
 #define P_Nw 12
 #define P_Nw2 288
 #define P_Nw2Hex 32
-#define P_Nt 2
+#define P_Nt 1
 #define P_Nc 3
 #define P_Ns 2
 #define P_Nq 3
@@ -30,8 +30,8 @@
 #define P_B0Nrows 1
 #define P_Mw 12
 #define P_NsFull 4
-#define P_sites_per_rank 2
-#define P_src_sites_per_rank 2
+#define P_sites_per_rank 4
+#define P_src_sites_per_rank 4
 #define P_tiling_factor 8
 
 #elif DATA_SET_2
