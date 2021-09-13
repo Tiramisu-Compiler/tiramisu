b837fa4069c4ce3eac47d1d3a56cf70e5c93a464
Mascinissa <fm_merouani@esi.dz>
2021-09-13 15:20:05 +0400

    Add autoscheduler tutorial

--- tutorials/tutorial_autoscheduler/wrapper.cpp
+++ tutorials/tutorial_autoscheduler/wrapper.cpp
@@ -8,26 +8,36 @@ using namespace std;
 
 int main(int, char **argv)
 {
-    Halide::Buffer<int32_t> buf01(1024, 1024,"buffer01");
+    Halide::Buffer<int32_t> buf01(1024, 1024, 2, 8);
 
-    init_buffer(buf01, (int32_t)2);
-    
+    int *c_buf00 = (int*)malloc(2 * sizeof(int));
+    parallel_init_buffer(c_buf00, 2,  (int32_t)72);
+    Halide::Buffer<int32_t> buf00(c_buf00, 2);
+
+    int *c_buf02 = (int*)malloc(1026 * 1026 * 3 * 8 * sizeof(int));
+    parallel_init_buffer(c_buf02, 1026 * 1026 * 3 * 8,  (int32_t)69);
+    Halide::Buffer<int32_t> buf02(c_buf02, 1026, 1026, 3, 8);
+
+    int *c_buf03 = (int*)malloc(3 * 3 * 3 * 2 * sizeof(int));
+    parallel_init_buffer(c_buf03, 3 * 3 * 3 * 2,  (int32_t)57);
+    Halide::Buffer<int32_t> buf03(c_buf03, 3, 3, 3, 2);
+
     std::vector<double> duration_vector;
     double start, end;
-    
-    for (int i = 0; i < 0; ++i) 
-        conv(buf01.raw_buffer());
-    
-    for (int i = 0; i < 2; i++)
+
+    for (int i = 0; i < 2; ++i)
+        conv(buf01.raw_buffer(), buf00.raw_buffer(), buf02.raw_buffer(), buf03.raw_buffer());
+
+    for (int i = 0; i < 10; i++)
     {
         start = rtclock();
-        conv(buf01.raw_buffer());
+        conv(buf01.raw_buffer(), buf00.raw_buffer(), buf02.raw_buffer(), buf03.raw_buffer());
         end = rtclock();
-        
+
         duration_vector.push_back((end - start) * 1000);
     }
 
     std::cout << median(duration_vector) << std::endl;
-    
+
     return 0;
 }
