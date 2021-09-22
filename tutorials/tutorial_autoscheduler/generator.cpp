d7598f2c5b3b17b1b3fda468c7c2d71d77f1bf64
Nabiltchoulak <tchoulaknabil@gmail.com>
2021-09-22 09:54:39 +0100

    debug2.1

--- tutorials/tutorial_autoscheduler/generator.cpp
+++ tutorials/tutorial_autoscheduler/generator.cpp
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
 
     bool perform_autoscheduling = false;
 
-    perform_autoscheduling=false;
+    perform_autoscheduling=true;
     
     // Generate a program with no schedule
     if (!perform_autoscheduling)
