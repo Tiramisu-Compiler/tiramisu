4073c8f646e19723f33598a56115e77dedb40ff4
teocollin1995 <teoc@mit.edu>
2022-04-01 15:13:47 -0400

    var <: expr

--- python_bindings/src/PyVar.cpp
+++ python_bindings/src/PyVar.cpp
@@ -4,7 +4,7 @@ namespace tiramisu {
   namespace PythonBindings {
 
     void define_var(py::module &m){
-      auto var_class = py::class_<var>(m, "var")
+      auto var_class = py::class_<var, expr>(m, "var")
         .def(py::init<tiramisu::primitive_t, std::string>())
         .def(py::init<std::string>())
         .def(py::init<std::string, tiramisu::expr, tiramisu::expr>())
