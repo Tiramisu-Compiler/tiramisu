ba4bee2ed697c720450eed015c5bd56ead24f4d9
Richard Sollee <solleer@mit.edu>
2022-04-04 21:29:15 -0400

    Add tile to computation

--- python_bindings/src/PyComputation.cpp
+++ python_bindings/src/PyComputation.cpp
@@ -7,7 +7,14 @@ namespace tiramisu {
       auto const_class = py::class_<computation>(m, "computation")
         .def(py::init<std::string, std::vector< var >, tiramisu::expr >())
         .def("parallelize", &computation::parallelize)
-        .def("store_in", py::overload_cast<tiramisu::buffer*>(&computation::store_in));
+        .def("store_in", py::overload_cast<tiramisu::buffer*>(&computation::store_in))
+        .def("store_in", py::overload_cast<tiramisu::buffer*, std::vector<tiramisu::expr>>(&computation::store_in))
+        .def("tile", py::overload_cast<var, var, int, int>(&computation::tile))
+        .def("tile", py::overload_cast<var, var, int, int, var, var, var, var>(&computation::tile))
+        .def("tile", py::overload_cast<var, var, var, int, int, int>(&computation::tile))
+        .def("tile", py::overload_cast<var, var, var, int, int, int, var, var, var, var, var, var>(&computation::tile))
+        .def("tile", py::overload_cast<int, int, int, int>(&computation::tile))
+        .def("tile", py::overload_cast<int, int, int, int, int, int>(&computation::tile));
     }
 
   }  // namespace PythonBindings
