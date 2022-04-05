e40b3ca07034802b574f19af157c94cccaa46bfd
Richard Sollee <solleer@mit.edu>
2022-04-04 21:41:45 -0400

    Add after, set_expression, gpu_tile, then, split, and cache_shared to computation

--- python_bindings/src/PyComputation.cpp
+++ python_bindings/src/PyComputation.cpp
@@ -14,7 +14,19 @@ namespace tiramisu {
         .def("tile", py::overload_cast<var, var, var, int, int, int>(&computation::tile))
         .def("tile", py::overload_cast<var, var, var, int, int, int, var, var, var, var, var, var>(&computation::tile))
         .def("tile", py::overload_cast<int, int, int, int>(&computation::tile))
-        .def("tile", py::overload_cast<int, int, int, int, int, int>(&computation::tile));
+        .def("tile", py::overload_cast<int, int, int, int, int, int>(&computation::tile))
+        .def("after", py::overload_cast<computation&, var>(&computation::after))
+        .def("after", py::overload_cast<computation&, int>(&computation::after))
+        .def("set_expression", &computation::set_expression)
+        .def("gpu_tile", py::overload_cast<var, var, int, int>(&computation::gpu_tile))
+        .def("gpu_tile", py::overload_cast<var, var, int, int, var, var, var, var>(&computation::gpu_tile))
+        .def("gpu_tile", py::overload_cast<var, var, var, int, int, int>(&computation::gpu_tile))
+        .def("gpu_tile", py::overload_cast<var, var, var, int, int, int, var, var, var, var, var, var>(&computation::gpu_tile))
+        .def("then", py::overload_cast<computation&, var>(&computation::then))
+        .def("split", py::overload_cast<var, int>(&computation::split))
+        .def("split", py::overload_cast<var, int, var, var>(&computation::split))
+        .def("split", py::overload_cast<int, int>(&computation::split))
+        .def("cache_shared", &computation::cache_shared);
     }
 
   }  // namespace PythonBindings
