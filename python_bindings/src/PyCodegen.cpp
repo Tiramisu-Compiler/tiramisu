305baefdf44f3880f0b96bc62ba18299f2de235f
Richard Sollee <solleer@mit.edu>
2022-04-04 21:07:27 -0400

    Add default arg for codegen

--- python_bindings/src/PyCodegen.cpp
+++ python_bindings/src/PyCodegen.cpp
@@ -6,7 +6,9 @@ namespace tiramisu {
 
     void define_codegen(py::module &m){
       m.def("codegen", 
-            py::overload_cast<const std::vector<tiramisu::buffer *> &, const std::string, const bool>(&tiramisu::codegen));
+            py::overload_cast<const std::vector<tiramisu::buffer *> &, const std::string, const bool>(&tiramisu::codegen),
+            "This function generates the declared function and computations in an object file",
+            py::arg("arguments"), py::arg("obj_filename"), py::arg("gen_cuda_stmt") = false);
     }
 
   }  // namespace PythonBindings
