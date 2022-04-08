47e9d13731493f8949f1c6b68b0d1b90b6b56239
teocollin1995 <teoc@mit.edu>
2022-04-07 22:27:10 -0400

    init and codegen from function

--- python_bindings/src/PyFunction.cpp
+++ python_bindings/src/PyFunction.cpp
@@ -1,10 +1,13 @@
 #include "PyFunction.h"
 
-
 namespace tiramisu {
   namespace PythonBindings {
     void define_function(py::module &m){
-      auto function_class = py::class_<function>(m, "function");
+      auto function_class = py::class_<function>(m, "function")
+	.def(py::init<std::string>())
+	.def("codegen", py::overload_cast<const std::vector<tiramisu::buffer *> &, const std::string, const bool>(&tiramisu::function::codegen));
+      
+
     }
 
 
