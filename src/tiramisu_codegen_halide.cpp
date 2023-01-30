d1ebeb624d7e9f04c6025ffaede34981def429a9
teocollin1995 <teoc@mit.edu>
2023-01-30 15:29:22 -0500

    proper includes.

--- src/tiramisu_codegen_halide.cpp
+++ src/tiramisu_codegen_halide.cpp
@@ -17,10 +17,10 @@
 
 #include <string>
 #include "../include/tiramisu/expr.h"
-#include "../3rdParty/Halide/src/Expr.h"
-#include "../3rdParty/Halide/src/Parameter.h"
+#include "Halide/src/Expr.h"
+#include "Halide/src/Parameter.h"
 #include "../include/tiramisu/debug.h"
-#include "../3rdParty/Halide/src/IR.h"
+#include "Halide/src/IR.h"
 #include "../include/tiramisu/core.h"
 
 namespace tiramisu
