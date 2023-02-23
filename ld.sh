731412baf803029a6808a9b9abf7937e2ede3d84
teocollin1995 <teoc@mit.edu>
2023-02-23 15:44:01 -0500

    get rid of unneeded file

--- ld.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-export PATH=/data/scratch/teoc/installs/cmake/cmake-3.21.4-linux-x86_64/bin:/data/scratch/teoc/installs/pythons/py39p/install/bin:/data/scratch/teoc/installs/tiramisu/tiramisu/3rdParty/llvm/build/bin:$PATH
-export LD_LIBRARY_PATH=/data/scratch/teoc/installs/pythons/py39p/install/lib:/data/scratch/teoc/installs/tiramisu/tiramisu/3rdParty/Halide/build/src:/data/scratch/teoc/installs/tiramisu/tiramisu/3rdParty/llvm/build/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/data/scratch/teoc/installs/tiramisu/tiramisu/build:$PYTHONPATH
