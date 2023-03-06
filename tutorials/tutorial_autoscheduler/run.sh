594ab920f1b743be04f540a6cb98a8bc2e22b279
affifboudaoud <hk_boudaoud@esi.dz>
2023-03-06 15:12:47 +0400

    changed cmake list to support script

--- tutorials/tutorial_autoscheduler/run.sh
+++ tutorials/tutorial_autoscheduler/run.sh
@@ -11,16 +11,23 @@ export PRUNE_SLOW_SCHEDULES=0
 export MAX_DEPTH=4
 export MIN_RUNS=1
 export TIRAMISU_ROOT=/data/kb4083/tiramisu/
+
+# Compile the autoscheduler
+cd -I${TIRAMISU_ROOT}/build
+make -j tiramisu_auto_scheduler
+
+cd -I${TIRAMISU_ROOT}/tutorials/tutorial_autoscheduler
+# Cmpile and run a program
 mkdir build ; cd build
 export BUILD_TARGET="generator"
 export FUNCTION_TO_BUILD=$1
-cmake ..
+cmake .. -DBUILD_TARGET="generator"  -DFUNCTION_TO_BUILD=$1
 make
 ../generator
 g++ -shared -o function.o.so $1.o
+# Generate a wrapper
 g++ -std=c++11 -fno-rtti -I${TIRAMISU_ROOT}/include -DTIRAMISU_MINI -I${TIRAMISU_ROOT}/3rdParty/Halide/include -I${TIRAMISU_ROOT}/3rdParty/isl/include/ -I${TIRAMISU_ROOT}/benchmarks -L${TIRAMISU_ROOT}/build -L${TIRAMISU_ROOT}/3rdParty/Halide/lib/ -L${TIRAMISU_ROOT}/3rdParty/isl/build/lib -o $1_wrapper -ltiramisu -lHalide -ldl -lpthread -lz -lm -Wl,-rpath,${TIRAMISU_ROOT}/build ../$1_wrapper.cpp ./function.o.so -ltiramisu -lHalide -ldl -lpthread -lz -lm
-echo running autoscheduler
-export BUILD_TARGET="autoscheduler"
-cmake ..
+
+cmake .. -DBUILD_TARGET="autoscheduler"  -DFUNCTION_TO_BUILD=$1
 make
 ../generator
