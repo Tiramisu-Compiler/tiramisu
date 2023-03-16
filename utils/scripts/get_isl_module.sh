8a317ece36b44bef895c09ef7811b0f36fae228d
Tchoulak Nassim <gn_tchoulak@esi.dz>
2023-03-17 00:56:52 +0100

    upgrade isl version to 0.23

--- utils/scripts/get_isl_module.sh
+++ utils/scripts/get_isl_module.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 PROJECT_SRC_DIR=$1
-VERSION=19
+VERSION=23
 
 set -e
 . ${PROJECT_SRC_DIR}/utils/scripts/functions.sh
@@ -15,7 +15,12 @@ echo "### Remove old ISL, get a new one and decompress it. ####"
 
 echo_and_run_cmd "cd ${PROJECT_SRC_DIR}/3rdParty/"
 echo_and_run_cmd "rm -rf isl/"
-echo_and_run_cmd "wget http://isl.gforge.inria.fr/isl-0.$VERSION.tar.gz"
+echo_and_run_cmd "wget https://libisl.sourceforge.io/isl-0.$VERSION.tar.gz"
 echo_and_run_cmd "tar -xvf isl-0.$VERSION.tar.gz"
 echo_and_run_cmd "rm -rf isl-0.$VERSION.tar.gz"
 echo_and_run_cmd "mv isl-0.$VERSION isl/"
+echo_and_run_cmd "cd ./isl"
+echo_and_run_cmd "mkdir build"
+echo_and_run_cmd "./configure --prefix=${PROJECT_SRC_DIR}/3rdParty/isl/build --enable-static --with-pic"
+echo_and_run_cmd "make"
+echo_and_run_cmd "make install"
