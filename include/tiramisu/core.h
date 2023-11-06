9cae4ec7d851bc58c14d4572abcfd7fe8377f602
Smail KOURTA <sk10691@nyu.edu>
2023-11-06 08:53:40 +0400

    added shifting corrector call by levels

--- include/tiramisu/core.h
+++ include/tiramisu/core.h
@@ -1488,6 +1488,18 @@ public:
     std::vector<std::tuple<tiramisu::var,int>> correcting_loop_fusion_with_shifting(std::vector<tiramisu::computation*> previous_computations, tiramisu::computation current, std::vector<tiramisu::var> vars_subjected_to_shifting);
 
     /**
+     * Automatically computes the shifting parameters for variables at levels \p var_levels_subjected_to_shifting of the \p current that would allow to legally fuse \p current computation,
+     * with the vector of computations \p previous_computations if it is possible.
+     * This method return a vector of tuples mapping each variable with the required shifting if the fusion is possible, and an empty vector otherwise(impossible fusion).
+     * Note: In case where the fusion is legal and doesn't require shifting, the vector of tuples would map the variable to 0.
+     * The method relies fully on the dependence analysis result, so the  method \p perform_full_dependency_analysis() must be invoked before.
+     * To correctly invoke this method : schedules must be aligned (same out dimension size) and ordered,
+     * so invoking \p prepare_schedules_for_legality_checks() method before is mandatory. 
+     * The shifting parameters given are always superior or equal to zero. This is an additional internal condition.
+    */
+    std::vector<std::tuple<tiramisu::var,int>> correcting_loop_fusion_with_shifting(std::vector<tiramisu::computation*> previous_computations, tiramisu::computation current, std::vector<int> var_levels_subjected_to_shifting);
+
+    /**
      * Uses the dependency analysis to check if the specified schedules of computations are legal.
      * This method only tests the dependencies between the computations specified in the input and ignore the rest.
      * must be invoked after the correct call to \p perform_full_dependency_analysis()
